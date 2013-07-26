7f4ad86ee83542ebb2a72adebce3a3cb6f83bf7c
Timothy M. Shead <tshead@sandia.gov>
2013-07-26 14:44:48 -0600

    First pass with our homegrown CCA that produces familiar canonical variates.

--- analysis-server/test-cca-homegrown.py
+++ analysis-server/test-cca-homegrown.py
@@ -2,6 +2,7 @@ from slycat.analysis import *
 import matplotlib.pyplot as pyplot
 import numpy
 import scipy.linalg
+import scipy.stats
 
 autos = load("../data/automobiles.csv", "csv-file", chunk_size=100)
 inputs = project(autos, "Year", "Cylinders", "Displacement")
@@ -15,30 +16,40 @@ Y = Y[good]
 ###################################################
 # Introducing ... the Slycat implementation of CCA!
 n = X.shape[0]
+p1 = X.shape[1]
+p2 = Y.shape[1]
+
 X -= X.mean(axis=0)
 Y -= Y.mean(axis=0)
 
 if True: # Optionally scale data to unit variance
-  X /= X.std(axis=0)
+  X /= X.std(axis=0) # TODO: decide whether we do this sklearn's way or matlab's way
   Y /= Y.std(axis=0)
 
 Q1, R1, P1 = scipy.linalg.qr(X, mode="economic", pivoting=True)
 Q2, R2, P2 = scipy.linalg.qr(Y, mode="economic", pivoting=True)
-numpy.testing.assert_array_almost_equal(X, numpy.dot(Q1, R1))
-numpy.testing.assert_array_almost_equal(Y, numpy.dot(Q2, R2))
 
-# TODO: Test for rank deficiencies here.
+Xrank = sum((numpy.abs(numpy.diag(R1)) > 0) * max(n, p1))
+Yrank = sum((numpy.abs(numpy.diag(R2)) > 0) * max(n, p2))
+if Xrank == 0:
+  raise Exception("X must contain at least one non-constant column.")
+if Xrank < p1:
+  raise Exception("X is not full rank.")
+if Yrank == 0:
+  raise Exception("Y must contain at least one non-constant column.")
+if Yrank < p2:
+  raise Exception("Y is not full rank.")
 
 L, D, M = scipy.linalg.svd(numpy.dot(Q1.T, Q2), full_matrices=False)
-numpy.testing.assert_array_almost_equal(numpy.dot(L, numpy.dot(numpy.diag(D), M)), numpy.dot(Q1.T, Q2))
 
 A = numpy.linalg.solve(R1, L)
-B = numpy.linalg.solve(R2, M)
-numpy.testing.assert_array_almost_equal(numpy.dot(R1, A), L)
-numpy.testing.assert_array_almost_equal(numpy.dot(R2, B), M)
+B = numpy.linalg.solve(R2, M.T)
 
 A *= numpy.sqrt(n - 1)
 B *= numpy.sqrt(n - 1)
+
+A = A.T[P1].T
+B = B.T[P2].T
 
 x = numpy.dot(X, A)
 y = numpy.dot(Y, B)
