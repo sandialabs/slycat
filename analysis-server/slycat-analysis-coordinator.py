ac7c7b9acfb7eb798d2aa9003836c801cf5e4d92
Timothy M. Shead <tshead@sandia.gov>
2013-08-05 16:29:09 -0600

    Remove unused code.

--- analysis-server/slycat-analysis-coordinator.py
+++ analysis-server/slycat-analysis-coordinator.py
@@ -10,7 +10,6 @@ import numpy
 import os
 import Pyro4
 from slycat.analysis import __file__ as plugin_root
-from slycat.analysis.api import InvalidArgument
 
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
@@ -53,81 +52,9 @@ class factory(pyro_object):
     """Returns the set of available slycat analysis workers."""
     return [Pyro4.Proxy(self.nameserver.lookup(worker)) for worker in self.nameserver.list(prefix="slycat.worker").keys()]
 
-  def require_attribute_name(self, name):
-    if not isinstance(name, basestring):
-      raise InvalidArgument("Attribute name must be a string.")
-    return name
-  def require_attribute_type(self, type):
-    allowed_types = ["int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64", "float32", "float64", "string"]
-    if type not in allowed_types:
-      raise InvalidArgument("Attribute type must be one of %s" % ",".join(allowed_types))
-    return type
-  def require_attribute(self, attribute):
-    if isinstance(attribute, basestring):
-      attribute = {"name":attribute, "type":"float64"}
-    elif isinstance(attribute, tuple):
-      if len(attribute) != 2:
-        raise InvalidArgument("Attribute must have a name and a type.")
-      attribute = {"name":attribute[0], "type":attribute[1]}
-    elif isinstance(attribute, dict):
-      if "name" not in attribute:
-        raise InvalidArgument("Attribute must have a name.")
-      if "type" not in attribute:
-        raise InvalidArgument("Attribute must have a type.")
-    self.require_attribute_name(attribute["name"])
-    self.require_attribute_type(attribute["type"])
-    return attribute
-  def require_attributes(self, attributes):
-    if isinstance(attributes, basestring):
-      attributes = [self.require_attribute(attributes)]
-    elif isinstance(attributes, tuple):
-      attributes = [self.require_attribute(attributes)]
-    elif isinstance(attributes, dict):
-      attributes = [self.require_attribute(attributes)]
-    else:
-      attributes = [self.require_attribute(attribute) for attribute in attributes]
-    return attributes
-  def require_attribute_names(self, names):
-    if isinstance(names, basestring):
-      return [self.require_attribute_name(names)]
-    return [self.require_attribute_name(name) for name in names]
-  def require_chunk_sizes(self, shape, chunk_sizes):
-    """Return array chunk sizes (tuple of dimension lengths), treating a single integer as a 1-tuple and sanity-checking the results against an array shape."""
-    if chunk_sizes is None:
-      chunk_sizes = shape
-    elif isinstance(chunk_sizes, int):
-      chunk_sizes = tuple([chunk_sizes])
-    else:
-      chunk_sizes = tuple(chunk_sizes)
-    if len(shape) != len(chunk_sizes):
-      raise InvalidArgument("Array shape and chunk sizes must contain the same number of dimensions.")
-    return chunk_sizes
-  def require_dimension_name(self, name):
-    if not isinstance(name, basestring):
-      raise InvalidArgument("Dimension name must be a string.")
-    return name
-  def require_dimension_names(self, names):
-    if isinstance(names, basestring):
-      return [self.require_dimension_name(names)]
-    return [self.require_dimension_name(name) for name in names]
-  def require_expression(self, expression):
-    if isinstance(expression, basestring):
-      expression = ast.parse(expression)
-    else:
-      raise InvalidArgument("Expression must be a string.")
-    return expression
   def require_object(self, uri):
     """Lookup a Pyro URI, returning the corresponding Python object."""
     return self._pyroDaemon.objectsById[uri.asString().split(":")[1].split("@")[0]]
-  def require_shape(self, shape):
-    """Return an array shape (tuple of dimension lengths), treating a single integer as a 1-tuple."""
-    if isinstance(shape, int):
-      shape = tuple([shape])
-    else:
-      shape = tuple(shape)
-    if not len(shape):
-      raise InvalidArgument("Array shape must have at least one dimension.")
-    return shape
 
 class array(pyro_object):
   """Abstract interface for a remote, multi-attribute, multi-dimensional array."""
