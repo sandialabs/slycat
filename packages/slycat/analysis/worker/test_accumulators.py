8d3e7afa20712f19093c07bdaa39825083384019
Timothy M. Shead <tshead@sandia.gov>
2013-08-06 21:38:22 -0600

    Add a test for accumulators on string types.

--- packages/slycat/analysis/worker/test_accumulators.py
+++ packages/slycat/analysis/worker/test_accumulators.py
@@ -3,12 +3,23 @@ import sys
 
 from slycat.analysis.worker.accumulator import *
 
-def test_basic():
+def test_min_basic_string():
+  m = minimum()
+  m.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(m.result(), "a")
+
+def test_min_reduce_string():
+  m = minimum()
+  m.accumulate(numpy.array(["b", "c", "d"]))
+  m.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(m.result(), "a")
+
+def test_histogram_basic():
   h = histogram(10)
   h.accumulate(numpy.random.random(100))
   numpy.testing.assert_equal(len(h.result()), 10)
 
-def test_multi():
+def test_histogram_reduce():
   h1 = histogram(10)
   h1.accumulate(numpy.random.random(100))
   h2 = histogram(10)
