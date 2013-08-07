4795609160350eef89d1b2bcb1a4527c59182b86
Timothy M. Shead <tshead@sandia.gov>
2013-08-07 00:04:36 -0600

    Update accumulators to work with string attributes.

--- packages/slycat/analysis/worker/accumulator.py
+++ packages/slycat/analysis/worker/accumulator.py
@@ -37,8 +37,9 @@ class average(accumulator):
     self.count = 0
     self.sum = 0.0
   def accumulate(self, observations):
-    self.count += observations.size
-    self.sum += observations.sum()
+    if observations.dtype.char != "S":
+      self.count += observations.size
+      self.sum += observations.sum()
   def reduce(self, other):
     self.count += other.count
     self.sum += other.sum
@@ -71,8 +72,14 @@ class distinct(accumulator):
 class maximum(accumulator):
   def __init__(self):
     self.max = None
+  @staticmethod
+  def flexible_max(observations):
+    if observations.dtype.char == "S":
+      return max(observations)
+    else:
+      return observations.max()
   def accumulate(self, observations):
-    self.max = observations.max() if self.max is None else max(self.max, observations.max())
+    self.max = maximum.flexible_max(observations) if self.max is None else max(self.max, maximum.flexible_max(observations))
   def reduce(self, other):
     self.max = other.max if self.max is None else self.max if other.max is None else max(self.max, other.max)
   def result(self):
@@ -81,8 +88,14 @@ class maximum(accumulator):
 class minimum(accumulator):
   def __init__(self):
     self.min = None
+  @staticmethod
+  def flexible_min(observations):
+    if observations.dtype.char == "S":
+      return min(observations)
+    else:
+      return observations.min()
   def accumulate(self, observations):
-    self.min = observations.min() if self.min is None else min(self.min, observations.min())
+    self.min = minimum.flexible_min(observations) if self.min is None else min(self.min, minimum.flexible_min(observations))
   def reduce(self, other):
     self.min = other.min if self.min is None else self.min if other.min is None else min(self.min, other.min)
   def result(self):
@@ -93,8 +106,9 @@ class summation(accumulator):
     self.count = 0
     self.sum = 0.0
   def accumulate(self, observations):
-    self.count += observations.size
-    self.sum += observations.sum()
+    if observations.dtype.char != "S":
+      self.count += observations.size
+      self.sum += observations.sum()
   def reduce(self, other):
     self.count += other.count
     self.sum += other.sum
@@ -105,11 +119,18 @@ class histogram(accumulator):
   def __init__(self, bins):
     self.bins = bins
     self.histogram = None
+  def __repr__(self):
+    return "<histogram %s %s>" % (self.bins, self.histogram)
+  def flexible_histogram(self, observations):
+    if observations.dtype.char == "S":
+      return numpy.zeros(len(self.bins) - 1, dtype="int64")
+    else:
+      return numpy.histogram(observations, self.bins)[0]
   def accumulate(self, observations):
     if self.histogram is None:
-      self.histogram = numpy.histogram(observations, self.bins)[0]
+      self.histogram = self.flexible_histogram(observations)
     else:
-      self.histogram += numpy.histogram(observations, self.bins)[0]
+      self.histogram += self.flexible_histogram(observations)
   def reduce(self, other):
     self.histogram = other.histogram if self.histogram is None else self.histogram if other.histogram is None else self.histogram + other.histogram
   def result(self):
--- packages/slycat/analysis/worker/test_accumulators.py
+++ packages/slycat/analysis/worker/test_accumulators.py
@@ -3,16 +3,71 @@ import sys
 
 from slycat.analysis.worker.accumulator import *
 
-def test_min_basic_string():
-  m = minimum()
-  m.accumulate(numpy.array(["a", "b", "c"]))
-  numpy.testing.assert_equal(m.result(), "a")
+def test_average_basic_string():
+  a = average()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), None)
 
-def test_min_reduce_string():
-  m = minimum()
-  m.accumulate(numpy.array(["b", "c", "d"]))
-  m.accumulate(numpy.array(["a", "b", "c"]))
-  numpy.testing.assert_equal(m.result(), "a")
+def test_average_reduce_string():
+  a = average()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), None)
+
+def test_count_basic_string():
+  a = count()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), 3)
+
+def test_count_reduce_string():
+  a = count()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), 6)
+
+def test_distinct_basic_string():
+  a = distinct()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), 3)
+
+def test_distinct_reduce_string():
+  a = distinct()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), 4)
+
+def test_maximum_basic_string():
+  a = maximum()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), "c")
+
+def test_maximum_reduce_string():
+  a = maximum()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), "d")
+
+def test_minimum_basic_string():
+  a = minimum()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), "a")
+
+def test_minimum_reduce_string():
+  a = minimum()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), "a")
+
+def test_summation_basic_string():
+  a = summation()
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), None)
+
+def test_summation_reduce_string():
+  a = summation()
+  a.accumulate(numpy.array(["b", "c", "d"]))
+  a.accumulate(numpy.array(["a", "b", "c"]))
+  numpy.testing.assert_equal(a.result(), None)
 
 def test_histogram_basic():
   h = histogram(10)
@@ -20,12 +75,24 @@ def test_histogram_basic():
   numpy.testing.assert_equal(len(h.result()), 10)
 
 def test_histogram_reduce():
-  h1 = histogram(10)
+  h1 = histogram(bins=numpy.linspace(0, 1, 10))
   h1.accumulate(numpy.random.random(100))
-  h2 = histogram(10)
+  h2 = histogram(bins=numpy.linspace(0, 1, 10))
   h2.accumulate(numpy.random.random(100))
-  h3 = histogram(10)
+  h3 = histogram(bins=numpy.linspace(0, 1, 10))
   h3.reduce(h1)
   h3.reduce(h2)
-  numpy.testing.assert_equal(len(h3.result()), 10)
+  numpy.testing.assert_equal(len(h3.result()), 9)
   numpy.testing.assert_array_equal(h3.result(), h1.result() + h2.result())
+
+def test_histogram_reduce_string():
+  h1 = histogram(bins=numpy.linspace(0, 1, 10))
+  h1.accumulate(numpy.array(["1", "2", "3"]))
+  h2 = histogram(bins=numpy.linspace(0, 1, 10))
+  h2.accumulate(numpy.array(["a", "b", "c"]))
+  h3 = histogram(bins=numpy.linspace(0, 1, 10))
+  h3.reduce(h1)
+  h3.reduce(h2)
+  numpy.testing.assert_equal(len(h3.result()), 9)
+  numpy.testing.assert_array_equal(h3.result(), h1.result() + h2.result())
+  numpy.testing.assert_array_equal(h3.result(), numpy.zeros(9))
