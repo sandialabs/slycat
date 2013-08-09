4af5d8b1cd0a49a7e09e120a79f00418a56cea4c
Timothy M. Shead <tshead@sandia.gov>
2013-08-08 23:36:57 -0600

    Improve the schema sampling in the mongodb() operator.

--- packages/slycat/analysis/plugins/mongodb.py
+++ packages/slycat/analysis/plugins/mongodb.py
@@ -7,8 +7,8 @@ from __future__ import division
 def register_client_plugin(context):
   try:
     import pymongo
-    def mongodb(connection, database, collection, host="localhost", port=27017):
-      return connection.create_remote_array("mongodb", [], host, port, database, collection)
+    def mongodb(connection, database, collection, samples=(0, 1000), host="localhost", port=27017):
+      return connection.create_remote_array("mongodb", [], host, port, database, collection, samples)
     context.register_plugin_function("mongodb", mongodb)
   except:
     pass
@@ -20,7 +20,7 @@ def register_worker_plugin(context):
     import pymongo
     import slycat.analysis.worker
 
-    def mongodb(factory, worker_index, host, port, database, collection, samples=(0, 1000)):
+    def mongodb(factory, worker_index, host, port, database, collection, samples):
       return factory.pyro_register(mongodb_array(worker_index, host, port, database, collection, samples))
 
     class mongodb_array(slycat.analysis.worker.array):
@@ -34,7 +34,7 @@ def register_worker_plugin(context):
 
         self.record_count = None
         self.chunk_size = None
-        self._attributes = None
+        self.output_attributes = None
 
       def update_dimensions(self):
         if self.record_count is None:
@@ -44,16 +44,36 @@ def register_worker_plugin(context):
           self.chunk_size = int(numpy.ceil(self.record_count / self.worker_count))
 
       def update_attributes(self):
-        if self._attributes is None:
-          cursor = pymongo.MongoClient(self.host, self.port)[self.database][self.collection].find()
-          self._attributes = sorted(set(["_id"] + [key for document in cursor for key in document]))
+        if self.output_attributes is None:
+          keys = set()
+          self.output_attributes = []
+          for document in pymongo.MongoClient(self.host, self.port)[self.database][self.collection].find()[self.samples[0]:self.samples[1]]:
+            for key in document:
+              if key not in keys:
+                value = document[key]
+                slycat.analysis.worker.log.debug("%s %s %s", key, type(value), value)
+                if isinstance(value, basestring):
+                  keys.add(key)
+                  self.output_attributes.append({"name":key,"type":"string"})
+                elif isinstance(value, float):
+                  keys.add(key)
+                  self.output_attributes.append({"name":key,"type":"float64"})
+                elif isinstance(value, int):
+                  keys.add(key)
+                  self.output_attributes.append({"name":key,"type":"int64"})
+                elif isinstance(value, long):
+                  keys.add(key)
+                  self.output_attributes.append({"name":key,"type":"int64"})
+                elif isinstance(value, bool):
+                  keys.add(key)
+                  self.output_attributes.append({"name":key,"type":"bool"})
 
       def dimensions(self):
         self.update_dimensions()
         return [{"name":"i", "type":"int64", "begin":0, "end":self.record_count, "chunk-size":self.chunk_size}]
       def attributes(self):
         self.update_attributes()
-        return [{"name":key, "type":"string"} for key in self._attributes]
+        return self.output_attributes
       def iterator(self):
         self.update_attributes()
         self.update_dimensions()
@@ -71,19 +91,19 @@ def register_worker_plugin(context):
         if self.chunk_count:
           raise StopIteration()
 
-        self._values = [[] for attribute in self.owner._attributes]
+        self.output_values = [[] for attribute in self.owner.output_attributes]
         for document in self.cursor:
-          for index, attribute in enumerate(self.owner._attributes):
-            self._values[index].append(document.get(attribute, ""))
+          for index, attribute in enumerate(self.owner.output_attributes):
+            self.output_values[index].append(document.get(attribute["name"], ""))
 
         self.chunk_count += 1
 
       def coordinates(self):
         return numpy.array([(self.chunk_count - 1) * self.owner.chunk_size], dtype="int64")
       def shape(self):
-        return numpy.array([len(self._values[0])], dtype="int64")
-      def values(self, attribute):
-        return numpy.ma.array(self._values[attribute])
+        return numpy.array([len(self.output_values[0])], dtype="int64")
+      def values(self, index):
+        return numpy.ma.array(self.output_values[index], dtype=self.owner.output_attributes[index]["type"])
     context.register_plugin_function("mongodb", mongodb)
   except:
     pass
