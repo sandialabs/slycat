0b28b6660b17c0a3a428f9f0f2c4cf50182b5590
Timothy M. Shead <tshead@sandia.gov>
2013-08-14 09:32:29 -0600

    Checkpoint work on the prn_files() loader.  See #8.

--- packages/slycat/analysis/plugins/prn_files.py
+++ packages/slycat/analysis/plugins/prn_files.py
@@ -31,6 +31,7 @@ def register_worker_plugin(context):
   import numpy
   import os
   import Pyro4
+  import re
 
   import slycat.analysis.plugin.worker
 
@@ -41,48 +42,80 @@ def register_worker_plugin(context):
     def __init__(self, worker_index, paths, chunk_size):
       slycat.analysis.plugin.worker.array.__init__(self, worker_index)
       self.paths = paths
-      self.line_count = None
       self.chunk_size = chunk_size
+      self.record_count = None
       self.output_attributes = None
 
-    def local_line_count(self):
-      line_count = 0
+    def local_record_count(self):
+      record_count = 0
       for index, path in enumerate(self.paths):
         if (index % self.worker_count) != self.worker_index:
           continue
         with open(path, "r") as stream:
           for line in stream:
-            line_count += 1
-          if line.strip() == "End of Xyce(TM) Simulation":
-            line_count -= 1
-          line_count -= 1 # Skip the header
-      return line_count
+            record_count += 1
+          if re.search("End\sof\sXyce(TM)\sSimulation", line) is not None:
+            record_count -= 1
+          record_count -= 1 # Skip the header
+      return record_count
 
-    def update_metrics(self):
+    def update_dimensions(self):
       # Count the number of lines in the file.
-      if self.line_count is None:
-        local_line_counts = [Pyro4.async(sibling).local_line_count() for sibling in self.siblings]
-        local_line_counts = [local_line_count.value for local_line_count in local_line_counts]
-        self.line_count = sum(local_line_counts)
+      if self.record_count is None:
+        local_record_counts = [Pyro4.async(sibling).local_record_count() for sibling in self.siblings]
+        local_record_counts = [local_record_count.value for local_record_count in local_record_counts]
+        self.record_count = sum(local_record_counts)
 
       # If the caller didn't specify a chunk size, split the file evenly among workers.
       if self.chunk_size is None:
-        self.chunk_size = int(numpy.ceil(self.line_count / self.worker_count))
+        self.chunk_size = int(numpy.ceil(self.record_count / self.worker_count))
+
+    def local_attributes(self):
+      unique_attributes = set()
+      attributes = []
+      for index, path in enumerate(self.paths):
+        if (index % self.worker_count) != self.worker_index:
+          continue
+        with open(path, "r") as stream:
+          line = stream.next()
+          for name in line.split():
+            if name not in unique_attributes:
+              unique_attributes.add(name)
+              attributes.append(name)
+      return attributes
+
+    def update_attributes(self):
+      if self.output_attributes is None:
+        local_attributes_list = [Pyro4.async(sibling).local_attributes() for sibling in self.siblings]
+        local_attributes_list = [local_attributes.value for local_attributes in local_attributes_list]
+        unique_global_attributes = set()
+        global_attributes = []
+        for local_attributes in local_attributes_list:
+          for name in local_attributes:
+            if name not in unique_global_attributes:
+              unique_global_attributes.add(name)
+              global_attributes.append(name)
+        self.output_attributes = [{"name":name, "type":"int64" if name == "Index" else "float64"} for name in global_attributes]
 
     def dimensions(self):
-      self.update_metrics()
-      return [{"name":"i", "type":"int64", "begin":0, "end":self.line_count, "chunk-size":self.chunk_size}]
+      self.update_dimensions()
+      return [{"name":"i", "type":"int64", "begin":0, "end":self.record_count, "chunk-size":self.chunk_size}]
+
     def attributes(self):
-      with open(self.path, "r") as stream:
-        line = stream.next()
-        return [{"name":name, "type":"int64" if name == "Index" else "float64"} for name in line.split()]
+      self.update_attributes()
+      return self.output_attributes
+
     def iterator(self):
-      self.update_metrics()
+      self.update_dimensions()
+      self.update_attributes()
       return self.pyro_register(prn_files_array_iterator(self))
+
     def file_path(self):
       return self.paths
+
     def local_file_size(self):
       return sum([os.stat(path).st_size for index, path in enumerate(self.paths) if index % self.worker_count == self.worker_index])
+
     def file_size(self):
       local_file_sizes = [Pyro4.async(sibling).local_file_size() for sibling in self.siblings]
       local_file_sizes = [local_file_size.value for local_file_size in local_file_sizes]
@@ -95,6 +128,7 @@ def register_worker_plugin(context):
       self.stream.next() # Skip the header
       self.chunk_id = -1
       self.lines = []
+
     def next(self):
       self.lines = []
       self.chunk_id += 1
@@ -117,13 +151,17 @@ def register_worker_plugin(context):
         pass
       if not len(self.lines):
         raise StopIteration()
+
     def coordinates(self):
       return numpy.array([self.chunk_id * self.owner.chunk_size], dtype="int64")
+
     def shape(self):
       return numpy.array([len(self.lines)], dtype="int64")
+
     def values(self, attribute):
       if attribute == 0: # Index
         return numpy.ma.array([int(line[attribute]) for line in self.lines], dtype="int64")
       else:
         return numpy.ma.array([float(line[attribute]) for line in self.lines], dtype="float64")
+
   context.register_plugin_function("prn_files", prn_files)
