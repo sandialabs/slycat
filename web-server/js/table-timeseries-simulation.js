6b1154d3c635079f3f5f2aca8b850164813d51e4
Alex Milosz Sielicki <alex@fostermilo.com>
2013-08-19 15:56:06 -0600

    Refactored color map switching functionality for easier reuse with cca.

--- web-server/js/table-timeseries-simulation.js
+++ web-server/js/table-timeseries-simulation.js
@@ -11,6 +11,7 @@ function timeseries_simulation_table(parameters, server_root, workerId)
   this.sort_order_callbacks = [];
   this.waveform_indexes = [];
   var bookmarker = parameters.bookmarker;
+  var colorMapper = parameters.colorMapper;
   this.cluster_name = parameters.cluster_name;
   this.cluster_index = parameters.cluster_index;
 
@@ -34,127 +35,6 @@ function timeseries_simulation_table(parameters, server_root, workerId)
   var column_max = data_table_metadata["column-max"];
   var column_min = data_table_metadata["column-min"];
   var column_names = data_table_metadata["column-names"];
-
-  var colorMaps = {
-    // This is meant to be used against a black background
-    "nightcolormap": {
-      "scalar": [0.0, 0.03125, 0.0625, 0.09375, 0.125, 0.15625, 0.1875, 0.21875, 0.25, 0.28125, 0.3125, 0.34375, 0.375, 0.40625, 0.4375, 0.46875, 0.5, 0.53125, 0.5625, 0.59375, 0.625, 0.65625, 0.6875, 0.71875, 0.75, 0.78125, 0.8125, 0.84375, 0.875, 0.90625, 0.9375, 0.96875, 1.0,],
-      "RGBs"  : [
-                  d3.rgb( 59,  76, 192),
-                  d3.rgb( 68,  90, 204),
-                  d3.rgb( 77, 104, 215),
-                  d3.rgb( 87, 117, 225),
-                  d3.rgb( 98, 130, 234),
-                  d3.rgb(108, 142, 241),
-                  d3.rgb(119, 154, 247),
-                  d3.rgb(130, 165, 251),
-                  d3.rgb(141, 176, 254),
-                  d3.rgb(152, 185, 255),
-                  d3.rgb(163, 194, 255),
-                  d3.rgb(174, 201, 253),
-                  d3.rgb(184, 208, 249),
-                  d3.rgb(194, 213, 244),
-                  d3.rgb(204, 217, 238),
-                  d3.rgb(213, 219, 230),
-                  d3.rgb(221, 221, 221),
-                  d3.rgb(229, 216, 209),
-                  d3.rgb(236, 211, 197),
-                  d3.rgb(241, 204, 185),
-                  d3.rgb(245, 196, 173),
-                  d3.rgb(247, 187, 160),
-                  d3.rgb(247, 177, 148),
-                  d3.rgb(247, 166, 135),
-                  d3.rgb(244, 154, 123),
-                  d3.rgb(241, 141, 111),
-                  d3.rgb(236, 127,  99),
-                  d3.rgb(229, 112,  88),
-                  d3.rgb(222,  96,  77),
-                  d3.rgb(213,  80,  66),
-                  d3.rgb(203,  62,  56),
-                  d3.rgb(192,  40,  47),
-                  d3.rgb(180,   4,  38),
-                ],
-      "className": "nightMode",
-    },
-    // This is used against a white background
-    "daycolormap": {
-      "scalar": [0,0.03125,0.0625,0.09375,0.125,0.15625,0.1875,0.21875,0.25,0.28125,0.3125,0.34375,0.375,0.40625,0.4375,0.46875,0.5,0.53125,0.5625,0.59375,0.625,0.65625,0.6875,0.71875,0.75,0.78125,0.8125,0.84375,0.875,0.90625,0.9375,0.96875,1,],
-      "RGBs"  : [
-                  d3.rgb(100, 108, 234),
-                  d3.rgb(115, 118, 240),
-                  d3.rgb(128, 128, 244),
-                  d3.rgb(140, 138, 248),
-                  d3.rgb(151, 147, 250),
-                  d3.rgb(161, 155, 251),
-                  d3.rgb(169, 163, 251),
-                  d3.rgb(177, 170, 250),
-                  d3.rgb(184, 177, 248),
-                  d3.rgb(189, 182, 245),
-                  d3.rgb(193, 187, 241),
-                  d3.rgb(197, 191, 236),
-                  d3.rgb(199, 194, 230),
-                  d3.rgb(200, 196, 224),
-                  d3.rgb(201, 198, 216),
-                  d3.rgb(200, 199, 208),
-                  d3.rgb(198, 198, 198),
-                  d3.rgb(210, 197, 195),
-                  d3.rgb(220, 194, 192),
-                  d3.rgb(229, 191, 187),
-                  d3.rgb(236, 186, 181),
-                  d3.rgb(243, 181, 175),
-                  d3.rgb(248, 175, 168),
-                  d3.rgb(251, 168, 160),
-                  d3.rgb(254, 159, 152),
-                  d3.rgb(255, 150, 143),
-                  d3.rgb(255, 140, 133),
-                  d3.rgb(253, 129, 123),
-                  d3.rgb(250, 117, 112),
-                  d3.rgb(246, 105, 101),
-                  d3.rgb(240,  91,  90),
-                  d3.rgb(233,  75,  78),
-                  d3.rgb(225,  57,  66),
-                ],
-      "className": "dayMode",
-    },
-  }
-
-  // These are unused for now. They are just slightly lighter than the ones that we use agains a black background
-  // var KenColorMapScalar2 = [0,0.03125,0.0625,0.09375,0.125,0.15625,0.1875,0.21875,0.25,0.28125,0.3125,0.34375,0.375,0.40625,0.4375,0.46875,0.5,0.53125,0.5625,0.59375,0.625,0.65625,0.6875,0.71875,0.75,0.78125,0.8125,0.84375,0.875,0.90625,0.9375,0.96875,1,];
-  // var KenColorMapRGBs2   = [
-  //                           d3.rgb( 55,  69, 171),
-  //                           d3.rgb( 63,  81, 182),
-  //                           d3.rgb( 70,  93, 192),
-  //                           d3.rgb( 79, 105, 201),
-  //                           d3.rgb( 87, 117, 209),
-  //                           d3.rgb( 96, 128, 215),
-  //                           d3.rgb(106, 138, 220),
-  //                           d3.rgb(115, 148, 224),
-  //                           d3.rgb(125, 158, 227),
-  //                           d3.rgb(135, 166, 228),
-  //                           d3.rgb(145, 174, 228),
-  //                           d3.rgb(155, 181, 226),
-  //                           d3.rgb(164, 186, 223),
-  //                           d3.rgb(174, 191, 219),
-  //                           d3.rgb(182, 195, 213),
-  //                           d3.rgb(191, 197, 206),
-  //                           d3.rgb(198, 198, 198),
-  //                           d3.rgb(205, 195, 188),
-  //                           d3.rgb(211, 190, 177),
-  //                           d3.rgb(215, 184, 166),
-  //                           d3.rgb(219, 176, 155),
-  //                           d3.rgb(220, 168, 143),
-  //                           d3.rgb(221, 159, 132),
-  //                           d3.rgb(220, 149, 121),
-  //                           d3.rgb(218, 138, 110),
-  //                           d3.rgb(215, 127,  99),
-  //                           d3.rgb(210, 114,  88),
-  //                           d3.rgb(204, 101,  78),
-  //                           d3.rgb(198,  87,  69),
-  //                           d3.rgb(190,  73,  59),
-  //                           d3.rgb(181,  57,  51),
-  //                           d3.rgb(171,  39,  43),
-  //                           d3.rgb(161,  13,  35),
-  //                          ];
 
   // Setup SlickGrid columns
   var columns = [];
@@ -197,27 +77,10 @@ function timeseries_simulation_table(parameters, server_root, workerId)
   if (parameters.colormap != null) {
     default_color_map = parameters.colormap;
     // Set up the background class for the waveform viewer
-    parameters.waveform_viewer.container.attr("class", colorMaps[default_color_map].className);
-
+    parameters.waveform_viewer.container.attr("class", colorMapper.getClassName(default_color_map));
   }
 
-  setUpColorMapsForAllColumns(default_color_map);
-
-  function setUpColorMapsForAllColumns(colorMapName) {
-    for(var j = 0; j != columns.length; ++j) {
-      columns[j].colorMap = createColorMap( columns[j].columnMin, columns[j].columnMax, colorMaps[colorMapName].scalar, colorMaps[colorMapName].RGBs );
-    }
-  }
-
-  function createColorMap(min, max, scalar, rgb) {
-    var range = max - min;
-    var domain = [];
-    for(i=0; i < scalar.length; i++) {
-      domain.push( (range * scalar[i]) + min );
-    }
-    var colorMap = d3.scale.linear().domain( domain ).range( rgb );
-    return colorMap;
-  }
+  colorMapper.setUpColorMapsForAllColumns(default_color_map, columns);
 
   // Initialize SlickGrid
   var grid;
@@ -430,7 +293,7 @@ function timeseries_simulation_table(parameters, server_root, workerId)
 
   this.updateColorMap = function(colorMapName) {
     // Swap in the new color map
-    setUpColorMapsForAllColumns(colorMapName);
+    colorMapper.setUpColorMapsForAllColumns(colorMapName, columns);
     // Update grid with new color map and make it show the new colors
     if( grid ) {
       grid.setColumns(columns);
@@ -439,7 +302,7 @@ function timeseries_simulation_table(parameters, server_root, workerId)
       grid.render();
     }
     // Set up the background class for the waveform viewer
-    parameters.waveform_viewer.container.attr("class", colorMaps[colorMapName].className);
+    parameters.waveform_viewer.container.attr("class", colorMapper.getClassName(colorMapName));
     // Change the colors of waveforms in the viewer and dendrogram
     self.setWaveformColorsPerSelectedColumn();
   }
