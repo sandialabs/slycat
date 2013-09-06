bac86e45db767058ac4963c1ca7bfaafb1cb3250
Timothy M. Shead <tshead@sandia.gov>
2013-09-06 09:20:54 -0600

    Add some command-line options to our CCA test scripts.

--- packages/slycat/web/server/test-cca-homegrown.py
+++ packages/slycat/web/server/test-cca-homegrown.py
@@ -2,26 +2,29 @@ from slycat.analysis.client import *
 from slycat.web.server.cca import cca
 import matplotlib.pyplot as pyplot
 import numpy
+import optparse
 import sys
 
-input_columns = ["Cylinders", "Displacement", "Weight", "Year"]
-output_columns = ["Acceleration", "Horsepower", "MPG"]
+parser = optparse.OptionParser()
+parser.add_option("--inputs", default="Cylinders Displacement Weight Year", help="Input variables.  Default: %default")
+parser.add_option("--outputs", default="Acceleration Horsepower MPG", help="Output variables.  Default: %default")
+parser.add_option("--scale", default=False, action="store_true", help="Scale inputs to unit variance.")
+options, arguments = parser.parse_args()
 
 autos = load("../data/cars.csv", "csv-file", chunk_size=100)
-inputs = project(autos, *input_columns)
-outputs = project(autos, *output_columns)
-X = numpy.array([values(inputs, column) for column in input_columns]).T.astype("double")
-Y = numpy.array([values(outputs, column) for column in output_columns]).T.astype("double")
+inputs = project(autos, *options.inputs.split())
+outputs = project(autos, *options.outputs.split())
+X = numpy.array([values(inputs, column) for column in options.inputs.split()]).T.astype("double")
+Y = numpy.array([values(outputs, column) for column in options.outputs.split()]).T.astype("double")
 good = ~numpy.isnan(Y).any(axis=1)
 X = X[good]
 Y = Y[good]
 
-x, y, x_loadings, y_loadings, r, wilks = cca(X, Y, scale_inputs=True)
+x, y, x_loadings, y_loadings, r, wilks = cca(X, Y, scale_inputs=options.scale)
 
-print r
-print wilks
-print x_loadings
-print y_loadings
+sys.stderr.write("r^2:\n%s\n" % r)
+sys.stderr.write("x loadings:\n%s\n" % x_loadings)
+sys.stderr.write("y loadings:\n%s\n" % y_loadings)
 
 pyplot.figure()
 pyplot.scatter(x.T[0], y.T[0], color="red")
