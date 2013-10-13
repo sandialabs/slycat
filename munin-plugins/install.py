8bfa023b451d07c001033b4a253a43c3d245ee72
Timothy M. Shead <tshead@sandia.gov>
2013-10-13 09:22:24 -0600

    First iteration of fully-functional couchdb plugins for munin.

--- munin-plugins/install.py
+++ munin-plugins/install.py
@@ -7,7 +7,7 @@ enabled_plugins = "/etc/munin/plugins"
 plugin_storage = "/usr/share/munin/plugins"
 context = "system_u:object_r:munin_unconfined_plugin_exec_t:s0"
 
-for plugin in ["couchdb-availability", "couchdb-database", "couchdb-requests"]:
+for plugin in ["couchdb-availability", "couchdb-database", "couchdb-request-times"]:
   subprocess.check_call(["cp", plugin, plugin_storage])
   subprocess.check_call(["chcon", context, os.path.join(plugin_storage, plugin)])
   subprocess.check_call(["ln", "-sf", os.path.join(plugin_storage, plugin), os.path.join(enabled_plugins, plugin)])
