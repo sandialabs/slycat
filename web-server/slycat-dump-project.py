8287fc25e9b0c912ec57847128d65c3e3b0d7ed8
Timothy M. Shead <tshead@sandia.gov>
2013-10-15 13:50:22 -0600

    Flatten the dump file hierarchy, eliminate duplicate files, and allow multiple projects in a dump directory.

--- web-server/slycat-dump-project.py
+++ web-server/slycat-dump-project.py
@@ -17,12 +17,10 @@ parser.add_argument("--couchdb-port", type=int, default=5984, help="CouchDB port
 parser.add_argument("--couchdb-database", default="slycat", help="CouchDB database.  Default: %(default)s")
 parser.add_argument("--iquery", default="/opt/scidb/13.3/bin/iquery", help="SciDB iquery executable.  Default: %(default)s")
 parser.add_argument("--force", action="store_true", help="Overwrite existing data.")
-parser.add_argument("--project-id", help="Project ID to dump.")
+parser.add_argument("--project-id", default=[], action="append", help="Project ID to dump.  You may specify --project-id multiple times.")
 parser.add_argument("--output-dir", help="Directory for storing results.")
 arguments = parser.parse_args()
 
-if arguments.project_id is None:
-  raise Exception("--project-id is required.")
 if arguments.output_dir is None:
   raise Exception("--output-dir is required.")
 
@@ -39,35 +37,39 @@ coordinator_dir = StringIO.StringIO(stdout).readlines()[1].split(",")[-1].strip(
 
 os.makedirs(arguments.output_dir)
 
-project = couchdb[arguments.project_id]
-project_dir = arguments.output_dir
-json.dump(project, open(os.path.join(project_dir, "project.json"), "w"))
+for project_id in arguments.project_id:
+  project = couchdb[project_id]
+  json.dump(project, open(os.path.join(arguments.output_dir, "project-%s.json" % project["_id"]), "w"))
 
-for row in couchdb.view("slycat/project-models", startkey=arguments.project_id, endkey=arguments.project_id):
-  model = couchdb[row["id"]]
-  model_dir = os.path.join(project_dir, "model-%s" % model["_id"])
-  os.mkdir(model_dir)
-  json.dump(model, open(os.path.join(model_dir, "model.json"), "w"))
+  project_arrays = set()
 
-  for key, value in model.items():
-    if not key.startswith("artifact:"):
-      continue
-    if not isinstance(value, dict):
-      continue
-    for role, array in value.items():
-      iquery = subprocess.Popen([arguments.iquery, "-o", "csv", "-aq", "show(%s)" % array], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-      stdout, stderr = iquery.communicate()
-      schema = StringIO.StringIO(stdout).readlines()[1].strip()[1:-1]
-      attributes = re.search(r"<(.*)>", schema).group(1).split(",")
-      dimensions = re.search(r"\[.*\]", schema).group(0)
+  for row in couchdb.view("slycat/project-models", startkey=project_id, endkey=project_id):
+    model = couchdb[row["id"]]
+    json.dump(model, open(os.path.join(arguments.output_dir, "model-%s.json" % model["_id"]), "w"))
 
-      open(os.path.join(model_dir, "%s.schema" % array), "w").write(schema)
+    for key, value in model.items():
+      if not key.startswith("artifact:"):
+        continue
+      if not isinstance(value, dict):
+        continue
+      for role, array in value.items():
+        if array in project_arrays:
+          continue
+        project_arrays.add(array)
 
-      for index, offset in enumerate(range(0, len(attributes), 250)):
-        subattributes = attributes[offset : offset + 250]
-        subschema = "%s%s<%s>%s" % (array, index, ",".join(subattributes), dimensions)
+        iquery = subprocess.Popen([arguments.iquery, "-o", "csv", "-aq", "show(%s)" % array], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = iquery.communicate()
+        schema = StringIO.StringIO(stdout).readlines()[1].strip()[1:-1]
+        attributes = re.search(r"<(.*)>", schema).group(1).split(",")
+        dimensions = re.search(r"\[.*\]", schema).group(0)
 
-        open(os.path.join(model_dir, "%s-%s.schema" % (array, index)), "w").write(subschema)
+        open(os.path.join(arguments.output_dir, "array-%s.schema" % array), "w").write(schema)
 
-        subprocess.check_call([arguments.iquery, "-n", "-aq", "save(project(%s, %s), '%s-%s.opaque', -2, 'OPAQUE')" % (array, ",".join([attribute.split(":")[0] for attribute in subattributes]), array, index)])
-        subprocess.check_call(["mv", os.path.join(coordinator_dir, "%s-%s.opaque" % (array, index)), os.path.join(model_dir, "%s-%s.opaque" % (array, index))])
+        for index, offset in enumerate(range(0, len(attributes), 250)):
+          subattributes = attributes[offset : offset + 250]
+          subschema = "%s%s<%s>%s" % (array, index, ",".join(subattributes), dimensions)
+
+          open(os.path.join(arguments.output_dir, "array-%s-%s.schema" % (array, index)), "w").write(subschema)
+
+          subprocess.check_call([arguments.iquery, "-n", "-aq", "save(project(%s, %s), 'temp.opaque', -2, 'OPAQUE')" % (array, ",".join([attribute.split(":")[0] for attribute in subattributes]))])
+          subprocess.check_call(["mv", os.path.join(coordinator_dir, "temp.opaque"), os.path.join(arguments.output_dir, "array-%s-%s.opaque" % (array, index))])
