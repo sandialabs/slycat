1f86161e5a26811ff5cb11c64be60d0139a77fc6
Timothy M. Shead <tshead@sandia.gov>
2013-11-08 13:02:38 -0700

    Checkpoint work on the hdf5 stress tests.

--- web-server/slycat-hdf5-timeseries-stress-test.py
+++ web-server/slycat-hdf5-timeseries-stress-test.py
@@ -4,7 +4,8 @@ import numpy
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", default="test.hdf5", help="Output file.  Default: %(default)s")
+parser.add_argument("--file", default="test.hdf5", help="Test file.  Default: %(default)s")
+parser.add_argument("--timings", default="times.npy", help="Timings file.  Default: %(default)s")
 parser.add_argument("--sample-count", type=int, default=1250000, help="Timeseries sample count.  Default: %(default)s")
 parser.add_argument("--series-count", type=int, default=1000, help="Timeseries count.  Default: %(default)s")
 parser.add_argument("--variable-count", type=int, default=100, help="Timeseries variable count.  Default: %(default)s")
@@ -21,4 +22,4 @@ for attribute_index in range(arguments.variable_count):
     attribute[offset : offset + arguments.sample_count] = numpy.random.random(arguments.sample_count)
     chunk_times.append(time.time() - start)
 
-numpy.save("times.npy", numpy.array(chunk_times))
+numpy.save(arguments.timings, numpy.array(chunk_times))
