3fa78de26988229fc22a6e8ab95f4da45c0ce31d
Timothy M. Shead <tshead@sandia.gov>
2014-01-22 09:23:18 -0700

    Update the sampling strategies in slycat-hdf5-timeseries-sampling.py

--- web-client/slycat-hdf5-timeseries-sampling.py
+++ web-client/slycat-hdf5-timeseries-sampling.py
@@ -11,11 +11,13 @@ This script loads data from a directory containing:
     One timeseries-N.hdf5 file for each row in the input table.
 """
 
+from __future__ import division
+
 import argparse
 import matplotlib.pyplot as pyplot
 import numpy
-import scipy.interpolate
 import os
+import scipy.signal
 import slycat.data.hdf5
 
 parser = argparse.ArgumentParser()
@@ -30,32 +32,42 @@ with slycat.data.hdf5.open(os.path.join(arguments.directory, "timeseries-%s.hdf5
   times = slycat.data.hdf5.get_array_attribute(file, 0, 0)[...]
   values = slycat.data.hdf5.get_array_attribute(file, 0, arguments.variable + 1)[...]
 
-print "Loaded %s samples." % len(values)
-
 # Compute uniform bins, based on the min and max times of the original data.
 bin_times, bin_width = numpy.linspace(times.min(), times.max(), arguments.bin_count, retstep=True)
 
-# Plot the original data.
+# Plot Uniform PLA.
+pyplot.figure()
 pyplot.plot(times, values, "-", color=(0.7, 0.7, 0.7), label="Timeseries %s variable %s" % (arguments.timeseries, arguments.variable))
+pyplot.plot(bin_times, numpy.interp(bin_times, times, values), label="Uniform PLA")
+pyplot.legend(loc="best")
+print "Uniform PLA samples: %s -> %s %s%% reduction" % (len(values), len(bin_times), 100 * (1 - len(bin_times) / len(values)))
 
-# Plot our naive rebinning.
+# Plot improved PAA.
 bin_edges = numpy.append(bin_times - (bin_width / 2), [bin_times[-1] + (bin_width / 2)])
 bin_indices = numpy.digitize(times, bin_times) - 1
 bin_counts = numpy.bincount(bin_indices)
 bin_sums = numpy.bincount(bin_indices, values)
+lonely_bins = (bin_counts < 2)
+bin_counts[lonely_bins] = 1
+bin_sums[lonely_bins] = numpy.interp(bin_times, times, values)[lonely_bins]
 bin_values = bin_sums / bin_counts
-pyplot.plot(bin_times, bin_values, "-", label="naive")
 
-pyplot.plot(bin_times, scipy.interpolate.griddata(times, values, bin_times, method="nearest"), "-", label="griddata-nearest")
-#pyplot.plot(bin_times, scipy.interpolate.griddata(times, values, bin_times, method="linear"), "-", label="griddata-linear")
-#pyplot.plot(bin_times, scipy.interpolate.griddata(times, values, bin_times, method="cubic"), "-", label="griddata-cubic")
-
-#rbf = scipy.interpolate.Rbf(times, values, function="linear")
-#pyplot.plot(bin_times, rbf(bin_times), "-", label="rbf")
-
+pyplot.figure()
+pyplot.plot(times, values, "-", color=(0.7, 0.7, 0.7), label="Timeseries %s variable %s" % (arguments.timeseries, arguments.variable))
+pyplot.plot(bin_times, bin_values, "-", label="Improved PAA")
 pyplot.legend(loc="best")
-pyplot.xlabel("Time")
-pyplot.ylabel("Value")
-pyplot.title(arguments.directory)
+print "Improved PAA samples: %s -> %s %s%% reduction" % (len(values), len(bin_times), 100 * (1 - len(bin_times) / len(values)))
+
+# Plot feature-based PLA.
+pyplot.figure()
+pyplot.plot(times, values, "-", color=(0.7, 0.7, 0.7), label="Timeseries %s variable %s" % (arguments.timeseries, arguments.variable))
+feature_times = numpy.unique(numpy.concatenate((times[:1], times[-1:], times[scipy.signal.argrelmin(values)[0].astype("int64")], times[scipy.signal.argrelmax(values)[0].astype("int64")])))
+pyplot.plot(feature_times, numpy.interp(feature_times, times, values), label="Feature PLA")
+pyplot.legend(loc="best")
+print "Feature PLA samples: %s -> %s %s%% reduction" % (len(values), len(feature_times), 100 * (1 - len(feature_times) / len(values)))
+
+#pyplot.xlabel("Time")
+#pyplot.ylabel("Value")
+#pyplot.title(arguments.directory)
 pyplot.show()
 
