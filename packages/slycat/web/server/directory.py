69f184380455668840cd672db8938fca715b4c66
Warren Hunt <wlhunt@sandia.gov>
2014-03-13 13:18:54 -0600

    adding ldap bind params

--- packages/slycat/web/server/directory.py
+++ packages/slycat/web/server/directory.py
@@ -27,9 +27,11 @@ class identity(prototype):
 
 class ldap(prototype):
   """Directory implementation that uses LDAP to perform user lookups."""
-  def __init__(self, server, dn):
+  def __init__(self, server, dn, w="", c=""):
     self.server = server
     self.dn = dn
+    self.who = w     #the bindDN for the query
+    self.cred = c    #credential associated with bindDN
     self.cache = {}
 
   def user(self, uid):
@@ -38,8 +40,10 @@ class ldap(prototype):
         import ldap
         ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
         connection = ldap.initialize(self.server)
-        connection.simple_bind_s()
-        entry = connection.search_s(self.dn % uid, ldap.SCOPE_BASE)[0][1]
+        connection.simple_bind_s(self.who, self.cred)
+        result = connection.search_s(self.dn % uid, ldap.SCOPE_BASE)
+        if len(result) < 1: raise Exception("Improper LDAP Query")
+        entry = result[0][1]
         self.cache[uid] = {
           "name" : entry["cn"][0],
           "email" : "%s@%s" % (uid, entry["esnAdministrativeDomainName"][0]),
