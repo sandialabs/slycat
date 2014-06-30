aaded1df2426f891dcad3ef75e6cb47fa2c58400
Alex Milosz Sielicki <alex@techmilo.com>
2014-06-30 14:14:38 -0600

    Making scatterplot respond to drop downs

--- web-server/js/variable-switcher.js
+++ web-server/js/variable-switcher.js
@@ -26,6 +26,10 @@ $.widget("parameter_image.variableswitcher",
       .appendTo(this.element)
       ;
     this.x_select = $("<select id='x-axis-switcher' name='x-axis-switcher' />")
+      .change(function(){
+        self.element.trigger("x-selection-changed", this.value);
+        console.log("x change to " + this.value);
+      })
       .appendTo(this.element)
       ;
 
@@ -33,6 +37,10 @@ $.widget("parameter_image.variableswitcher",
       .appendTo(this.element)
       ;
     this.y_select = $("<select id='y-axis-switcher' name='y-axis-switcher' />")
+      .change(function(){
+        self.element.trigger("y-selection-changed", this.value);
+        console.log("y change to " + this.value);
+      })
       .appendTo(this.element)
       ;
 
@@ -40,6 +48,10 @@ $.widget("parameter_image.variableswitcher",
       .appendTo(this.element)
       ;
     this.images_select = $("<select id='images-switcher' name='images-switcher' />")
+      .change(function(){
+        self.element.trigger("images-selection-changed", this.value);
+        console.log("image change to " + this.value);
+      })
       .appendTo(this.element)
       ;
 
@@ -72,25 +84,49 @@ $.widget("parameter_image.variableswitcher",
 
   _set_x: function()
   { 
+    var self = this;
     this.x_select.empty();
     for(var i = 0; i < this.options.x_variables.length; i++) {
-      $("<option />").text(this.options.metadata['column-names'][this.options.x_variables[i]]).appendTo(this.x_select);
+      $("<option />")
+        .text(this.options.metadata['column-names'][this.options.x_variables[i]])
+        .attr("value", this.options.x_variables[i])
+        .attr("selected", function(){
+          return self.options["x-variable"] == self.options.x_variables[i] ? "selected" : false;
+        })
+        .appendTo(this.x_select)
+        ;
     }
   },
 
   _set_y: function()
   { 
+    var self = this;
     this.y_select.empty();
     for(var i = 0; i < this.options.y_variables.length; i++) {
-      $("<option />").text(this.options.metadata['column-names'][this.options.y_variables[i]]).appendTo(this.y_select);
+      $("<option />")
+        .text(this.options.metadata['column-names'][this.options.y_variables[i]])
+        .attr("value", this.options.y_variables[i])
+        .attr("selected", function(){
+          return self.options["y-variable"] == self.options.y_variables[i] ? "selected" : false;
+        })
+        .appendTo(this.y_select)
+        ;
     }
   },
 
   _set_images: function()
   { 
+    var self = this;
     this.images_select.empty();
     for(var i = 0; i < this.options.image_variables.length; i++) {
-      $("<option />").text(this.options.metadata['column-names'][this.options.image_variables[i]]).appendTo(this.images_select);
+      $("<option />")
+        .text(this.options.metadata['column-names'][this.options.image_variables[i]])
+        .attr("value", this.options.image_variables[i])
+        .attr("selected", function(){
+          return self.options["image-variable"] == self.options.image_variables[i] ? "selected" : false;
+        })
+        .appendTo(this.images_select)
+        ;
     }
   },
 
