3a21eee58de760e5ddbdfa5b92a12503fd476e06
Timothy M. Shead <tshead@sandia.gov>
2014-07-17 16:39:01 -0600

    First pass at remote parameter image model ingestion.

--- web-client/slycat-generate-parameter-image-model.py
+++ web-client/slycat-generate-parameter-image-model.py
@@ -2,24 +2,22 @@
 # DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 # rights in this software.
 
+import argparse
 import numpy
 import numpy.core.defchararray
 import os
-import slycat.web.client
 import PIL.Image, PIL.ImageDraw, PIL.ImageFont
 
-parser = slycat.web.client.option_parser()
+parser = argparse.ArgumentParser()
+parser.add_argument("--data-file", default="images.csv", help="Output data file.  Default: %(default)s")
 parser.add_argument("--image-count", type=int, default=3, help="Image column count.  Default: %(default)s")
 parser.add_argument("--image-directory", default="images", help="Image directory.  Default: %(default)s")
 parser.add_argument("--image-height", type=int, default=1000, help="Image height.  Default: %(default)s")
 parser.add_argument("--image-hostname", default="localhost", help="Image hostname.  Default: %(default)s")
 parser.add_argument("--image-width", type=int, default=1000, help="Image width.  Default: %(default)s")
 parser.add_argument("--input-count", type=int, default=3, help="Input column count.  Default: %(default)s")
-parser.add_argument("--marking", default="", help="Marking type.  Default: %(default)s")
 parser.add_argument("--metadata-count", type=int, default=3, help="Metadata column count.  Default: %(default)s")
-parser.add_argument("--model-name", default="Demo Parameter Image Model", help="New model name.  Default: %(default)s")
 parser.add_argument("--output-count", type=int, default=3, help="Output column count.  Default: %(default)s")
-parser.add_argument("--project-name", default="Demo Parameter Image Project", help="New project name.  Default: %(default)s")
 parser.add_argument("--row-count", type=int, default=100, help="Row count.  Default: %(default)s")
 parser.add_argument("--seed", type=int, default=12345, help="Random seed.  Default: %(default)s")
 parser.add_argument("--unused-count", type=int, default=3, help="Unused column count.  Default: %(default)s")
@@ -32,6 +30,14 @@ metadata_data = numpy.core.defchararray.add("metadata ", numpy.arange(arguments.
 image_data = numpy.core.defchararray.mod("file://%s%s/%%s.jpg" % (arguments.image_hostname, os.path.abspath(arguments.image_directory)), numpy.arange(arguments.row_count * arguments.image_count).reshape(arguments.row_count, arguments.image_count).astype("string"))
 string_data = numpy.column_stack((metadata_data, image_data))
 
+columns = [column for column in numeric_data.T] + [column for column in string_data.T]
+
+with open(arguments.data_file, "w") as file:
+  file.write(",".join(["n%s" % column for column in range(numeric_data.shape[1])] + ["s%s" % column for column in range(string_data.shape[1])]) + "\n")
+  for row in range(numeric_data.shape[0]):
+    file.write(",".join([str(column[row]) for column in columns]) + "\n")
+
+# Generate random images ...
 font = PIL.ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSerif-BoldItalic.ttf", 200)
 os.mkdir(arguments.image_directory)
 for index in numpy.arange(arguments.row_count * arguments.image_count):
@@ -40,42 +46,4 @@ for index in numpy.arange(arguments.row_count * arguments.image_count):
   draw.text((100, 100), "%s" % index, "black", font)
   image.save(os.path.join(arguments.image_directory, "%s.jpg" % index))
 
-# Setup a connection to the Slycat Web Server.
-connection = slycat.web.client.connect(arguments)
 
-# Create a new project to contain our model.
-pid = connection.find_or_create_project(arguments.project_name)
-
-# Create the new, empty model.
-mid = connection.create_model(pid, "parameter-image", arguments.model_name, arguments.marking)
-
-# Upload our observations as "data-table".
-connection.start_array_set(mid, "data-table")
-
-# Start our single "data-table" array.
-attributes = [("n%s" % column, "float64") for column in range(numeric_data.shape[1])] + [("s%s" % column, "string") for column in range(string_data.shape[1])]
-dimensions = [("row", "int64", 0, arguments.row_count)]
-connection.start_array(mid, "data-table", 0, attributes, dimensions)
-
-# Upload data into the array.
-index = 0
-for column in numeric_data.T:
-  connection.store_array_set_data(mid, "data-table", 0, index, data=column)
-  index += 1
-for column in string_data.T:
-  connection.store_array_set_data(mid, "data-table", 0, index, data=column)
-  index += 1
-
-# Store the remaining parameters.
-connection.store_parameter(mid, "input-columns", range(arguments.input_count))
-connection.store_parameter(mid, "output-columns", range(arguments.input_count, arguments.input_count + arguments.output_count))
-connection.store_parameter(mid, "image-columns", range(numeric_data.shape[1] + arguments.metadata_count, numeric_data.shape[1] + arguments.metadata_count + arguments.image_count))
-
-# Signal that we're done uploading data to the model.  This lets Slycat Web
-# Server know that it can start computation.
-connection.finish_model(mid)
-# Wait until the model is ready.
-connection.join_model(mid)
-
-# Supply the user with a direct link to the new model.
-slycat.web.client.log.info("Your new model is located at %s/models/%s" % (arguments.host, mid))
