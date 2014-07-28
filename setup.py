7c82ed3e13a9123f052f93e8ea742c688b6003f9
Timothy M. Shead <tshead@sandia.gov>
2014-07-28 15:14:52 -0600

    Stub-out a setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Copyright 2013, Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+
+from setuptools import setup, find_packages
+setup(
+  name = "slycat",
+  version = "1.1.0",
+  packages = find_packages(),
+)
