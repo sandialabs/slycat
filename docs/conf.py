41a9503fd3c58cdcaefb11d52a7a2c5996663709
Timothy M. Shead <tshead@sandia.gov>
2014-07-30 09:40:18 -0600

    Second attempt to stub-out binary dependencies when building documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,29 @@
 import sys
 import os
 
+class module_proxy(object):
+  __all__ = []
+
+  def __init__(self, *args, **kwargs):
+    pass
+
+  def __call__(self, *args, **kwargs):
+    return module_proxy()
+
+  @classmethod
+  def __getattr__(cls, name):
+    if name in ("__file__", "__path__"):
+        return "/dev/null"
+    elif name[0] == name[0].upper():
+      proxy_type = type(name, (), {})
+      proxy_type.__module__ = __name__
+      return proxy_type
+    else:
+      return module_proxy()
+
+for module_name in ["cherrypy", "couchdb", "h5py", "numpy", "paramiko", "pystache"]:
+  sys.modules[mod_name] = module_proxy()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
