4d5de41ccaf8788f9430cb59d80b344a8c1fe494
Charlie <cjsnide@sandia.gov>
2014-08-29 17:14:55 -0600

    ldap test - stubbed

--- tests/ldap_lookup_tests.py
+++ tests/ldap_lookup_tests.py
@@ -1,15 +1,21 @@
 import slycat.web.server.directory as d
-import nose
-import ConfigParser
+import nose.tools
+import mock
+import cherrypy
 
-config = ConfigParser.ConfigParser()
-config.read("tests/test-config.ini")
-l = d.ldap(config.get("slycat","ldap_query_server"), config.get("slycat","ldap_query_dn"))
+cherrypy.log.screen = False
+
+l = d.ldap("example.com", "example_dn")
+l.ldap_query = mock.Mock(return_value={'DefaultShell': ['/bin/bash'], 'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
 
 def test_uid_to_username_lookup():
-  nose.tools.assert_equal("cjsnide", l.uid_to_username("31776"))
+  l.ldap_query = mock.Mock(return_value={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
+  nose.tools.assert_equal("fooby", l.uid_to_username("31776"))
+  l.ldap_query = mock.Mock(return_value=None)
   nose.tools.assert_equal("317FOOBAR76", l.uid_to_username("317FOOBAR76"))
 
 def test_gid_to_username_lookup():
-  nose.tools.assert_equal("cjsnide", l.gid_to_username("31776"))
+  l.ldap_query = mock.Mock(return_value={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
+  nose.tools.assert_equal("fooby", l.gid_to_username("31776"))
+  l.ldap_query = mock.Mock(return_value=None)
   nose.tools.assert_equal("317FOOBAR76", l.gid_to_username("317FOOBAR76"))
