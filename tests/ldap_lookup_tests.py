883fe13749c6db54710e5d82c381791e30059889
Charlie <cjsnide@sandia.gov>
2014-09-02 17:14:06 -0600

    testing the user() method to refactor with ldap_query and we now have to mock a private method, so slight change of mocking

--- tests/ldap_lookup_tests.py
+++ tests/ldap_lookup_tests.py
@@ -6,16 +6,22 @@ import cherrypy
 cherrypy.log.screen = False
 
 l = d.ldap("example.com", "example_dn")
-l.ldap_query = mock.Mock(return_value={'DefaultShell': ['/bin/bash'], 'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
 
 def test_uid_to_username_lookup():
-  l.ldap_query = mock.Mock(return_value={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
-  nose.tools.assert_equal("fooby", l.uid_to_username("31776"))
-  l.ldap_query = mock.Mock(return_value=None)
-  nose.tools.assert_equal("317FOOBAR76", l.uid_to_username("317FOOBAR76"))
+  ret_val={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']}
+  with mock.patch.object(l, '_ldap__ldap_query', return_value=ret_val) as method:
+    nose.tools.assert_equal("fooby", l.uid_to_username("31776"))
+  with mock.patch.object(l, '_ldap__ldap_query', return_value=None) as method:
+    nose.tools.assert_equal("317FOOBAR76", l.uid_to_username("317FOOBAR76"))
 
 def test_gid_to_username_lookup():
-  l.ldap_query = mock.Mock(return_value={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']})
-  nose.tools.assert_equal("fooby", l.gid_to_username("31776"))
-  l.ldap_query = mock.Mock(return_value=None)
-  nose.tools.assert_equal("317FOOBAR76", l.gid_to_username("317FOOBAR76"))
+  ret_val={'displayName': ['FooBar FooFoo'], 'uid': ['fooby']}
+  with mock.patch.object(l, '_ldap__ldap_query', return_value=ret_val) as method:
+    nose.tools.assert_equal("fooby", l.gid_to_username("31776"))
+  with mock.patch.object(l, '_ldap__ldap_query', return_value=None) as method:
+    nose.tools.assert_equal("317FOOBAR76", l.gid_to_username("317FOOBAR76"))
+
+def test_user_query_lookup():
+  ret_val={'displayName': ['FooBar FooFoo'], 'uid': ['fooby'], 'cn': ['my_cn'], 'esnAdministrativeDomainName': ["example.com"], 'memberOf': ["role1","role2"]}
+  with mock.patch.object(l, '_ldap__ldap_query', return_value=ret_val) as method:
+    nose.tools.assert_equal({"name": "my_cn", "email": "fooby@example.com", "roles": ["role1","role2"]}, l.user("fooby"))
