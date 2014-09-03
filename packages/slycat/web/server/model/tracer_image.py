bff06a545cbc77324d829d7ca1cc42f6ded73faa
Charlie <cjsnide@sandia.gov>
2014-09-03 17:57:04 -0600

    new tracer image model files

---
+++ packages/slycat/web/server/model/tracer_image.py
@@ -0,0 +1,31 @@
+# Copyright 2013, Sandia Corporation. Under the terms of Contract
+# DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+# rights in this software.
+
+from slycat.web.server.model import *
+
+import cherrypy
+import datetime
+import slycat.web.server.database.couchdb
+import threading
+import traceback
+
+def compute(mid):
+  try:
+    database = slycat.web.server.database.couchdb.connect()
+    model = database.get("model", mid)
+
+    # Do useful work here
+
+    update(database, model, state="finished", result="succeeded", finished=datetime.datetime.utcnow().isoformat(), progress=1.0, message="")
+
+  except:
+    cherrypy.log.error("%s" % traceback.format_exc())
+
+    database = slycat.web.server.database.couchdb.connect()
+    model = database.get("model", mid)
+    update(database, model, state="finished", result="failed", finished=datetime.datetime.utcnow().isoformat(), message=traceback.format_exc())
+
+def finish(database, model):
+  thread = threading.Thread(name="Compute Parameter Image Model", target=compute, kwargs={"mid" : model["_id"]})
+  thread.start()
