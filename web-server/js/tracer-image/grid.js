dfc1aa55c7caa5d1cff4cfca1aff654ae83d7f26
David <drlochr@sandia.gov>
2014-09-10 14:47:23 -0600

    Fixed the width sizing issue
    multiple plots are no longer pushed into fewer columns when the window
    is resized downwards

--- web-server/js/tracer-image/grid.js
+++ web-server/js/tracer-image/grid.js
@@ -5,12 +5,19 @@ function Grid(grid_ref, size, type) {
   this.plot_type = type || ScatterPlot;
 }
 
+Grid.prototype._get_percentage = function(number_objects) {
+  return 100/number_objects + "%";
+}
+
 Grid.prototype.setup = function() {
   for(var j=0; j<this.size[1]; j++) {
-    var row = $("<div/>").attr({id: "row_" + j, translate: "transform(0," + (180*j) + ")"});
+    var row = $("<div/>").attr({id: "row_" + j});
     $(this.grid_ref).append(row);
     for(var i=0; i<this.size[0]; i++) {
-      var cell = $("<div/>").attr({id: "cell_" + i + "_" + j, style: "float: left; width: " + (row.width() / this.size[0]) + "px;"});
+      var cell = $("<div/>").attr(
+        {id: "cell_" + i + "_" + j,
+          style: "float: left; width: " + this._get_percentage(this.size[0]) + ";"
+        });
       row.append(cell);
       plot = new this.plot_type("plot_" + i + "_" + j, "#" + cell.attr("id"), {x: 1/this.size[0], y: 1/this.size[1]});
       plot.controls = new Controls(plot);
