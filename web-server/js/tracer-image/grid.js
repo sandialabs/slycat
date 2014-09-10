628f163c6733e50a07b466fd0ff3477002e17b9d
David <drlochr@sandia.gov>
2014-09-10 13:11:26 -0600

    Initial commit of multiple grids being displayed
    Still need to get all of these into a single (ideally) svg, which
    would require reworking of the controls

--- web-server/js/tracer-image/grid.js
+++ web-server/js/tracer-image/grid.js
@@ -6,9 +6,13 @@ function Grid(grid_ref, size, type) {
 }
 
 Grid.prototype.setup = function() {
-  for(var i=0; i<this.size[0]; i++) {
-    for(var j=0; j<this.size[1]; j++) {
-      plot = new this.plot_type("plot_" + i + "_" + j, this.grid_ref);
+  for(var j=0; j<this.size[1]; j++) {
+    var row = $("<div/>").attr({id: "row_" + j, translate: "transform(0," + (180*j) + ")"});
+    $(this.grid_ref).append(row);
+    for(var i=0; i<this.size[0]; i++) {
+      var cell = $("<div/>").attr({id: "cell_" + i + "_" + j, style: "float: left; width: " + (row.width() / this.size[0]) + "px;"});
+      row.append(cell);
+      plot = new this.plot_type("plot_" + i + "_" + j, "#" + cell.attr("id"), {x: 1/this.size[0], y: 1/this.size[1]});
       plot.controls = new Controls(plot);
       this.plots.push(plot);
     }
