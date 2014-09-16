7cce1801644d9d387c0186d1f64886963f7240ac
Gabriel Arrillaga <gearril@sandia.gov>
2014-09-16 11:08:52 -0600

    Refactored to integrate MovieControl with Movie object.

--- web-server/js/tracer-image/movie.js
+++ web-server/js/tracer-image/movie.js
@@ -1,23 +1,31 @@
+/*
+ Copyright 2013, Sandia Corporation. Under the terms of Contract
+ DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+ rights in this software.
+ */
+
 function Movie(plot) {
   // a plot has access to the image set for the specific cell of the grid
   this.plot = plot;
   this.movie_ref = this.plot.plot_ref + " .movie";
   this.jq_movie = $(this.movie_ref);
   this.d3_movie = d3.select(this.movie_ref).selectAll("image");
-  this.hide();
   this.interval = 400;
   this.current_image = null;
   // TODO leverage bookmarker here for state of movie
   // start out of range and we increment when we ask for the next image
   this.current_image_index = -1;
-  this.height = $(this.plot.grid_ref).attr("height");
-  this.width = $(this.plot.grid_ref).attr("width");
+  this.height = null;
+  this.width = null;
+  this.open_control = null; //handle to foreignObject so SVG can manipulate
 }
 
 Movie.prototype.build_movie = function() {
   var self = this;
   // TODO images for "image set"
   // TODO this may just work after LG fixes controls wrt image set??
+  self.height = $(this.plot.grid_ref).attr("height");
+  self.width = $(this.plot.grid_ref).attr("width");
   this.d3_movie = this.d3_movie
                       .data(this.plot.images.filter(function(d){return d.length > 0;}))
                       .enter().append("image")
@@ -30,6 +38,47 @@ Movie.prototype.build_movie = function() {
                            );
 };
 
+Movie.prototype.build_open_button = function(container) {
+  var self = this;
+  self.open_control = container.append('foreignObject')
+    .classed('open-movie', true) //need a class since d3 can't select foreignObject elements properly in Chrome
+    .attr('width', 50) //TODO: make this sizing not be stupid
+    .attr('height', 40);
+  var open_body = self.open_control.append('xhtml:body')
+    .style('background', 'transparent');;
+  var open_button = open_body.append('button')
+    .classed('play-movie', true)
+    .on('click', function() {
+      $(self.plot.plot_ref + ' .scatterplot').hide();
+      self.play();
+      self.build_close_button(d3.select(self.movie_ref));
+    });
+  open_button.append('img')
+    .attr('src', '/style/play.png');
+}
+
+Movie.prototype.build_close_button = function(container) {
+  var self = this;
+  var close_button = container.append("g")
+        .classed("close-movie", true);
+  close_button.append("rect")
+    .attr("x", 5)
+    .attr("y", 5)
+    .attr("width", 16)
+    .attr("height", 16)
+    .attr("rx", 2)
+    .attr("ry", 2)
+    .style("fill", "rgba(0%,0%,0%,0.2)")
+    .on("click", function() {
+      self.hide();
+    });
+  close_button.append("path")
+    .attr("d", "M" + (8) + " " + (8) + " l10 10 m0 -10 l-10 10")
+    .style("stroke", "rgba(100%,100%,100%, 0.8)")
+    .style("stroke-width", 3)
+    .style("pointer-events", "none");
+};
+
 Movie.prototype.show = function() {
   this.resize();
 };
@@ -39,9 +88,6 @@ Movie.prototype.resize = function() {
   self.height = 375; // TODO $(this.plot.plot_ref + " .scatterplot-pane").height());
   $(this.jq_movie).css("width", self.width);
   $(this.jq_movie).css("height", self.height);
-};
-
-Movie.prototype.hide = function() {
 };
 
 // when the movie is over (reached end of loop), repeat by calling loop again
@@ -65,6 +111,7 @@ Movie.prototype.play = function() {
   // TODO get ALL hostnames for the image set - assuming there can be more than one?
   // TODO set the hostname to something ... loop over all hostnames and get session cache for that hostname
   // TODO right now we just look at the first image
+
   if(!login.logged_into_host_for_file(this.plot.images[0])) {
     this.stop();
     var plot = $(this.plot.plot_ref + " .scatterplot");
@@ -92,6 +139,13 @@ Movie.prototype.stop = function() {
 
 Movie.prototype.step = function() {
 
+};
+
+Movie.prototype.hide = function() {
+  var self = this;
+  //self.close_body.style('visibility', 'hidden');
+  $(self.movie_ref).hide();
+  $(self.plot.plot_ref + ' .scatterplot').show();
 };
 
 Movie.prototype.next_image = function() {
