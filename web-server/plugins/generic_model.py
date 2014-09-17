2b75c6b5a507ace1e9756746b104863b11421a55
Timothy M. Shead <tshead@sandia.gov>
2014-09-17 03:28:09 +0000

    Implement the generic model plugin.

--- web-server/plugins/generic_model.py
+++ web-server/plugins/generic_model.py
@@ -1,4 +1,31 @@
 def register_slycat_plugin(context):
-  def finalize(mid):
-    print "finalizing generic model %s!" % mid
-  context.register_model("generic", finalize)
+  """Called during startup when the plugin is loaded."""
+  import slycat.web.server.database.couchdb
+  import slycat.web.server.model
+  import threading
+  import traceback
+
+  def compute(mid):
+    """Called in a thread to perform work on the model."""
+    try:
+      database = slycat.web.server.database.couchdb.connect()
+      model = database.get("model", mid)
+
+      # Do useful work here
+
+      slycat.web.server.model.update(database, model, state="finished", result="succeeded", finished=datetime.datetime.utcnow().isoformat(), progress=1.0, message="")
+
+    except:
+      cherrypy.log.error("%s" % traceback.format_exc())
+
+      database = slycat.web.server.database.couchdb.connect()
+      model = database.get("model", mid)
+      slycat.web.server.model.update(database, model, state="finished", result="failed", finished=datetime.datetime.utcnow().isoformat(), message=traceback.format_exc())
+
+  def finish(model):
+    """Called to finish the model.  This function must return immediately, so any real work should be done in a separate thread."""
+    thread = threading.Thread(name="Compute Generic Model", target=compute, kwargs={"mid" : model["_id"]})
+    thread.start()
+
+  # Register our new model type
+  context.register_model("generic", finish)
