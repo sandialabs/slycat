6e41ca58164d947c9d48ba51896a3785c6cf4535
Timothy M. Shead <tshead@sandia.gov>
2014-09-26 17:05:45 +0000

    Refining our example plugins.

--- web-client/slycat-hello-world-model.py
+++ web-client/slycat-hello-world-model.py
@@ -2,15 +2,15 @@ import slycat.web.client
 
 parser = slycat.web.client.option_parser()
 parser.add_argument("--marking", default="", help="Marking type.  Default: %(default)s")
-parser.add_argument("--model-name", default="Demo Test Model", help="New model name.  Default: %(default)s")
-parser.add_argument("--project-name", default="Demo Test Project", help="New project name.  Default: %(default)s")
+parser.add_argument("--model-name", default="Hello World Model", help="New model name.  Default: %(default)s")
+parser.add_argument("--project-name", default="Hello World Project", help="New project name.  Default: %(default)s")
 arguments = parser.parse_args()
 
 connection = slycat.web.client.connect(arguments)
 
 pid = connection.find_or_create_project(arguments.project_name)
 
-mid = connection.post_project_models(pid, "test", arguments.model_name, arguments.marking)
+mid = connection.post_project_models(pid, "hello-world", arguments.model_name, arguments.marking)
 
 connection.post_model_finish(mid)
 connection.join_model(mid)
