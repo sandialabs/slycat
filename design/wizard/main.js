eeae6fcba736fa5073bac1068b2696ba3347e2bf
Timothy M. Shead <tshead@sandia.gov>
2014-10-24 13:13:30 -0600

    Updating the wizard test to use requirejs.

---
+++ design/wizard/main.js
@@ -0,0 +1,18 @@
+require.config(
+{
+  paths:
+  {
+    "angular" : "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min",
+    "angular_amd" : "http://cdn.jsdelivr.net/angular.amd/0.2/angularAMD.min",
+  },
+  shim:
+  {
+    "angular_amd" : ["angular"],
+  }
+});
+
+require(["app.js"], function()
+{
+  console.log("main.js");
+});
+
