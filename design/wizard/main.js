4effa9836792bdd292b15b43ca77b82c769d6bbb
Timothy M. Shead <tshead@sandia.gov>
2014-10-24 14:50:44 -0600

    Griding away at dynamic template + controller loading.

--- design/wizard/main.js
+++ design/wizard/main.js
@@ -11,8 +11,33 @@ require.config(
   }
 });
 
-require(["app.js"], function()
+define(["angular_amd"], function(angular_amd)
 {
-  console.log("main.js");
-});
+  var app = angular.module("wizard-app", []);
 
+  app.controller("wizard-controller", function($scope)
+  {
+    $scope.a = 5;
+    $scope.b = 3;
+    $scope.template = "";
+
+    $scope.set_template = function(template)
+    {
+      $scope.template = template + ".html";
+    }
+  });
+
+  app.controller("math-controller", function($scope)
+  {
+    $scope.operator = "+";
+    $scope.result = $scope.a + $scope.b;
+  });
+
+  app.controller("string-controller", function($scope)
+  {
+    $scope.operator = "+";
+    $scope.result = $scope.a + "" + $scope.b;
+  });
+
+  angular_amd.bootstrap(app);
+});
