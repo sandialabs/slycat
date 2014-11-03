bad2e3a2cb9aa49241b2197dbfe6329c7f81d072
Gabriel Arrillaga <gearril@sandia.gov>
2014-11-03 16:06:20 -0700

    Fixed bug where selecting closest point on path was no longer working. Reformatted a couple sections of code to make more readable.

--- web-server/js/tracer-image-scatterplots.js
+++ web-server/js/tracer-image-scatterplots.js
@@ -520,39 +520,60 @@ $.widget("tracer_image.scatterplot", {
 
       var offsets = this._get_plot_offsets();
 
-      filtered_indices.map(function(d){return [self.x_scale(x[d]), self.y_scale(y[d])];})
-        .reduce(function(prev, next, index){
-          // Get the endpoints in reverse order, we'll subtract the index later:
-          var endpoints = [next, prev];
-          var find_closest = function(){
-            var coord = [d3.event.layerX - offsets[0], d3.event.layerY - offsets[1]];
-            //Just doing this for a rough comparison, why bother with expensive math like sqrt and exponents:
-            var distance = endpoints.map(function(point){ return Math.abs(point[0] - coord[0]) + Math.abs(point[1] - coord[1]); });
-            console.debug(distance);
-            ////For reference, this is the more accurate way to do it:
-            //var distance = endpoints.map(function(point){ var dx = (point[0] - coord[0]); var dy = (point[1] = coord[1]); return Math.sqrt(dx*dx + dy*dy); })
+      filtered_indices.map(function(d) {
+        return [self.x_scale(x[d]), self.y_scale(y[d])];
+      }).reduce(function(prev, next, index) {
+        // Get the endpoints in reverse order, we'll subtract the index later:
+        var endpoints = [next, prev];
+        var find_closest = function() {
+          var e = event;
+          var coord = [d3.event.layerX - offsets[0], d3.event.layerY - offsets[1]];
+          //Just doing this for a rough comparison, why bother with expensive math like sqrt and exponents:
+          var distance = endpoints.map(function(point) {
+            return Math.abs(point[0] - coord[0]) + Math.abs(point[1] - coord[1]);
+          });
+          console.debug(distance);
+          ////For reference, this is the more accurate way to do it:
+          //var distance = endpoints.map(function(point){ var dx = (point[0] - coord[0]); var dy = (point[1] = coord[1]); return Math.sqrt(dx*dx + dy*dy); })
           //Push this to the window's event thread, to avoid blocking user responses:
-          window.setTimeout(function(){
-            var image_index = get_closest_image_index.apply(this, distance.map(function(dist, j){return {l: dist, i: index - j}}).sort(function(a,b){return a.i - b.i}));
-            self.options.selection = [image_index];
+          window.setTimeout(function() {
+            var image_index = get_closest_image_index
+              .apply(this, distance.map(function(dist, j) {
+                  return {l: dist, i: index - j};
+                }).sort(function(a,b){
+                  return a.i - b.i;
+                })
+              );
+            if (e.ctrlKey) {
+              var not_selected = (self.options.selection.indexOf(image_index) == -1);
+              if (not_selected) {
+                self.options.selection.push(image_index);
+              }
+            }
+            else {
+              self.options.selection = [image_index];
+            }
             self._schedule_update({render_selection:true});
-            self.element.trigger("selection-changed", [[image_index]]);
+            self.element.trigger("selection-changed", [self.options.selection]);
             self._open_hover(image_index, true);
-          }, 50); };
-          time_line_group.append("path")
-            .attr("stroke", self.options.color(v[index]))
-            .attr("stroke-width", 3)
-            .attr("d", function(){return make_line([prev, next])})
-            .attr("index", index)
-            .on("click.find", find_closest);
-          time_line_group.append("path")
-            .attr("stroke", color_scale(index))
-            .attr("stroke-width", 1)
-            .attr("d", function(){return make_line([prev, next])})
-            .attr("index", index)
-            .on("click.find", find_closest);
-          return next;
-        });
+          }, 50);
+        };
+        // thicker line colored according to color var selected
+        time_line_group.append("path")
+          .attr("stroke", self.options.color(v[index]))
+          .attr("stroke-width", 3)
+          .attr("d", function(){return make_line([prev, next])})
+          .attr("index", index)
+          .on('mousedown', find_closest);
+        // thinner black/white timeline in center
+        time_line_group.append("path")
+          .attr("stroke", color_scale(index))
+          .attr("stroke-width", 1)
+          .attr("d", function(){return make_line([prev, next])})
+          .attr("index", index)
+          .on('mousedown', find_closest);
+        return next;
+      });
     }
 
     if(self.updates["render_selection"])
