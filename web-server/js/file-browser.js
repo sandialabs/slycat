20aff2e97f59b3132c0cd557f963b0509f1f675a
Matthew Smith <mdsmith@sandia.gov>
2014-11-05 14:00:26 -0700

    Beginnings of moving away from the mac style file browser towards a traditional file browser. Now when you expand a folder, it goes into the folder showing only the contents of the folder you are now in. Going up a directory will go into the parent directory

--- web-server/js/file-browser.js
+++ web-server/js/file-browser.js
@@ -57,54 +57,57 @@ $.widget("slycat.browser",
       }
     }
 
+    //THIS IS GONE FOR REFACTOR
     //This method will take a column id and collapse all folder after it while updating the file path input field to be correct.
-    function toggle_all_folders_after_column(column_id, slide_boolean){
-      visible_column_ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
-      max = visible_column_ids[visible_column_ids.length-1];
-      for(var i = (column_id+1); i <= max; i++){
-        $(".file-browser#"+i).remove();
-        visible_column_ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
-        if(slide_boolean){
-          column_to_show = visible_column_ids[0] - 1;
-          $(".file-browser#"+column_to_show).show();
-        }
-      }
-      new_value = $(".path-entry-input").val().split("/").splice(0,column_id-1).join("/")+"/";
-      //Undo the autocomplete hiding of rows.
-      $(".file-browser#"+column_id).find("tr").show();
-      $(".path-entry-input").val(new_value);
-    }
+    //function toggle_all_folders_after_column(column_id, slide_boolean){
+    //  visible_column_ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
+    //  max = visible_column_ids[visible_column_ids.length-1];
+    //  for(var i = (column_id+1); i <= max; i++){
+    //    $(".file-browser#"+i).remove();
+    //    visible_column_ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
+    //    if(slide_boolean){
+    //      column_to_show = visible_column_ids[0] - 1;
+    //      $(".file-browser#"+column_to_show).show();
+    //    }
+    //  }
+    //  new_value = $(".path-entry-input").val().split("/").splice(0,column_id-1).join("/")+"/";
+    //  //Undo the autocomplete hiding of rows.
+    //  $(".file-browser#"+column_id).find("tr").show();
+    //  $(".path-entry-input").val(new_value);
+    //}
 
+    //THIS IS GONE FOR REFACTOR
     //Watch what they type into the file path input field and click, or unclick folders based on it.
-    $(".path-entry-input").keypress(function(event){
-      if(event.which == 47){
-        folder_array = $(this).val().split("/");
-        latest_folder = folder_array[folder_array.length-1];
-        //Click the folder
-        input_with_text = true;
-        $("tr#"+latest_folder.replace(".","dot")).find(".arrow").click();
-      }else if (event.which == 8){
-        //Backspace was pressed
-        folder_array = $(this).val().split("/");
-        latest_folder = folder_array[folder_array.length-1];
-        if (latest_folder == ""){
-          path_to_add = folder_array[folder_array.length-2];
-          column = folder_array.length-1;
-          toggle_all_folders_after_column(column, true);
-          previous_dir = $(".file-browser#"+column).find(".directory.open");
-          previous_dir.removeClass("open");
-          //Random character appended so that the backspace will take effect on it. Hacky? Yes. Future fix desired.
-          $(this).val($(this).val()+path_to_add+"r");
-        }
-      }
-    });
+    //$(".path-entry-input").keypress(function(event){
+    //  if(event.which == 47){
+    //    folder_array = $(this).val().split("/");
+    //    latest_folder = folder_array[folder_array.length-1];
+    //    //Click the folder
+    //    input_with_text = true;
+    //    $("tr#"+latest_folder.replace(".","dot")).find(".arrow").click();
+    //  }else if (event.which == 8){
+    //    //Backspace was pressed
+    //    folder_array = $(this).val().split("/");
+    //    latest_folder = folder_array[folder_array.length-1];
+    //    if (latest_folder == ""){
+    //      path_to_add = folder_array[folder_array.length-2];
+    //      column = folder_array.length-1;
+    //      toggle_all_folders_after_column(column, true);
+    //      previous_dir = $(".file-browser#"+column).find(".directory.open");
+    //      previous_dir.removeClass("open");
+    //      //Random character appended so that the backspace will take effect on it. Hacky? Yes. Future fix desired.
+    //      $(this).val($(this).val()+path_to_add+"r");
+    //    }
+    //  }
+    //});
 
+    //THIS IS GONE FOR REFACTOR
     //This is where the autocomplete happens.
-    $(".path-entry-input").keyup(function(event){
-      column_id = $(this).val().split("/").length-1;
-      $(".file-browser#"+(column_id+1)).find('tr').hide();
-      matches = $(".file-browser#"+(column_id+1)).find('tr:regex(id,^'+$(this).val().replace(".","dot").split("/").splice(column_id,1).join("/")+')').show();
-    });
+    //$(".path-entry-input").keyup(function(event){
+    //  column_id = $(this).val().split("/").length-1;
+    //  $(".file-browser#"+(column_id+1)).find('tr').hide();
+    //  matches = $(".file-browser#"+(column_id+1)).find('tr:regex(id,^'+$(this).val().replace(".","dot").split("/").splice(column_id,1).join("/")+')').show();
+    //});
 
     function toggle_directory(self)
     {
@@ -112,40 +115,44 @@ $.widget("slycat.browser",
       {
         if($(this).hasClass("open")) // Collapse this node ...
         {
-          id = parseInt($(this).parent().parent()[0].id);
-          id_to_show = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort()[0]-1;
-          toggle_all_folders_after_column(id,true);
+          //THIS IS GONE FOR REFACTOR
+          //id = parseInt($(this).parent().parent()[0].id);
+          //id_to_show = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort()[0]-1;
+          //toggle_all_folders_after_column(id,true);
           $(this).removeClass("open");
         }
         else // Expand this node ...
         {
+          //THIS IS GONE FOR REFACTOR
           //If the folder we are expanding is the host, we want to prepend a / to the path input box.
-          if ($(this).hasClass("host")){
-            $(".path-entry-input").val("/")
-          }
-          var current_position = parseInt($(this).parent().parent().attr("id"));
-          var slide = false;
-          if (current_position >= 4){
-            slide = true;
-          }
-          //Remove the next column if it exists before adding new one. This covers the same level expansion.
-          if($(".file-browser#"+(current_position+1)).length){
-            //We set a boolean so we know not to slide on this expansion
-            slide = false;
-            toggle_all_folders_after_column(current_position, slide);
-            //Need to remove the activated icon from the same column folder.
-            previous_dir = $(".file-browser#"+current_position).find(".directory.open");
-            previous_dir.removeClass("open");
-          }
-          if($(this).attr("id") != undefined && input_with_text == false){
-            folder_name = $(this).attr("id").replace("dot",".");
-            old_value = $(".path-entry-input").val().split("/");
-            old_value = old_value.splice(0,old_value.length-1).join("/");
-            $(".path-entry-input").val(old_value +"/"+ folder_name + "/");
-          }
-          input_with_text = false;
+          //if ($(this).hasClass("host")){
+          //  $(".path-entry-input").val("/")
+          //}
+          //var current_position = parseInt($(this).parent().parent().attr("id"));
+          //var slide = false;
+          //if (current_position >= 4){
+          //  slide = true;
+          //}
+          ////Remove the next column if it exists before adding new one. This covers the same level expansion.
+          //if($(".file-browser#"+(current_position+1)).length){
+          //  //We set a boolean so we know not to slide on this expansion
+          //  slide = false;
+          //  toggle_all_folders_after_column(current_position, slide);
+          //  //Need to remove the activated icon from the same column folder.
+          //  previous_dir = $(".file-browser#"+current_position).find(".directory.open");
+          //  previous_dir.removeClass("open");
+          //}
+          //if($(this).attr("id") != undefined && input_with_text == false){
+          //  folder_name = $(this).attr("id").replace("dot",".");
+          //  old_value = $(".path-entry-input").val().split("/");
+          //  old_value = old_value.splice(0,old_value.length-1).join("/");
+          //  $(".path-entry-input").val(old_value +"/"+ folder_name + "/");
+          //}
+          //input_with_text = false;
           //Add text to the file path input when clicked.
           $(this).addClass("open");
+
+          //This call gets the files and folders in the toggled/opened directory.
           $.ajax(
           {
             context : this,
@@ -161,21 +168,49 @@ $.widget("slycat.browser",
             {
 
               var path = $(this).data("path");
+              //THIS IS GONE FOR REFACTOR
               //This is where the horizontal sliding occurs.
-              if (slide == true){
-                ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
-                min = ids[0];
-                //We hide the lowest numbered column.
-                $(".file-browser#"+min).hide();
-              }
-              $("<table>").addClass("file-browser").attr("id",current_position+1).appendTo(self.element);
+              //if (slide == true){
+              //  ids = $(".file-browser:visible").map(function(){return parseInt(this.id);}).sort();
+              //  min = ids[0];
+              //  //We hide the lowest numbered column.
+              //  $(".file-browser#"+min).hide();
+              //}
+              //console.log(current_position);
+
+              //$("<table>").addClass("file-browser").attr("id",current_position+1).appendTo(self.element);
               //No matter what we append to the next table if we are expanding a folder.
-              var container = $(".file-browser#"+(current_position+1));
-              if(result.names.length == 0){
-                var item = $("<tr>").attr("id","empty").appendTo(container);
-                var entry = $("<div/>").appendTo(item);
-                var label = $("<span class='label'></span>").text("Folder is empty.").appendTo(entry);
+              //var container = $(".file-browser#"+(current_position+1));
+              var container = $(".file-browser");
+
+              //remove all previous folders and directories so that we are "moving" to the new folder.
+              $(".file-browser").find("tr.directory").remove();          
+              $(".file-browser").find("tr.file").remove();          
+              //THIS IS GONE FOR REFACTOR
+              //This is where we are saying that a folder is empty.
+              //if(result.names.length == 0){
+              //  var item = $("<tr>").attr("id","empty").appendTo(container);
+              //  var entry = $("<div/>").appendTo(item);
+              //  var label = $("<span class='label'></span>").text("Folder is empty.").appendTo(entry);
+              //}
+
+              //APPEND A FOLDER THAT WILL GO UP A LEVEL.
+              var item = $("<tr>").attr("id","up_dir").appendTo(container);
+              var entry = $("<div/>").appendTo(item);
+              var arrow = $("<span class='arrow'></span>").appendTo(entry);
+              var icon = $("<span class='icon'></span>").appendTo(entry);
+              var label = $("<span class='label'></span>").text("Move up a directory").appendTo(entry);
+              item_path = path.replace(/\/\.?\w*\/?$/, "");
+              if (item_path == ""){
+                item_path += "/"
               }
+              item.data("path", item_path);
+              item.bind("toggle-directory", toggle_directory(self));
+              entry.click(select_item(self, item));
+              item.addClass("directory");
+              arrow.click(click_arrow(item));
+              entry.dblclick(double_click_directory(item));
+              //END OF ADDING UP A DIRECTORY FOLDER.
               for(var i = 0; i != result.names.length; ++i)
               {
                 name = result.names[i];
