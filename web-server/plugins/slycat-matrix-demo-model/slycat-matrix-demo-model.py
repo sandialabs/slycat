1c5aa873f196eee138747d048c2aa914ff2cf548
Timothy M. Shead <tshead@sandia.gov>
2014-11-13 21:39:43 +0000

    Cleanup unused code in the matrix demo model.

--- web-server/plugins/slycat-matrix-demo-model/slycat-matrix-demo-model.py
+++ web-server/plugins/slycat-matrix-demo-model/slycat-matrix-demo-model.py
@@ -1,18 +1,14 @@
 def register_slycat_plugin(context):
-  import cherrypy
   import datetime
   import json
   import os
-  import pystache
   import slycat.web.server.model
 
   def finish(database, model):
     slycat.web.server.model.update(database, model, state="finished", result="succeeded", finished=datetime.datetime.utcnow().isoformat(), progress=1.0, message="")
 
   def html(database, model):
-    context = dict()
-    context["server-root"] = cherrypy.request.app.config["slycat"]["server-root"]
-    return pystache.render(open(os.path.join(os.path.dirname(__file__), "ui.html"), "r").read(), context)
+    return open(os.path.join(os.path.dirname(__file__), "ui.html"), "r").read()
 
   def product(database, model, command, **kwargs):
     pass
