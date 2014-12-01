116c0a6e43b01f4d08c221fe976e39d83efef6d3
Timothy M. Shead <tshead@sandia.gov>
2014-12-01 19:30:29 +0000

    Add project ACL editing to the navbar.

--- web-server/js/slycat-navbar.js
+++ web-server/js/slycat-navbar.js
@@ -10,6 +10,17 @@
       var component = this;
 
       component.alerts = ko.mapping.fromJS([]);
+      component.permission = ko.observable("reader");
+      component.permission_description = ko.pureComputed(function()
+      {
+        if(component.permission() == "reader")
+          return "Readers can view all data in a project.";
+        if(component.permission() == "writer")
+          return "Writers can view all data in a project, and add, modify, or delete models.";
+        if(component.permission() == "administrator")
+          return "Administrators can view all data in a project, add, modify, and delete models, modify or delete the project, and add or remove project members.";
+      });
+      component.new_user = ko.observable("");
       component.model = ko.mapping.fromJS({_id:params.model_id, name:params.model_name, created:"", creator:"",description:"", marking:params.model_marking});
       component.model_popover = ko.pureComputed(function()
       {
@@ -18,9 +29,8 @@
       component.new_model_description = ko.observable("");
       component.new_model_marking = ko.observable(params.model_marking);
       component.new_model_name = ko.observable(params.model_name);
-      component.new_project_description = ko.observable("");
-      component.new_project_name = ko.observable("");
       component.project = ko.mapping.fromJS({_id:params.project_id,name:params.project_name,acl:{"administrators":[],"writers":[],"readers":[]},created:"", creator:"", description:""});
+      component.new_project = ko.mapping.fromJS({_id:params.project_id,name:params.project_name,acl:{"administrators":[],"writers":[],"readers":[]},created:"", creator:"", description:""});
       component.project_popover = ko.pureComputed(function()
       {
         var members = [];
@@ -85,8 +95,8 @@
       {
         var project =
         {
-          "name" : component.new_project_name(),
-          "description" : component.new_project_description(),
+          "name" : component.new_project.name(),
+          "description" : component.new_project.description(),
         };
 
         $.ajax(
@@ -107,12 +117,50 @@
         });
       }
 
+      component.edit_project = function()
+      {
+        ko.mapping.fromJS(ko.mapping.toJS(component.project), component.new_project)
+      }
+
+      component.add_project_member = function()
+      {
+        if(component.permission() == "reader")
+        {
+          component.new_project.acl.readers.push({user:ko.observable(component.new_user())})
+        }
+        if(component.permission() == "writer")
+        {
+          component.new_project.acl.writers.push({user:ko.observable(component.new_user())})
+        }
+        if(component.permission() == "administrator")
+        {
+          component.new_project.acl.administrators.push({user:ko.observable(component.new_user())})
+        }
+      }
+
+      component.remove_project_member = function(context)
+      {
+        component.new_project.acl.readers.remove(function(item)
+        {
+          return item.user()==context.user();
+        });
+        component.new_project.acl.writers.remove(function(item)
+        {
+          return item.user()==context.user();
+        });
+        component.new_project.acl.administrators.remove(function(item)
+        {
+          return item.user()==context.user();
+        });
+      }
+
       component.save_project = function()
       {
         var project =
         {
-          "name" : component.new_project_name(),
-          "description" : component.new_project_description(),
+          "name" : component.new_project.name(),
+          "description" : component.new_project.description(),
+          "acl" : ko.mapping.toJS(component.new_project.acl),
         };
 
         $.ajax(
@@ -216,8 +264,7 @@
           success : function(project)
           {
             ko.mapping.fromJS(project, component.project);
-            component.new_project_name(project.name);
-            component.new_project_description(project.description);
+            ko.mapping.fromJS(project, component.new_project);
           },
         });
       }
@@ -365,7 +412,7 @@
         </ul> \
         <ul class="nav navbar-nav navbar-right"> \
           <li data-bind="visible: !project._id() && !model._id()"><button type="button" class="btn btn-xs btn-success navbar-btn" data-toggle="modal" data-target="#slycat-create-project">Create Project</button></li> \
-          <li data-bind="visible: project._id() && !model._id()"><button type="button" class="btn btn-xs btn-info navbar-btn" data-toggle="modal" data-target="#slycat-edit-project">Edit Project</button></li> \
+          <li data-bind="visible: project._id() && !model._id()"><button type="button" class="btn btn-xs btn-info navbar-btn" data-bind="click:edit_project" data-toggle="modal" data-target="#slycat-edit-project">Edit Project</button></li> \
           <li data-bind="visible: model._id()"><button type="button" class="btn btn-xs btn-info navbar-btn" data-toggle="modal" data-target="#slycat-edit-model">Edit Model</button></li> \
           <li class="navbar-text"><span data-bind="text:user.name"></span> (<span data-bind="text:user.uid"></span>)</li> \
           <li class="dropdown"> \
@@ -414,13 +461,13 @@
             <div class="form-group"> \
               <label for="slycat-create-project-name" class="col-sm-2 control-label">Name</label> \
               <div class="col-sm-10"> \
-                <input id="slycat-create-project-name" class="form-control" type="text" placeholder="Name" data-bind="value:new_project_name"></input> \
+                <input id="slycat-create-project-name" class="form-control" type="text" placeholder="Name" data-bind="value:new_project.name"></input> \
               </div> \
             </div> \
             <div class="form-group"> \
               <label for="slycat-create-project-description" class="col-sm-2 control-label">Description</label> \
               <div class="col-sm-10"> \
-                <textarea id="slycat-create-project-description" class="form-control" placeholder="Description" rows="5" data-bind="value:new_project_description"></textarea> \
+                <textarea id="slycat-create-project-description" class="form-control" placeholder="Description" rows="5" data-bind="value:new_project.description"></textarea> \
               </div> \
             </div> \
           </form> \
@@ -443,13 +490,29 @@
             <div class="form-group"> \
               <label for="slycat-project-name" class="col-sm-2 control-label">Name</label> \
               <div class="col-sm-10"> \
-                <input id="slycat-project-name" class="form-control" type="text" placeholder="Name" data-bind="value:new_project_name"></input> \
+                <input id="slycat-project-name" class="form-control" type="text" placeholder="Name" data-bind="value:new_project.name"></input> \
               </div> \
             </div> \
             <div class="form-group"> \
               <label for="slycat-project-description" class="col-sm-2 control-label">Description</label> \
               <div class="col-sm-10"> \
-                <textarea id="slycat-project-description" class="form-control" placeholder="Description" rows="5" data-bind="value:new_project_description"></textarea> \
+                <textarea id="slycat-project-description" class="form-control" placeholder="Description" rows="5" data-bind="value:new_project.description"></textarea> \
+              </div> \
+            </div> \
+            <div class="form-group"> \
+              <label class="col-sm-2 control-label">Members</label> \
+              <div class="col-sm-10"> \
+                <p class="form-control-static"> \
+                <!-- ko foreach: new_project.acl.administrators --> \
+                  <span class="label label-danger"><span data-bind="text:user"></span><span class="glyphicon glyphicon-remove" data-bind="click:$parent.remove_project_member"></span></span> \
+                <!-- /ko --> \
+                <!-- ko foreach: new_project.acl.writers --> \
+                  <span class="label label-warning"><span data-bind="text:user"></span><span class="glyphicon glyphicon-remove" data-bind="click:$parent.remove_project_member"></span></span> \
+                <!-- /ko --> \
+                <!-- ko foreach: new_project.acl.readers --> \
+                  <span class="label label-primary"><span data-bind="text:user"></span><span class="glyphicon glyphicon-remove" data-bind="click:$parent.remove_project_member"></span></span> \
+                <!-- /ko --> \
+                </p> \
               </div> \
             </div> \
           </form> \
@@ -469,24 +532,28 @@
           <h3 class="modal-title">Add Project Member</h3> \
         </div> \
         <div class="modal-body"> \
-          <form role="form"> \
+          <form class="form-horizontal"> \
             <div class="form-group"> \
-              <label>Permissions</label> \
-              <div class="btn-group" style="width:100%"> \
-                <label class="btn btn-primary" ng-model="group.current" btn-radio="\'reader\'">Reader</label> \
-                <label class="btn btn-primary" ng-model="group.current" btn-radio="\'writer\'">Writer</label> \
-                <label class="btn btn-primary" ng-model="group.current" btn-radio="\'administrator\'">Administrator</label> \
+              <label class="col-sm-2 control-label">Permissions</label> \
+              <div class="col-sm-10"> \
+                <div class="btn-group" data-toggle="buttons" data-bind="radio:permission" style="width:100%"> \
+                  <label class="btn btn-primary"><input type="radio" value="reader"/>Reader</label> \
+                  <label class="btn btn-primary"><input type="radio" value="writer"/>Writer</label> \
+                  <label class="btn btn-primary"><input type="radio" value="administrator"/>Administrator</label> \
+                </div> \
+                <p class="help-block" data-bind="text:permission_description"></p> \
               </div> \
-              <p class="help-block">{{group.description[group.current]}}</p> \
             </div> \
             <div class="form-group"> \
-              <label>Username</label> \
-              <input type="text" class="form-control input-sm" title="Username" placeholder="Username" ng-model="member.uid"></input> \
+              <label class="col-sm-2 control-label">Username</label> \
+              <div class="col-sm-10"> \
+                <input type="text" class="form-control input-sm" title="Username" placeholder="Username" data-bind="value:new_user"></input> \
+              </div> \
             </div> \
           </form> \
         </div> \
         <div class="modal-footer"> \
-          <button class="btn btn-success" data-bind="">Add Member</button> \
+          <button class="btn btn-success" data-dismiss="modal" data-bind="click:add_project_member">Add Member</button> \
           <button class="btn btn-warning" data-dismiss="modal">Cancel</button> \
         </div> \
       </div> \
