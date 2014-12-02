2ff01f7c22fbfb0c02ff341ef9735b5ced3a3b3a
Timothy M. Shead <tshead@sandia.gov>
2014-12-02 14:33:24 -0700

    Zeroing in on a realistic wizard design.

--- design/my-wizard.js
+++ design/my-wizard.js
@@ -3,7 +3,28 @@ define(["text!my-wizard.html"], function(html)
   function constructor(params)
   {
     var model = {};
-    model.stuff = ko.observableArray([{name:"apples"}, {name:"oranges"}, {name:"cats"}]);
+    model.tab = ko.observable(0);
+    model.name = ko.observable("New Model");
+    model.description = ko.observable("");
+    model.cancel = function()
+    {
+      console.log("cancel");
+    }
+    model.create = function()
+    {
+      console.log("create", model.name(), model.description());
+      model.tab(1);
+    }
+    model.load = function()
+    {
+      console.log("load");
+      model.tab(2);
+    }
+    model.finish = function()
+    {
+      console.log("finish");
+      model.tab(3);
+    }
     return model;
   }
 
