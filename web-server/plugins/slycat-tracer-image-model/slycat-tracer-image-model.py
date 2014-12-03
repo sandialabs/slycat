9fc9188ea3b6a64583e5afd9c26adbaa0b7274e6
Gabriel Arrillaga <gearril@sandia.gov>
2014-12-03 11:21:17 -0700

    Starting to move tracer to plugin world.

--- web-server/plugins/slycat-tracer-image-model/slycat-tracer-image-model.py
+++ web-server/plugins/slycat-tracer-image-model/slycat-tracer-image-model.py
@@ -13,3 +13,84 @@ def register_slycat_plugin(context):
 
   # Register our new model type
   context.register_model("tracer-image", finish, html)
+
+  # Register JS
+  javascripts = [
+    # This JS is loaded by header (global bundle defined in get_model in handlers.py)
+    # "js/jquery-2.1.1.min.js",
+    # "js/jquery-migrate-1.2.1.js",
+    # "js/jquery.json-2.4.min.js",
+    # "js/jquery-ui-1.10.4.custom.min.js",
+    # "js/jquery.knob.js",
+    # "js/jquery.qtip.min.js",
+    # "js/knockout-3.2.0.js",
+    # "js/knockout.mapping.js",
+    # "js/slycat-browser.js",
+    # "js/slycat-navbar.js",
+    # "js/slycat-model.js",
+    # End JS loaded by header
+    "jquery.layout-latest.min.js",
+    "jquery.ba-bbq.min.js",
+    "d3.min.js",
+    "bookmarker.js",
+    "chunker.js",
+    "color-switcher.js",
+    "jquery.mousewheel.js",
+    "jquery.scrollintoview.min.js",
+    "jquery.event.drag-2.2.js",
+    "slick.core.js",
+    "slick.grid.js",
+    "slick.rowselectionmodel.js",
+    "slick.headerbuttons.js",
+    "slick.autotooltips.js",
+    "slick.slycateditors.js",
+    "popcorn-complete.js",
+    "modernizr.custom.01940.js"
+    # For development and debugging, loading some js dynamically inside model.
+    # "tracer-image-scatterplots.js",
+    # "tracer-image-table.js",
+    # "layout.js",
+    # "model.js",
+    # "scatterplot.js",
+    # "plot-control.js",
+    # "table.js",
+    # "controls.js",
+    # "movie.js",
+    # "grid.js",
+    # "login.js",
+    # "init.js",
+    # "load.js",
+    # "selector-brush.js"
+  ]
+  context.register_model_bundle("tracer-image", "text/javascript", [
+    os.path.join(os.path.dirname(__file__), js) for js in javascripts
+    ])
+
+
+  # Register CSS
+  stylesheets = [
+    # This CSS is loaded by header
+    # "css/smoothness/jquery-ui-1.10.4.custom.min.css",
+    # "css/jquery.qtip.min.css",
+    # "css/namespaced-bootstrap.css",
+    # "css/slycat.css",
+    # End CSS loaded by header
+    "jquery-ui-1.10.4.custom.min.css",
+    # TODO seems don't need this b/c in header? "jquery-ui-1.10.4.custom.min.css",
+    "slick.grid.css",
+    "slick-default-theme.css",
+    "slick.headerbuttons.css",
+    "slick-slycat-theme.css",
+    # For development and debugging, loading ui.css dynamically inside model.
+    # "model-tracer-image.css",
+  ]
+  context.register_model_bundle("tracer-image", "text/css", [
+    os.path.join(os.path.dirname(__file__), css) for css in stylesheets
+    ])
+
+  # Register images and other resources
+  images = [
+   # TODO track down images
+  ]
+  for image in images:
+    context.register_model_resource("tracer-image", image, os.path.join(os.path.dirname(__file__), image))
