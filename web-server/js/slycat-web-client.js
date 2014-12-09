b20d13d2d95530fd9573f5585a0696bf221238a7
Timothy M. Shead <tshead@sandia.gov>
2014-12-09 17:07:25 +0000

    Expanding the Javascript client API.

--- web-server/js/slycat-web-client.js
+++ web-server/js/slycat-web-client.js
@@ -2,6 +2,35 @@ define("slycat-web-client", ["slycat-server-root"], function(server_root)
 {
   var module = {};
 
+  module.delete_model = function(params)
+  {
+    $.ajax(
+    {
+      type : "DELETE",
+      url : server_root + "models/" + params.mid,
+      success : function()
+      {
+        if(params.success)
+          params.success();
+      }
+    });
+  }
+
+  module.get_configuration_remote_hosts = function(params)
+  {
+    $.ajax(
+    {
+      dataType : "json",
+      type : "GET",
+      url : server_root + "configuration/remote-hosts",
+      success : function(result)
+      {
+        if(params.success)
+          params.success(result);
+      }
+    });
+  }
+
   module.post_project_models = function(params)
   {
     $.ajax(
@@ -20,6 +49,27 @@ define("slycat-web-client", ["slycat-server-root"], function(server_root)
       {
         if(params.success)
           params.success(result.id);
+      }
+    });
+  }
+
+  module.post_remotes = function(params)
+  {
+    $.ajax(
+    {
+      contentType : "application/json",
+      data : $.toJSON(
+      {
+        "hostname" : params.hostname,
+        "username" : params.username,
+        "password" : params.password,
+      }),
+      type : "POST",
+      url : server_root + "remotes",
+      success : function(result)
+      {
+        if(params.success)
+          params.success(result.sid);
       }
     });
   }
