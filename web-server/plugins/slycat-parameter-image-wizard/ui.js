66f54dc63e52f927bff74f237924ed2aa38919a2
Timothy M. Shead <tshead@sandia.gov>
2014-12-09 23:34:12 +0000

    First iteration of a working parameter image model wizard.

--- web-server/plugins/slycat-parameter-image-wizard/ui.js
+++ web-server/plugins/slycat-parameter-image-wizard/ui.js
@@ -6,10 +6,11 @@ define(["slycat-web-client", "text!" + $("#slycat-server-root").attr("href") + "
     component.tab = ko.observable(0);
     component.project = ko.mapping.fromJS({_id:params.project_id});
     component.model = ko.observable(null);
-    component.remote = ko.observable(null);
-    component.browser = ko.observable(null);
-    component.sid = ko.observable(null);
     component.mid = ko.observable(null);
+    component.remote = ko.observable(null);
+    component.sid = ko.observable(null);
+    component.browser = ko.observable(null);
+    component.attributes = ko.mapping.fromJS([]);
 
     component.cancel = function()
     {
@@ -60,22 +61,109 @@ define(["slycat-web-client", "text!" + $("#slycat-server-root").attr("href") + "
         name: "data-table",
         success: function()
         {
-          component.tab(3);
+          client.get_model_table_metadata(
+          {
+            mid: component.mid(),
+            name: "data-table",
+            success: function(metadata)
+            {
+              console.log("metadata", metadata);
+              var attributes = [];
+              for(var i = 0; i != metadata["column-names"].length; ++i)
+                attributes.push({name:metadata["column-names"][i], type:metadata["column-types"][i], categories:[]})
+              ko.mapping.fromJS(attributes, component.attributes);
+              component.tab(3);
+            }
+          });
         }
       });
     }
     component.finish = function()
     {
-      client.post_model_finish(
+      var input_columns = [];
+      var output_columns = [];
+      var rating_columns = [];
+      var category_columns = [];
+      var image_columns = [];
+      for(var i = 0; i != component.attributes().length; ++i)
+      {
+        var categories = component.attributes()[i].categories();
+        for(var j = 0; j != categories.length; ++j)
+        {
+          if(categories[j] == "input")
+            input_columns.push(i);
+          else if(categories[j] == "output")
+            output_columns.push(i);
+          else if(categories[j] == "rating")
+            rating_columns.push(i);
+          else if(categories[j] == "category")
+            category_columns.push(i);
+          else if(categories[j] == "image")
+            image_columns.push(i);
+        }
+      }
+
+      client.put_model_parameter(
       {
         mid: component.mid(),
+        name: "input-columns",
+        value: input_columns,
+        input: true,
         success: function()
         {
-          component.tab(4);
+          client.put_model_parameter(
+          {
+            mid: component.mid(),
+            name: "output-columns",
+            value: output_columns,
+            input: true,
+            success: function()
+            {
+              client.put_model_parameter(
+              {
+                mid: component.mid(),
+                name: "rating-columns",
+                value: rating_columns,
+                input: true,
+                success: function()
+                {
+                  client.put_model_parameter(
+                  {
+                    mid: component.mid(),
+                    name: "category-columns",
+                    value: category_columns,
+                    input: true,
+                    success: function()
+                    {
+                      client.put_model_parameter(
+                      {
+                        mid: component.mid(),
+                        name: "image-columns",
+                        value: image_columns,
+                        input: true,
+                        success: function()
+                        {
+                          client.post_model_finish(
+                          {
+                            mid: component.mid(),
+                            success: function()
+                            {
+                              component.tab(4);
+                            }
+                          });
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
         }
       });
     }
 
+    window.component = component;
     return component;
   }
 
