23b00923e20b7d0bbbad98695a43959e5ceba8c5
Timothy M. Shead <tshead@sandia.gov>
2014-12-24 14:14:20 -0700

    Update the SSE example for incremental updates on the client side.

--- design/sse-test/server.py
+++ design/sse-test/server.py
@@ -1,4 +1,8 @@
 import cherrypy
+import datetime
+import json
+import numpy
+import os
 import time
 
 class Root():
@@ -10,11 +14,38 @@ class Root():
   def time(self):
     cherrypy.response.headers["content-type"] = "text/event-stream"
     def content():
-      while True:
-        yield "data: %s\n\n" % time.time()
-        time.sleep(2)
+      while cherrypy.engine.state == cherrypy.engine.states.STARTED:
+        yield "data: %s\n\n" % datetime.datetime.utcnow()
+        time.sleep(1)
     return content()
   time._cp_config = {"response.stream": True}
 
-cherrypy.quickstart(Root(), '/', config={})
+  @cherrypy.expose
+  def birds(self):
+    cherrypy.response.headers["content-type"] = "text/event-stream"
+    def content():
+      birds = {}
+      while cherrypy.engine.state == cherrypy.engine.states.STARTED:
+        id = numpy.random.choice(10)
+        operation = numpy.random.choice(["remove"] + ["update"] * 10)
+        if operation == "remove":
+          if id in birds:
+            del birds[id]
+            yield "data: %s\n\n" % json.dumps({"remove":{"id":id}})
+        else:
+          if id in birds:
+            birds[id]["rev"] += 1
+          else:
+            birds[id] = {"rev":0}
+          yield "data: %s\n\n" % json.dumps({"update":{"id":id, "rev":birds[id]["rev"]}})
+        time.sleep(0.1)
+    return content()
+  birds._cp_config = {"response.stream": True}
+
+cherrypy.quickstart(Root(), '/', config={
+  "/js" : {
+    "tools.staticdir.dir" : os.path.abspath("js"),
+    "tools.staticdir.on" : True,
+    }
+  })
 
