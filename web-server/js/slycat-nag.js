cf61baa157728c261cfec2a9f03b3a6bb922fe9d
Timothy M. Shead <tshead@sandia.gov>
2014-12-25 00:42:30 +0000

    Switch from browser detection to feature detection for nagging the user.

--- web-server/js/slycat-nag.js
+++ web-server/js/slycat-nag.js
@@ -4,30 +4,16 @@ DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 rights in this software.
 */
 
-// Disable console traces for jquery.migrate
-$.migrateTrace = false;
-
 define("slycat-nag", [], function()
 {
   var nag = false;
 
-  var newWebkit  = $.browser.webkit  && parseFloat($.browser.version) >= 534.53;
-  var newChrome  = $.browser.chrome  && parseFloat($.browser.version) >= 17;
-  var newMozilla = $.browser.mozilla && parseFloat($.browser.version) >= 10;
-
-  // Warn IE8- users they need a real browser
-  if ($.browser.msie && parseInt($.browser.version,10)<9)
-  {
+  // We need EventSource for our live project and model feeds.
+  if(!window.EventSource)
     nag = true;
-  }
-  // Warn everyone other than WebKit 534.53+ or Chrome 17+ or FF10+ they might need to upgrade
-  else if ( !(newWebkit || newChrome || newMozilla) )
-  {
-    nag = true;
-  }
 
   if(nag)
   {
-    alert("Your browser might not provide the features needed by Slycat. We suggest switching to a current version of Firefox, Chrome, or Safari. You can also try Internet Explorer version 9 or above, but support for it is experimental.");
+    alert("Your browser is missing features required by Slycat. We suggest switching to a current version of Firefox, Chrome, or Safari.");
   }
 });
