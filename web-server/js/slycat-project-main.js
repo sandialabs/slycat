cc7cc669c1b0a757c3735d00447e9f9966058e25
Timothy M. Shead <tshead@sandia.gov>
2015-01-08 19:58:46 +0000

    Found a CSS-only approach to sizing the .slycat-content area on each page.

--- web-server/js/slycat-project-main.js
+++ web-server/js/slycat-project-main.js
@@ -25,15 +25,6 @@ define("slycat-project-main", ["slycat-server-root", "slycat-web-client", "slyca
           return page.markings()[i].badge();
       }
     }
-
-    // Size the page content to consume available space
-    function size_content()
-    {
-      $(".slycat-content").css("min-height", $(window).height() - $("slycat-navbar > div").height());
-    }
-    $(window).resize(size_content);
-    window.setTimeout(function() { $(window).resize(); }, 10);
-
     ko.applyBindings(page);
   };
 
