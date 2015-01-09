8d1bdb8bf2c3b1a7688c462dd8e7b7ed15b1993d
David <drlochr@sandia.gov>
2015-01-09 11:21:24 -0700

    Fixes for login, adjusting module name to standard

--- web-server/plugins/slycat-tracer-image-model/js/init.js
+++ web-server/plugins/slycat-tracer-image-model/js/init.js
@@ -1,4 +1,4 @@
-define("slycat-tracer-image-model", ["slycat-model-main", "Model", "Layout", "Table", "Grid", "ScatterPlot", "Login", "domReady!"], function(slycat_main, Model, Layout, Table, Grid, ScatterPlot, Login) {
+define("slycat-tracer-image-model", ["slycat-model-main", "Model", "Layout", "Table", "Grid", "ScatterPlot", "slycat-tracer-model-login", "domReady!"], function(slycat_main, Model, Layout, Table, Grid, ScatterPlot, Login) {
   slycat_main.start(); //for some reason this isn't running before the model-specific JS executes like it does for the other models
   $(window).resize(); //triggers slycat_main module to set the height of div.slycat-content, which as a parent node is required for jQuery UI layout
   var model = new Model();
