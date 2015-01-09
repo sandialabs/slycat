4396084a3032441c51f4dea9b54208a7b3360fc4
Gabriel Arrillaga <gearril@sandia.gov>
2015-01-09 10:16:55 -0700

    Converted Table to AMD module. Removed handlebar interpolation and dependence on global variables.

--- web-server/plugins/slycat-tracer-image-model/js/table.js
+++ web-server/plugins/slycat-tracer-image-model/js/table.js
@@ -1,194 +1,201 @@
-function Table() {
-  console.debug("Setup table");
-  this.statistics = null;
-  this.ready = false;
-};
+define("Table", ["slycat-server-root"], function(server_root) {
 
-Table.prototype.setup = function() {
-  console.debug("inside TABLE setup()");
-  // TODO what do we do here with the x and y index ??
-  // TODO - for now using the first plot
-  // TODO - can this be decoupled?
-  // this means we are still good for parameter image
-  var self = this;
-  var plot = grid.plots[0];
-  var x_index = plot.x_index;
-  var y_index = plot.y_index;
-  var v_index = plot.v_index;
-  var images_index = plot.images_index;
-  var selected_simulations = plot.selected_simulations;
-  var hidden_simulations = plot.hidden_simulations;
-  if(!this.ready && model.metadata && (this.statistics.length == model.metadata["column-count"])
-     && layout.bookmark && (x_index != null) && (y_index != null) && (images_index != null)
-     && (selected_simulations != null) && (hidden_simulations != null) ) {
-    this.ready = true;
+  function Table(model, grid) {
+    console.debug("Setup table");
+    this.model = model;
+    this.grid = grid;
+    this.statistics = null;
+    this.ready = false;
+  };
 
-    $("#table-pane .load-status").css("display", "none");
+  Table.prototype.setup = function() {
+    console.debug("inside TABLE setup()");
+    // TODO what do we do here with the x and y index ??
+    // TODO - for now using the first plot
+    // TODO - can this be decoupled?
+    // this means we are still good for parameter image
+    var self = this;
+    var plot = self.grid.plots[0];
+    var x_index = plot.x_index;
+    var y_index = plot.y_index;
+    var v_index = plot.v_index;
+    var images_index = plot.images_index;
+    var selected_simulations = plot.selected_simulations;
+    var hidden_simulations = plot.hidden_simulations;
+    if(!this.ready && this.model.metadata && (this.statistics.length == this.model.metadata["column-count"])
+       && this.model.bookmark && (x_index != null) && (y_index != null) && (images_index != null)
+       && (selected_simulations != null) && (hidden_simulations != null) ) {
+      this.ready = true;
 
-    var other_columns = [];
-    for(var i = 0; i != model.metadata["column-count"] - 1; ++i) {
-      if($.inArray(i, model.input_columns) == -1 && $.inArray(i, model.output_columns) == -1
-         && $.inArray(i, model.rating_columns) == -1 && $.inArray(i, model.category_columns) == -1) {
-        other_columns.push(i);
+      $("#table-pane .load-status").css("display", "none");
+
+      var other_columns = [];
+      for(var i = 0; i != self.model.metadata["column-count"] - 1; ++i) {
+        if($.inArray(i, self.model.input_columns) == -1 && $.inArray(i, self.model.output_columns) == -1
+           && $.inArray(i, self.model.rating_columns) == -1 && $.inArray(i, self.model.category_columns) == -1) {
+          other_columns.push(i);
+        }
       }
+
+      var table_options = {
+        "server-root" : server_root,
+        mid : self.model.id,
+        aid : "data-table",
+        metadata : self.model.metadata,
+        statistics : self.statistics,
+        inputs : self.model.input_columns,
+        outputs : self.model.output_columns,
+        others : other_columns,
+        images : self.model.image_columns,
+        ratings : self.model.rating_columns,
+        categories : self.model.category_columns,
+        "image-variable" : images_index,
+        "x-variable" : x_index,
+        "y-variable" : y_index,
+        "row-selection" : selected_simulations,
+        hidden_simulations : hidden_simulations
+      };
+
+      var colormap = self.model.bookmark["colormap"] !== undefined ? self.model.bookmark["colormap"] : "night";
+      table_options.colormap = $("#color-switcher").colorswitcher("get_color_map", colormap);
+
+      if("sort-variable" in self.model.bookmark && "sort-order" in self.model.bookmark) {
+        table_options["sort-variable"] = self.model.bookmark["sort-variable"];
+        table_options["sort-order"] = self.model.bookmark["sort-order"];
+      }
+
+      if("variable-selection" in self.model.bookmark) {
+        table_options["variable-selection"] = [self.model.bookmark["variable-selection"]];
+      }
+      else {
+        table_options["variable-selection"] = [self.model.metadata["column-count"] - 1];
+      }
+
+      $("#table").table(table_options);
+
+      // Log changes to the table sort order ...
+      $("#table").bind("variable-sort-changed", function(event, variable, order) {
+        self.variable_sort_changed(variable, order);
+      });
+
+      /* TODO: implement the table controls to sync with a 'selected plot'
+       // Log changes to the x variable ...
+       $("#table").bind("x-selection-changed", function(event, variable) {
+       plot.x_selection_changed(variable);
+       });
+
+       // Log changes to the y variable ...
+       $("#table").bind("y-selection-changed", function(event, variable) {
+       plot.y_selection_changed(variable);
+       });
+
+       // Log changes to the images variable ...
+       $("#table").bind("images-selection-changed", function(event, variable) {
+       plot.images_selection_changed(variable);
+       });
+
+       // Log changes to the table variable selection ...
+       // column selection in table
+       $("#table").bind("variable-selection-changed", function(event, selection) {
+       plot.selected_variable_changed(selection[0]);
+       });
+       */
+
+      // Log changes to the table row selection ...
+      $("#table").bind("row-selection-changed", function(event, selection) {
+        // The table selection is an array buffer which can't be
+        // serialized as JSON, so convert it to an array.
+        var temp = [];
+        for(var i = 0; i != selection.length; ++i)
+          temp.push(selection[i]);
+        plot.selected_simulations_changed(temp);
+        $(".scatterplot").scatterplot("option", "selection",  temp);
+      });
+
+      // Changing the colormap updates the table ...
+      $("#color-switcher").bind("colormap-changed", function(event, colormap) {
+        $("#table").table("option", "colormap", $("#color-switcher").colorswitcher("get_color_map", colormap));
+      });
+      /* TODO: implement the table controls to sync with a 'selected plot'
+       // Changing the table variable updates the scatterplot ...
+       $("#table").bind("variable-selection-changed", function(event, selection) {
+       plot.update_value(selection[0]);
+       });
+       */
+      // Changing the scatterplot selection updates the table row selection and controls ..
+      $(".scatterplot").bind("selection-changed", function(event, selection) {
+        var temp = [];
+        for(var i = 0; i != selection.length; ++i)
+          temp.push(selection[i]);
+        $("#table").table("option", "row-selection", selection);
+        $(".scatterplot").scatterplot("option", "selection",  temp);
+      });
+      /*
+       // Changing the x variable updates the table ...
+       $(plot.plot_ref + " .controls").bind("x-selection-changed", function(event, variable) {
+
+       $("#table").table("option", "x-variable", variable);
+       });
+
+       // Changing the y variable updates the table ...
+       $(plot.plot_ref + " .controls").bind("y-selection-changed", function(event, variable) {
+       $("#table").table("option", "y-variable", variable);
+       });
+
+       // Changing the image variable updates the table ...
+       $(plot.plot_ref + " .controls").bind("images-selection-changed", function(event, variable) {
+       $("#table").table("option", "image-variable", variable);
+       });
+
+       // Changing the color variable updates the table ...
+       $(plot.plot_ref + " .controls").bind("color-selection-changed", function(event, variable) {
+       $("#table").table("option", "variable-selection", [Number(variable)]);
+       });
+       */
     }
+  };
 
-    var table_options = {
-      "server-root" : "{{server-root}}",
-      mid : "{{_id}}",
-      aid : "data-table",
-      metadata : model.metadata,
-      statistics : table.statistics,
-      inputs : model.input_columns,
-      outputs : model.output_columns,
-      others : other_columns,
-      images : model.image_columns,
-      ratings : model.rating_columns,
-      categories : model.category_columns,
-      "image-variable" : images_index,
-      "x-variable" : x_index,
-      "y-variable" : y_index,
-      "row-selection" : selected_simulations,
-      hidden_simulations : hidden_simulations
-    };
-
-    var colormap = layout.bookmark["colormap"] !== undefined ? layout.bookmark["colormap"] : "night";
-    table_options.colormap = $("#color-switcher").colorswitcher("get_color_map", colormap);
-
-    if("sort-variable" in layout.bookmark && "sort-order" in layout.bookmark) {
-
-      table_options["sort-variable"] = layout.bookmark["sort-variable"];
-      table_options["sort-order"] = layout.bookmark["sort-order"];
+  Table.prototype.load_statistics = function(columns, callback, callback_obj) {
+    // TODO what's going on with callback in terms of OO?
+    // the callback does not contain the same context as "this" here does
+    var self = this;
+    console.debug(this);
+    console.debug("inside load table statistics");
+    var requests = Array();
+    for(var i=0; i<columns.length; i++) {
+      requests.push(
+        $.ajax({
+          url : server_root + "models/" + self.model.id + "/arraysets/data-table/arrays/0/attributes/" + columns[i] + "/statistics",
+          contentType : "application/json",
+        })
+      );
     }
-
-    if("variable-selection" in layout.bookmark) {
-      table_options["variable-selection"] = [layout.bookmark["variable-selection"]];
-    }
-    else {
-      table_options["variable-selection"] = [model.metadata["column-count"] - 1];
-    }
-
-    $("#table").table(table_options);
-
-    // Log changes to the table sort order ...
-    $("#table").bind("variable-sort-changed", function(event, variable, order) {
-      self.variable_sort_changed(variable, order);
+    var defer = $.when.apply($, requests);
+    defer.done(function(){
+      // This is executed only after every ajax request has been completed
+      $.each(arguments, function(index, responseData){
+        // "responseData" contains an array of response information for each specific request
+        self.statistics[parseInt(columns[index])] = responseData.length == undefined ? responseData : responseData[0];
+      });
+      callback.call(callback_obj || self);
     });
+  };
 
-    /* TODO: implement the table controls to sync with a 'selected plot'
-     // Log changes to the x variable ...
-     $("#table").bind("x-selection-changed", function(event, variable) {
-     plot.x_selection_changed(variable);
-     });
-
-     // Log changes to the y variable ...
-     $("#table").bind("y-selection-changed", function(event, variable) {
-     plot.y_selection_changed(variable);
-     });
-
-     // Log changes to the images variable ...
-     $("#table").bind("images-selection-changed", function(event, variable) {
-     plot.images_selection_changed(variable);
-     });
-
-     // Log changes to the table variable selection ...
-     // column selection in table
-     $("#table").bind("variable-selection-changed", function(event, selection) {
-     plot.selected_variable_changed(selection[0]);
-     });
-     */
-
-    // Log changes to the table row selection ...
-    $("#table").bind("row-selection-changed", function(event, selection) {
-      // The table selection is an array buffer which can't be
-      // serialized as JSON, so convert it to an array.
-      var temp = [];
-      for(var i = 0; i != selection.length; ++i)
-        temp.push(selection[i]);
-      plot.selected_simulations_changed(temp);
-      $(".scatterplot").scatterplot("option", "selection",  temp);
-    });
-
-    // Changing the colormap updates the table ...
-    $("#color-switcher").bind("colormap-changed", function(event, colormap) {
-      $("#table").table("option", "colormap", $("#color-switcher").colorswitcher("get_color_map", colormap));
-    });
-    /* TODO: implement the table controls to sync with a 'selected plot'
-     // Changing the table variable updates the scatterplot ...
-     $("#table").bind("variable-selection-changed", function(event, selection) {
-     plot.update_value(selection[0]);
-     });
-     */
-    // Changing the scatterplot selection updates the table row selection and controls ..
-    $(".scatterplot").bind("selection-changed", function(event, selection) {
-      var temp = [];
-      for(var i = 0; i != selection.length; ++i)
-        temp.push(selection[i]);
-      $("#table").table("option", "row-selection", selection);
-      $(".scatterplot").scatterplot("option", "selection",  temp);
-    });
-    /*
-     // Changing the x variable updates the table ...
-     $(plot.plot_ref + " .controls").bind("x-selection-changed", function(event, variable) {
-
-     $("#table").table("option", "x-variable", variable);
-     });
-
-     // Changing the y variable updates the table ...
-     $(plot.plot_ref + " .controls").bind("y-selection-changed", function(event, variable) {
-     $("#table").table("option", "y-variable", variable);
-     });
-
-     // Changing the image variable updates the table ...
-     $(plot.plot_ref + " .controls").bind("images-selection-changed", function(event, variable) {
-     $("#table").table("option", "image-variable", variable);
-     });
-
-     // Changing the color variable updates the table ...
-     $(plot.plot_ref + " .controls").bind("color-selection-changed", function(event, variable) {
-     $("#table").table("option", "variable-selection", [Number(variable)]);
-     });
-     */
-  }
-};
-
-Table.prototype.load_statistics = function(columns, callback, callback_obj) {
-  // TODO what's going on with callback in terms of OO?
-  // the callback does not contain the same context as "this" here does
-  var self = this;
-  console.debug(this);
-  console.debug("inside load table statistics");
-  var requests = Array();
-  for(var i=0; i<columns.length; i++) {
-    requests.push(
-      $.ajax({
-        url : "{{server-root}}models/{{_id}}/arraysets/data-table/arrays/0/attributes/" + columns[i] + "/statistics",
-        contentType : "application/json",
-      })
-    );
-  }
-  var defer = $.when.apply($, requests);
-  defer.done(function(){
-    // This is executed only after every ajax request has been completed
-    $.each(arguments, function(index, responseData){
-      // "responseData" contains an array of response information for each specific request
-      self.statistics[parseInt(columns[index])] = responseData.length == undefined ? responseData : responseData[0];
-    });
-    callback.call(callback_obj || self);
-  });
-};
-
-Table.prototype.variable_sort_changed = function(variable, order) {
-  console.debug("inside variable_sort_changed()");
-  console.debug("variable sort changed -- ajax");
+  Table.prototype.variable_sort_changed = function(variable, order) {
+    console.debug("inside variable_sort_changed()");
+    console.debug("variable sort changed -- ajax");
+    var self = this;
     $.ajax({
       type : "POST",
-      url : "{{server-root}}events/models/{{_id}}/select/sort-order/" + variable + "/" + order
+      url : server_root + "events/models/" + self.model.id + "/select/sort-order/" + variable + "/" + order
     });
-  layout.bookmarker.updateState( {"sort-variable" : variable, "sort-order" : order} );
-};
+    self.model.bookmarker.updateState( {"sort-variable" : variable, "sort-order" : order} );
+  };
 
-Table.prototype.select_rows = function(rows){
-  $(".scatterplot").scatterplot("option", "selection",  rows);
-  $("#table").table("option", "row-selection", rows);
-};
+  Table.prototype.select_rows = function(rows){
+    $(".scatterplot").scatterplot("option", "selection",  rows);
+    $("#table").table("option", "row-selection", rows);
+  };
+
+  return Table;
+});
