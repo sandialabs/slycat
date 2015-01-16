f0eb17411cca5f8e6edf37befa472cbaf02c67dc
Alex Milosz Sielicki <alex@techmilo.com>
2015-01-16 21:50:56 +0000

    Patch jquery.knob.js to specify an AMD module id so it can be bundled.

--- web-server/plugins/slycat-timeseries-model/js/jquery.knob.js
+++ web-server/plugins/slycat-timeseries-model/js/jquery.knob.js
@@ -13,7 +13,7 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
+        define('knob', ['jquery'], factory);
     } else {
         // Browser globals
         factory(jQuery);
