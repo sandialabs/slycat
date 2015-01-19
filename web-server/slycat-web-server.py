e4831568c0e3d40c08578c0532916b99a0b8127d
Timothy M. Shead <tshead@sandia.gov>
2015-01-19 16:34:07 +0000

    Ensure we always have a working default config, but allow the default to be overridden with an environment variable.

--- web-server/slycat-web-server.py
+++ web-server/slycat-web-server.py
@@ -2,12 +2,22 @@
 # DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 # rights in this software.
 
-import optparse
-parser = optparse.OptionParser()
-parser.add_option("--config", default="config.ini", help="Path to a file containing configuration parameters.")
-options, arguments = parser.parse_args()
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", default=None, help="Path to a file containing configuration parameters.")
+arguments = parser.parse_args()
+
+config_path = "config.ini"
+
+if "SLYCAT_WEB_SERVER_CONFIG" in os.environ:
+  config_path = os.environ["SLYCAT_WEB_SERVER_CONFIG"]
+
+if arguments.config is not None:
+  config_path = arguments.config
 
 import os
 import slycat.web.server.engine
-slycat.web.server.engine.start(os.path.dirname(os.path.abspath(__file__)), options.config)
+slycat.web.server.engine.start(os.path.dirname(os.path.abspath(__file__)), config_path)
 
