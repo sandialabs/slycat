d40b02eda54dbd0ac2ce48c2b0a08f959a0a8687
Timothy M. Shead <tshead@sandia.gov>
2015-01-27 16:11:48 -0700

    Ongoing scrollbar work.

--- web-server/js/slycat-scrollbar.js
+++ web-server/js/slycat-scrollbar.js
@@ -11,20 +11,21 @@ require(["slycat-server-root", "knockout", "knockout-mapping"], function(server_
     viewModel: function(params)
     {
       var scrollbar = this;
-      scrollbar.axis = ko.observable("horizontal");
-      scrollbar.width = ko.observable(500);
-      scrollbar.height = params.height || ko.observable(10);
+      scrollbar.axis = ko.unwrap(params.axis) || "vertical";
+      scrollbar.length = params.length || ko.observable(500);
+      scrollbar.width = params.width || ko.observable(10);
       scrollbar.bar =
       {
-        width: ko.observable(10),
-        height: ko.observable(10),
-        last_drag: ko.observable(null),
+        length: params.bar_length || ko.observable(50),
+        width: params.bar_width || ko.observable(10),
+        dragging: params.dragging || ko.observable(false),
+        last_drag: null,
       };
       scrollbar.domain =
       {
-        min: ko.observable(0),
-        value: ko.observable(0.5),
-        max: ko.observable(1),
+        min: params.domain_min || ko.observable(0),
+        value: params.domain_value || ko.observable(0.5),
+        max: params.domain_max || ko.observable(1),
       };
       scrollbar.range =
       {
@@ -35,47 +36,49 @@ require(["slycat-server-root", "knockout", "knockout-mapping"], function(server_
 
       scrollbar.range.max = ko.pureComputed(function()
       {
-        if(scrollbar.axis() == "vertical")
-          return scrollbar.height() - scrollbar.bar.height();
-        else
-          return scrollbar.width() - scrollbar.bar.width();
+        return scrollbar.length() - scrollbar.bar.length();
       });
       scrollbar.range.value = ko.pureComputed(function()
       {
         return (scrollbar.domain.value() - scrollbar.domain.min()) / (scrollbar.domain.max() - scrollbar.domain.min()) * (scrollbar.range.max() - scrollbar.range.min() + scrollbar.range.min());
       });
+      scrollbar.style = ko.pureComputed(function()
+      {
+        var result = {};
+        result[scrollbar.axis == "vertical" ? "height" : "width"] = scrollbar.length() + "px";
+        result[scrollbar.axis == "vertical" ? "width" : "height"] = scrollbar.width() + "px";
+        return result;
+      });
       scrollbar.bar.style = ko.pureComputed(function()
       {
         var result = {};
-        result.width = scrollbar.bar.width() + "px";
-        result.height = scrollbar.bar.height() + "px";
-        if(scrollbar.axis() == "vertical")
-          result.top = scrollbar.range.value() + "px";
-        else
-          result.left = scrollbar.range.value() + "px";
+        result[scrollbar.axis == "vertical" ? "height" : "width"] = scrollbar.bar.length() + "px";
+        result[scrollbar.axis == "vertical" ? "width" : "height"] = scrollbar.bar.width() + "px";
+        result[scrollbar.axis == "vertical" ? "top" : "left"] = scrollbar.range.value() + "px";
         return result;
       });
       scrollbar.bar.mousedown = function(model, event)
       {
-        scrollbar.bar.last_drag([event.screenX, event.screenY]);
+        scrollbar.bar.dragging(true);
+        scrollbar.bar.last_drag = [event.screenX, event.screenY];
         window.addEventListener("mousemove", scrollbar.bar.mousemove, true);
         window.addEventListener("mouseup", scrollbar.bar.mouseup, true);
       }
       scrollbar.bar.mousemove = function(event)
       {
         var new_range;
-        if(scrollbar.axis() == "vertical")
-          new_range = scrollbar.range.value() + event.screenY - scrollbar.bar.last_drag()[1];
+        if(scrollbar.axis == "vertical")
+          new_range = scrollbar.range.value() + event.screenY - scrollbar.bar.last_drag[1];
         else
-          new_range = scrollbar.range.value() + event.screenX - scrollbar.bar.last_drag()[0];
+          new_range = scrollbar.range.value() + event.screenX - scrollbar.bar.last_drag[0];
         var new_value = (new_range - scrollbar.range.min()) / (scrollbar.range.max() - scrollbar.range.min()) * (scrollbar.domain.max() - scrollbar.domain.min()) + scrollbar.domain.min();
 
         scrollbar.domain.value(Math.max(scrollbar.domain.min(), Math.min(scrollbar.domain.max(), new_value)));
-        scrollbar.bar.last_drag([event.screenX, event.screenY]);
+        scrollbar.bar.last_drag = [event.screenX, event.screenY];
       }
       scrollbar.bar.mouseup = function(event)
       {
-        scrollbar.bar.last_drag(null);
+        scrollbar.bar.dragging(false);
         window.removeEventListener("mousemove", scrollbar.bar.mousemove, true);
         window.removeEventListener("mouseup", scrollbar.bar.mouseup, true);
       }
