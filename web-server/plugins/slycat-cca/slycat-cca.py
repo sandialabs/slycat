a7784b0fe814452b115787ff42522388a473074e
Timothy M. Shead <tshead@sandia.gov>
2015-02-02 03:47:51 +0000

    Merge the CCA model plugin and wizard plugins into a single module.

--- web-server/plugins/slycat-cca/slycat-cca.py
+++ web-server/plugins/slycat-cca/slycat-cca.py
@@ -140,3 +140,15 @@ def register_slycat_plugin(context):
     ])
   context.register_model_resource("cca", "images", os.path.join(os.path.dirname(__file__), "images"))
 
+  # Register custom wizards for creating CCA models.
+  context.register_wizard("remote-cca", "New Remote CCA Model", require={"action":"create", "context":"project"})
+  context.register_wizard_resource("remote-cca", "ui.js", os.path.join(os.path.dirname(__file__), "js/remote-ui.js"))
+  context.register_wizard_resource("remote-cca", "ui.html", os.path.join(os.path.dirname(__file__), "remote-ui.html"))
+
+  context.register_wizard("local-cca", "New Local CCA Model", require={"action":"create", "context":"project"})
+  context.register_wizard_resource("local-cca", "ui.js", os.path.join(os.path.dirname(__file__), "js/local-ui.js"))
+  context.register_wizard_resource("local-cca", "ui.html", os.path.join(os.path.dirname(__file__), "local-ui.html"))
+
+  context.register_wizard("rerun-cca", "Modified CCA Model", require={"action":"create", "context":"model", "model-type":["cca"]})
+  context.register_wizard_resource("rerun-cca", "ui.js", os.path.join(os.path.dirname(__file__), "js/rerun-ui.js"))
+  context.register_wizard_resource("rerun-cca", "ui.html", os.path.join(os.path.dirname(__file__), "rerun-ui.html"))
