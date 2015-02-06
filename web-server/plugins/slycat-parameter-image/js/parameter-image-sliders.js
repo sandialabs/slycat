df9e48c098c5802936e43ad4826a9ac3c435f463
Alex Milosz Sielicki <alex@techmilo.com>
2015-02-06 18:59:08 +0000

    Working on sliders

--- web-server/plugins/slycat-parameter-image/js/parameter-image-sliders.js
+++ web-server/plugins/slycat-parameter-image/js/parameter-image-sliders.js
@@ -17,26 +17,72 @@ $.widget("parameter_image.sliders",
   {
   	var self = this;
 
-  	this.x_slider = $("<slycat-range-slider params=\"axis:'vertical', length:length_slider, low:x_low, high:x_high\"></slycat-range-slider>")
-      .appendTo(this.element)
-      ;
+  	// this.x_slider = $("<slycat-range-slider params=\"axis:'vertical', length:length, low:x_low, high:x_high\"></slycat-range-slider>")
+   //    .appendTo(this.element)
+   //    ;
 
-    this.y_slider = $("<slycat-range-slider params=\"axis:'vertical', length:length_slider, low:x_low, high:x_high, reverse: true\"></slycat-range-slider>")
-      .appendTo(this.element)
-      ;
+   //  this.y_slider = $("<slycat-range-slider params=\"axis:'vertical', length:length, low:x_low, high:x_high, reverse: true\"></slycat-range-slider>")
+   //    .appendTo(this.element)
+   //    ;
 
-    this.feedback = $("<p>x: <span data-bind='text: x_low'></span> - <span data-bind='text: x_high'></span></p>")
-    	.appendTo(this.element)
-    	;
+    // this.z_slider = $("<div>").
+    // 	.data("bind", {
+    // 		component : {
+    // 			name : "slycat-range-slider",
+    // 			params : {
+    // 				axis : "vertical",
+    // 				length : length,
+    // 				low : x_low,
+    // 				high : x_high,
+    // 				reverse : true,
+    // 			},
+    // 		},
+    // 	})
+    // 	.appendTo(this.element)
+    // 	;
+
+    // this.z_slider = $("<div>")
+    // 	.data("bind", {
+    // 		component : {
+    // 			name : "slycat-range-slider",
+    // 			params : {
+    // 				axis : "vertical",
+    // 				length : 'length',
+    // 				low : 'x_low',
+    // 				high : 'x_high',
+    // 				reverse : true,
+    // 			},
+    // 		},
+    // 	})
+    // 	.appendTo(this.element)
+    // 	;
+
+
+    // this.feedback = $("<p>x: <span data-bind='text: x_low'></span> - <span data-bind='text: x_high'></span></p>")
+    // 	.appendTo(this.element)
+    // 	;
+
+    var sliders = {
+    	sliders : [
+    		{ params: "axis:'vertical', length:length, low:x_low, high:x_high" },
+    		{ params: "axis:'vertical', length:length, low:x_low, high:x_high" },
+    		{ params: "axis:'vertical', length:length, low:x_low, high:x_high" },
+    		{ params: "axis:'vertical', length:length, low:x_low, high:x_high" },
+    	]
+    }
+
+    ko.applyBindings(sliders, this.element[0]);
+
+    // this.container = $("<div data-bind='foreach: sliders'>")
 
     var page = mapping.fromJS(
     {
       x_low: 0.3,
       x_high: 0.7,
-      length_slider: 600,
+      length: 600,
     });
 
-    ko.applyBindings(page, this.element[0]);
+    // ko.applyBindings(page, this.element[0]);
 
   },
 
