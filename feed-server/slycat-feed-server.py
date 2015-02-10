0f43d3f94c31a95dd4a89a578c52dcb888d71a24
Timothy M. Shead <tshead@sandia.gov>
2015-02-10 21:55:19 +0000

    Update the feed server main loop to broadcast changes to multiple clients.

--- feed-server/slycat-feed-server.py
+++ feed-server/slycat-feed-server.py
@@ -1,3 +1,4 @@
+import datetime
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -5,17 +6,21 @@ import tornado.web
 import tornado.websocket
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
+  clients = []
+
   def open(self, *args, **kwargs):
     print("WebSocket opened %s %s" % (args, kwargs))
-    self.write_message("foo");
-    self.write_message("bar");
-    self.write_message("baz");
+    self.write_message("a");
+    self.write_message("b");
+    self.write_message("c");
+    EchoWebSocket.clients.append(self)
 
   def on_message(self, message):
-    self.write_message(u"You said: " + message)
+    pass
 
   def on_close(self):
     print("WebSocket closed %s %s" % (self.close_code, self.close_reason))
+    EchoWebSocket.clients.remove(self)
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
@@ -28,10 +33,6 @@ class MainHandler(tornado.web.RequestHandler):
     <h1>Hello, World!</h1>
     <script type="application/javascript">
       var ws = new WebSocket("wss://192.168.59.103:8888/websocket/myticket");
-      ws.onopen = function()
-      {
-        ws.send("Hello, world");
-      };
       ws.onmessage = function (evt)
       {
         console.log(evt.data);
@@ -46,10 +47,17 @@ application = tornado.web.Application([
   ("/websocket/(.*)", EchoWebSocket),
 ], debug=True)
 
+def tick():
+  timestamp = datetime.datetime.utcnow().isoformat()
+  for client in EchoWebSocket.clients:
+    client.write_message(timestamp)
+
 if __name__ == "__main__":
   tornado.options.parse_command_line()
   server = tornado.httpserver.HTTPServer(application, ssl_options={"certfile":"../web-server/web-server.pem", "keyfile":"../web-server/web-server.key"})
   server = tornado.httpserver.HTTPServer(application)
   server.listen(8888)
+
+  tornado.ioloop.PeriodicCallback(tick, 2000).start()
   tornado.ioloop.IOLoop.instance().start()
 
