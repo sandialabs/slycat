c900ec9592337317eda1a59b85d0dd48f927339e
Timothy M. Shead <tshead@sandia.gov>
2015-02-12 19:36:59 +0000

    Provide better formatting options for the feed client.

--- feed-server/slycat-feed-client.py
+++ feed-server/slycat-feed-client.py
@@ -1,11 +1,31 @@
 import argparse
+import json
 import slycat.web.client
 import sys
 import tornado.websocket
 
 parser = slycat.web.client.ArgumentParser()
 parser.add_argument("--feed-host", default="ws://localhost:8093", help="Root URL of the feed server.  Default: %(default)s")
+parser.add_argument("--format", choices=["raw", "summary"], default="summary", help="Output data format.  Default: %(default)s")
 arguments = parser.parse_args()
+
+def format_raw(message, stream):
+  stream.write("%s\n" % message)
+
+def format_summary(message, stream):
+  change = json.loads(message)
+  if "deleted" in change:
+    stream.write("  delete %s\n" % change["id"])
+  else:
+    if change["doc"]["type"] == "project":
+      stream.write("p update %s\n" % change["id"])
+    elif change["doc"]["type"] == "model":
+      stream.write("m update %s\n" % change["id"])
+
+if arguments.format == "raw":
+  formatter = format_raw
+elif arguments.format == "summary":
+  formatter = format_summary
 
 connection = slycat.web.client.connect(arguments)
 tid = connection.get_ticket()
@@ -17,7 +37,7 @@ def watch_feed():
     message = yield websocket.read_message()
     if message is None:
       break
-    sys.stdout.write("%s\n" % message)
+    formatter(message, sys.stdout)
     sys.stdout.flush()
 
 try:
