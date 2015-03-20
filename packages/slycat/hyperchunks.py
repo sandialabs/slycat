09454ed375034154d49940a01df128695bc99920
Timothy M. Shead <tshead@sandia.gov>
2015-03-19 22:50:59 -0600

    More hyperchunks work.

--- packages/slycat/hyperchunks.py
+++ packages/slycat/hyperchunks.py
@@ -7,31 +7,80 @@
 import numbers
 import numpy
 
-def _format_index(index):
-  if isinstance(index, numbers.Integral):
-    return str(index)
-  elif isinstance(index, type(Ellipsis)):
-    return "..."
-  elif isinstance(index, slice):
-    return ("%s:%s" % ("" if index.start is None else index.start, "" if index.stop is None else index.stop)) + ("" if index.step is None else ":%s" % index.step)
-  else:
-    raise ValueError("Not a valid index expression: %s" % index)
-
-def _format_index_expression(index_expression):
-  return ",".join([_format_index(index) for index in index_expression])
-
-def _validate_index_expression(index_expression):
+def _validate_index_expression(index_expression, dimension_count=None):
   if not isinstance(index_expression, tuple):
     raise ValueError("An index expression must be a tuple.")
+  if dimension_count is not None:
+    if len(index_expression) != dimension_count:
+      raise ValueError("Index expression must only contain one dimension.")
   for dimension in index_expression:
     if not isinstance(dimension, (numbers.Integral, slice, type(Ellipsis))):
       raise ValueError("Each dimension in an index expression must be an integer, slice, or Ellipsis.")
   return index_expression
 
+def _format_index_dimension(dimension):
+  if isinstance(dimension, numbers.Integral):
+    return str(dimension)
+  elif isinstance(dimension, type(Ellipsis)):
+    return "..."
+  elif isinstance(dimension, slice):
+    return ("%s:%s" % ("" if dimension.start is None else dimension.start, "" if dimension.stop is None else dimension.stop)) + ("" if dimension.step is None else ":%s" % dimension.step)
+  else:
+    raise ValueError("Not a valid index dimension: %s" % dimension)
+
+def _format_index_expression(index_expression):
+  return ",".join([_format_index_dimension(dimension) for dimension in index_expression])
+
+def _parse_index_expression(index_expression):
+  if not isinstance(index_expression, basestring):
+    raise ValueError("A string is required.")
+
+  result = []
+  for dimension in index_expression.split(","):
+    if dimension == "...":
+      result.append(Ellipsis)
+    else:
+      dimension = dimension.split(":")
+      if len(dimension) == 1:
+        result.append(int(dimension[0]))
+      elif len(dimension) == 2:
+        result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1])))
+      elif len(dimension) == 3:
+        result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1]), None if dimension[2] == "" else int(dimension[2])))
+      else:
+        raise ValueError("Not a valid slice.")
+  return tuple(result)
+
+
+
 class Hyperslice(object):
   """Represents a multi-dimensional array hyperslice (set of indices for every dimension in the array)."""
-  def __init__(self, indices):
-    self._indices = _validate_index_expression(indices)
+  def __init__(self, *args):
+    if len(args) == 1 and isinstance(args[0], basestring):
+      result = []
+      for dimension in args[0].split(","):
+        if dimension == "...":
+          result.append(Ellipsis)
+        else:
+          dimension = dimension.split(":")
+          if len(dimension) == 1:
+            result.append(int(dimension[0]))
+          elif len(dimension) == 2:
+            result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1])))
+          elif len(dimension) == 3:
+            result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1]), None if dimension[2] == "" else int(dimension[2])))
+          else:
+            raise ValueError()
+      self._indices = tuple(result)
+    elif len(args) == 1:
+      if not isinstance(args[0], tuple):
+        raise ValueError("Hyperslice must be a tuple.")
+      for dimension in args[0]:
+        if not isinstance(dimension, (numbers.Integral, slice, type(Ellipsis))):
+          raise ValueError("Each dimension in a Hyperslice must be an integer, slice, or Ellipsis.")
+      self._indices = args[0]
+    else:
+      raise ValueError()
 
   def __repr__(self):
     return """slycat.hyperchunks.Hyperslice("%s")""" % self.format()
@@ -44,8 +93,13 @@ class Hyperslice(object):
 
 class Hyperslices(object):
   """Represents a collection of Hyperslice instances that all share the same number of dimensions."""
-  def __init__(self):
-    self._hyperslices = []
+  def __init__(self, *args):
+    if len(args) == 1 and isinstance(args[0], basestring):
+      self._hyperslices = [Hyperslice(hyperslice) for hyperslice in args[0].split("|")]
+    elif len(args) == 1:
+      self._hyperslices = []
+      for hyperslice in args[0]:
+        self.append(hyperslice)
 
   def __len__(self):
     return len(self._hyperslices)
@@ -59,15 +113,31 @@ class Hyperslices(object):
   def __str__(self):
     return self.format()
 
+  def append(self, hyperslice):
+    if not isinstance(hyperslice, Hyperslice):
+      raise ValueError("Not a Hyperslice.")
+    self._hyperslices.append(hyperslice)
+
   def format(self):
     return "|".join([hyperslice.format() for hyperslice in self._hyperslices])
 
 class Hyperchunk(object):
   """Represents a hyperchunk (a set of array indices, optional set of attribute indices, and zero-to-many Hyperslices instances)."""
-  def __init__(self, arrays, attributes=None):
-    self._arrays = _validate_index_expression(arrays)
-    self._attributes = _validate_index_expression(attributes) if attributes is not None else None
-    self._hyperslices = Hyperslices()
+  def __init__(self, arrays, attributes=None, hyperslices=Hyperslices()):
+    if isinstance(arrays, basestring):
+      sections = arrays.split("/")
+      if len(sections) == 1:
+        self._arrays = _parse_index_expression(sections[0])
+        self._attributes = None
+        self._hyperslices = Hyperslices()
+      elif len(sections) == 2:
+        hyperchunks.append(Hyperchunk(_parse_index_expression(sections[0]), _parse_index_expression(sections[1])))
+      elif len(sections) == 3:
+        hyperchunks.append(Hyperchunk(_parse_index_expression(sections[0]), _parse_index_expression(sections[1]), _parse_index_expression(sections[2])))
+    else:
+      self._arrays = _validate_index_expression(arrays, dimension_count=1)
+      self._attributes = _validate_index_expression(attributes, dimension_count=1) if attributes is not None else None
+      self._hyperslices = _validate_index_expression(hyperslices)
 
   def __repr__(self):
     return """slycat.hyperchunks.Hyperchunk("%s")""" % self.format()
@@ -85,8 +155,17 @@ class Hyperchunk(object):
 
 class Hyperchunks(object):
   """Represents a collection zero-to-many Hyperchunk instances."""
-  def __init__(self):
-    self._hyperchunks = []
+  def __init__(self, *args):
+    if len(args) == 1 and isinstance(args[0], basestring):
+      self._hyperchunks = [Hyperchunk(hyperchunk) for hyperchunk in args[0].split(";")]
+    elif len(args) == 1:
+      self._hyperchunks = []
+      for hyperchunk in args[0]:
+        self.append(hyperchunk)
+    elif len(args) == 0:
+      self._hyperchunks = []
+    else:
+      raise ValueError()
 
   def __len__(self):
     return len(self._hyperchunks)
@@ -108,77 +187,3 @@ class Hyperchunks(object):
       raise ValueError("Not a hyperchunk.")
     self._hyperchunks.append(hyperchunk)
 
-#import numbers
-#
-#def validate(hyperslice):
-#  """Validate an object to confirm that it is a valid hyperslice."""
-#  if isinstance(hyperslice, (numbers.Integral, slice, type(Ellipsis))):
-#    return hyperslice
-#
-#  if isinstance(hyperslice, tuple):
-#    for dimension in hyperslice:
-#      if not isinstance(dimension, (numbers.Integral, slice, type(Ellipsis))):
-#        raise ValueError("Not a valid hyperslice: %s" % hyperslice)
-#    return hyperslice
-#
-#  raise ValueError("Not a valid hyperslice: %s" % hyperslice)
-#
-#def _format_dimension(dimension):
-#  if isinstance(dimension, numbers.Integral):
-#    return str(dimension)
-#  elif isinstance(dimension, slice):
-#    return ("%s:%s" % ("" if dimension.start is None else dimension.start, "" if dimension.stop is None else dimension.stop)) + ("" if dimension.step is None else ":%s" % dimension.step)
-#  elif isinstance(dimension, type(Ellipsis)):
-#    return "..."
-#  raise ValueError("Not a valid slice: %s" % dimension)
-#
-#def format(hyperslice):
-#  """Convert a hyperslice to a string representation.
-#
-#  Parameters
-#  ----------
-#  hyperslice : integer, slice, Ellipsis, or tuple containing any combination of integers, slices, or Ellipsis.
-#    The hyperslice to be converted to a string.
-#
-#  Returns
-#  -------
-#  string
-#  """
-#  if isinstance(hyperslice, (numbers.Integral, slice, type(Ellipsis))):
-#    return _format_dimension(hyperslice)
-#  elif isinstance(hyperslice, tuple):
-#    return ",".join([_format_dimension(dimension) for dimension in hyperslice])
-#  raise ValueError("Not a valid hyperslice: %s" % hyperslice)
-#
-#def parse(hyperslice):
-#  """Parse the string representation of a hyperslice.
-#
-#  Parameters
-#  ----------
-#  hyperslice : string
-#    The string representation of a hyperslice.
-#
-#  Returns
-#  -------
-#  tuple containing integers, slices, or Ellipsis.
-#  """
-#
-#  if not isinstance(hyperslice, basestring):
-#    raise ValueError("An input string is required.")
-#
-#  result = []
-#  for dimension in hyperslice.split(","):
-#    if dimension == "...":
-#      result.append(Ellipsis)
-#    else:
-#      dimension = dimension.split(":")
-#      if len(dimension) == 1:
-#        result.append(int(dimension[0]))
-#      elif len(dimension) == 2:
-#        result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1])))
-#      elif len(dimension) == 3:
-#        result.append(slice(None if dimension[0] == "" else int(dimension[0]), None if dimension[1] == "" else int(dimension[1]), None if dimension[2] == "" else int(dimension[2])))
-#      else:
-#        raise ValueError("Not a valid slice.")
-#  return tuple(result)
-#
