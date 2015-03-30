59dd8d26bd3d2f454792f143ba0d2ba00f3a1a7e
Timothy M. Shead <tshead@sandia.gov>
2015-03-30 21:01:43 +0000

    Simplify web-server command-line arguments.

--- web-server/slycat-web-server.py
+++ web-server/slycat-web-server.py
@@ -4,20 +4,11 @@
 
 import argparse
 import os
+import slycat.web.server.engine
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config", default=None, help="Path to a file containing configuration parameters.")
+parser.add_argument("--config", default="config.ini", help="Path to a file containing configuration parameters.")
 arguments = parser.parse_args()
 
-config_path = "config.ini"
-
-if "SLYCAT_WEB_SERVER_CONFIG" in os.environ:
-  config_path = os.environ["SLYCAT_WEB_SERVER_CONFIG"]
-
-if arguments.config is not None:
-  config_path = arguments.config
-
-import os
-import slycat.web.server.engine
-slycat.web.server.engine.start(os.path.dirname(os.path.abspath(__file__)), config_path)
+slycat.web.server.engine.start(os.path.dirname(os.path.abspath(__file__)), arguments.config)
 
