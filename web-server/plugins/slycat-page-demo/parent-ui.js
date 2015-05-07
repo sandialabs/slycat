9e57169c32b7b2f7b634856893e2094aaff1dace
Timothy M. Shead <tshead@sandia.gov>
2015-05-07 19:39:49 +0000

    First pass at incorporating child windows into bookmark state.

--- web-server/plugins/slycat-page-demo/parent-ui.js
+++ web-server/plugins/slycat-page-demo/parent-ui.js
@@ -4,18 +4,16 @@ DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 rights in this software.
 */
 
-define("slycat-page-demo-model", ["slycat-server-root", "knockout", "lodash", "domReady!"], function(server_root, ko, lodash)
+define("slycat-page-demo-model", ["slycat-server-root", "slycat-bookmark-manager", "slycat-web-client", "knockout", "lodash", "URI", "domReady!"], function(server_root, bookmark_manager, client, ko, lodash, URI)
 {
   // Setup storage for the data we're going to plot.
   var page = {};
+  page.closing = false;
   page.children = ko.observableArray();
   page.add_child = function()
   {
     page.children.push(window.open(server_root + "resources/models/page-demo/child-ui.html"));
-    lodash.each(page.children(), function(child)
-    {
-      child.postMessage("Child created", "*");
-    });
+    page.bookmark.updateState({children: page.children().length});
   }
   page.send_message = function()
   {
@@ -55,10 +53,61 @@ define("slycat-page-demo-model", ["slycat-server-root", "knockout", "lodash", "d
 
   ko.applyBindings(page, document.getElementById("slycat-page-demo"));
 
-  window.addEventListener("message", receive_message, false);
-  function receive_message(event)
+  client.get_model(
+  {
+    mid: URI(window.location).segment(-1),
+    success: function(model)
+    {
+      page.bookmark = bookmark_manager.create(model.project, model._id);
+      page.bookmark.getState(function(state)
+      {
+        console.log("bookmark state:", state);
+        if("children" in state)
+        {
+          for(var i = 0; i != state.children; ++i)
+          {
+            page.children.push(window.open(server_root + "resources/models/page-demo/child-ui.html"));
+            window.setTimeout(function () { window.focus() }, 10000);
+          }
+        }
+        else
+        {
+          page.bookmark.updateState({children: 0});
+        }
+      });
+    }
+  });
+
+  window.addEventListener("message", function(event)
   {
     console.log(event);
-  }
+    if(event.data === "closing")
+    {
+      if(!page.closing)
+      {
+        page.children.remove(event.source);
+        page.bookmark.updateState({children: page.children().length});
+      }
+    }
+  });
+
+  window.addEventListener("beforeunload", function(event)
+  {
+    page.closing = true;
+    if(page.children().length)
+    {
+      var message = "This will affect all related tabs / windows.";
+      event.returnValue = message;
+      return message;
+    }
+  });
+
+  window.addEventListener("unload", function(event)
+  {
+    lodash.each(page.children(), function(child)
+    {
+      child.close();
+    });
+  });
 });
 
