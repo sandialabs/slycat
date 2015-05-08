13e25a73651a931507f49b277e010c6167f8c1d9
Timothy M. Shead <tshead@sandia.gov>
2015-05-08 20:23:40 +0000

    Differentiate between orphan pages caused by a browser restart and URIs bookmarked from a child page.

--- web-server/plugins/slycat-page-demo/child-ui.js
+++ web-server/plugins/slycat-page-demo/child-ui.js
@@ -6,17 +6,16 @@ rights in this software.
 
 define("slycat-page-demo-child-page", ["knockout", "URI"], function(ko, URI)
 {
-  // If this page is an orphan, close it.
-  if(URI().hasQuery("role", "child") === true)
-  {
-    window.close();
-    return;
-  }
-
-  // This must be a new child, mark it as a child.
+  // If this page is a new child, mark it as a child.
   if(URI().hasQuery("role", "new-child") === true)
   {
     window.history.replaceState(null, null, URI().setQuery("role", "child"));
+  }
+  // If this page is an orphan, close it.
+  else if(URI().hasQuery("role", "orphan") === true)
+  {
+    window.close();
+    return;
   }
 
   window.addEventListener("message", function(event)
@@ -31,6 +30,12 @@ define("slycat-page-demo-child-page", ["knockout", "URI"], function(ko, URI)
     {
       window.history.replaceState(null, null, URI().setQuery("bid", event.data.bid));
     }
+  });
+
+  window.addEventListener("beforeunload", function(event)
+  {
+    // Mark ourselves as an orphan.
+    window.history.replaceState(null, null, URI().setQuery("role", "orphan"));
   });
 
   window.addEventListener("unload", function(event)
