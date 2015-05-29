203f18addff1d286891ac92ca1d558a6e93aade1
Timothy M. Shead <tshead@sandia.gov>
2015-05-29 17:56:02 +0000

    Centralize test server configuration, and prompt for passwords instead of using environment variables, to avoid accidental disclosure.

--- features/steps/rest-api.py
+++ features/steps/rest-api.py
@@ -1,17 +1,11 @@
 from behave import *
 
 import nose.tools
-import os
 import slycat.web.client
-
-server_host = os.environ.get("SLYCAT_TEST_SERVER", "https://localhost")
-server_proxy = os.environ.get("SLYCAT_TEST_PROXY", "")
-server_user = os.environ.get("SLYCAT_TEST_SERVER_USER", "slycat")
-server_password = os.environ.get("SLYCAT_TEST_SERVER_PASSWORD", "slycat")
 
 @given(u'a running Slycat server.')
 def step_impl(context):
-  context.connection = slycat.web.client.Connection(host=server_host, proxies={"http":server_proxy, "https":server_proxy}, auth=(server_user, server_password))
+  context.connection = slycat.web.client.Connection(host=context.server_host, proxies={"http":context.server_proxy, "https":context.server_proxy}, auth=(context.server_user, context.server_password))
   context.connection.get_configuration_version()
 
 @when(u'a client requests the server version.')
