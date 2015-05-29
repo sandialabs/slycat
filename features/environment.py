c6b0d229434409fdead1e00cd40cccb0f957a6dc
Timothy M. Shead <tshead@sandia.gov>
2015-05-29 18:10:00 +0000

    Code cleanup.

--- features/environment.py
+++ features/environment.py
@@ -6,4 +6,4 @@ def before_all(context):
   context.server_host = os.environ.get("SLYCAT_TEST_SERVER", "https://localhost")
   context.server_proxy = os.environ.get("SLYCAT_TEST_SERVER_PROXY", "")
   context.server_user = os.environ.get("SLYCAT_TEST_SERVER_USER", "slycat")
-  context.server_password = getpass.getpass("%s password: " % os.environ["SLYCAT_TEST_SERVER_USER"]) if "SLYCAT_TEST_SERVER_USER" in os.environ else "slycat"
+  context.server_password = getpass.getpass("%s password: " % context.server_user) if "SLYCAT_TEST_SERVER_USER" in os.environ else "slycat"
