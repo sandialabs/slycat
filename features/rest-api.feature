00d5ad1c92daec7ae9d54eede49c4d1ea0267706
Timothy M. Shead <tshead@sandia.gov>
2015-05-29 20:01:37 +0000

    Added all of the "GET Configuration Foo" requests to the REST API tests.

--- features/rest-api.feature
+++ features/rest-api.feature
@@ -1,5 +1,25 @@
 Feature: REST API
+  Scenario: GET Configuration Markings
+    Given a running Slycat server.
+    When a client requests the set of available markings.
+    Then the server should return a list of markings.
+  Scenario: GET Configuration Parsers
+    Given a running Slycat server.
+    When a client requests the set of available parsers.
+    Then the server should return a list of parsers.
+  Scenario: GET Configuration Remote Hosts
+    Given a running Slycat server.
+    When a client requests the set of configured remote hosts.
+    Then the server should return a list of remote hosts.
+  Scenario: GET Configuration Support Email
+    Given a running Slycat server.
+    When a client requests the server support email.
+    Then the server should return its support email.
   Scenario: GET Configuration Version
     Given a running Slycat server.
     When a client requests the server version.
     Then the server should return its version.
+  Scenario: GET Configuration Wizard
+    Given a running Slycat server.
+    When a client requests available server wizards.
+    Then the server should return a list of available wizards.
--- features/steps/rest-api.py
+++ features/steps/rest-api.py
@@ -8,13 +8,76 @@ def step_impl(context):
   context.connection = slycat.web.client.Connection(host=context.server_host, proxies={"http":context.server_proxy, "https":context.server_proxy}, auth=(context.server_user, context.server_password))
   context.connection.get_configuration_version()
 
+@when(u'a client requests the set of available markings.')
+def step_impl(context):
+  context.response = context.connection.get_configuration_markings()
+
+@then(u'the server should return a list of markings.')
+def step_impl(context):
+  nose.tools.assert_is_instance(context.response, list)
+  for item in context.response:
+    nose.tools.assert_is_instance(item, dict)
+    nose.tools.assert_in("badge", item)
+    nose.tools.assert_in("label", item)
+    nose.tools.assert_in("page-before", item)
+    nose.tools.assert_in("page-after", item)
+    nose.tools.assert_in("type", item)
+
+@when(u'a client requests the set of available parsers.')
+def step_impl(context):
+  context.response = context.connection.get_configuration_parsers()
+
+@then(u'the server should return a list of parsers.')
+def step_impl(context):
+  nose.tools.assert_is_instance(context.response, list)
+  for item in context.response:
+    nose.tools.assert_is_instance(item, dict)
+    nose.tools.assert_in("categories", item)
+    nose.tools.assert_in("label", item)
+    nose.tools.assert_in("type", item)
+
+@when(u'a client requests the set of configured remote hosts.')
+def step_impl(context):
+  context.response = context.connection.get_configuration_remote_hosts()
+
+@then(u'the server should return a list of remote hosts.')
+def step_impl(context):
+  nose.tools.assert_is_instance(context.response, list)
+  for item in context.response:
+    nose.tools.assert_is_instance(item, dict)
+    nose.tools.assert_in("agent", item)
+    nose.tools.assert_in("hostname", item)
+
+@when(u'a client requests the server support email.')
+def step_impl(context):
+  context.response = context.connection.get_configuration_support_email()
+
+@then(u'the server should return its support email.')
+def step_impl(context):
+  nose.tools.assert_is_instance(context.response, dict)
+  nose.tools.assert_in("address", context.response)
+  nose.tools.assert_in("subject", context.response)
+
 @when(u'a client requests the server version.')
 def step_impl(context):
-  context.result = context.connection.get_configuration_version()
+  context.response = context.connection.get_configuration_version()
 
 @then(u'the server should return its version.')
 def step_impl(context):
-  nose.tools.assert_is_instance(context.result, dict)
-  nose.tools.assert_in("commit", context.result)
-  nose.tools.assert_in("version", context.result)
+  nose.tools.assert_is_instance(context.response, dict)
+  nose.tools.assert_in("commit", context.response)
+  nose.tools.assert_in("version", context.response)
+
+@when(u'a client requests available server wizards.')
+def step_impl(context):
+  context.response = context.connection.get_configuration_wizards()
+
+@then(u'the server should return a list of available wizards.')
+def step_impl(context):
+  nose.tools.assert_is_instance(context.response, list)
+  for item in context.response:
+    nose.tools.assert_is_instance(item, dict)
+    nose.tools.assert_in("label", item)
+    nose.tools.assert_in("require", item)
+    nose.tools.assert_in("type", item)
 
