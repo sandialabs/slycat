111bf913653b2f265e4e355d5973adde9c063152
srbdev <srbdevelopment@gmail.com>
2015-06-04 13:57:48 -0600

    #448 - rebrands STL viewer to 3D viewer and closes ticket.

--- web-server/plugins/slycat-stl-model/ui.js
+++ web-server/plugins/slycat-stl-model/ui.js
@@ -1,4 +1,4 @@
 define('slycat-stl-model', ['slycat-web-client', 'knockout', 'knockout-mapping', 'URI', 'domReady!'], function(client, ko, mapping, URI) {
 
-  ko.applyBindings({}, document.getElementById('slycat-stl-viewer-container'));
+  ko.applyBindings({}, document.getElementById('slycat-3d-viewer-container'));
 });
