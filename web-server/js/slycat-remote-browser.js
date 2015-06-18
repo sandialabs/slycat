7d16182fa0337920ed261cab98050c4b166de587
Alex M. Sielicki <alex@techmilo.com>
2015-06-18 18:47:16 +0000

    Scrolling to top on directory change. Closes #459

--- web-server/js/slycat-remote-browser.js
+++ web-server/js/slycat-remote-browser.js
@@ -138,6 +138,7 @@ define("slycat-remote-browser", ["slycat-server-root", "slycat-web-client", "kno
             for(var i = 0; i != results.names.length; ++i)
               files.push({name:results.names[i], size:results.sizes[i], type:results.types[i], mtime:results.mtimes[i], mime_type:results["mime-types"][i]});
             mapping.fromJS(files, component.raw_files);
+            $('.slycat-remote-browser-files').scrollTop(0);
           },
           error : function(results)
           {
