de3ae56cd01287a261d673df393d60ae71ecfca7
Alex M. Sielicki <alex@techmilo.com>
2015-07-29 21:17:30 +0000

    putting cluster-columns parameter

--- web-server/plugins/slycat-parameter-image-plus-model/js/wizard-ui.js
+++ web-server/plugins/slycat-parameter-image-plus-model/js/wizard-ui.js
@@ -376,10 +376,18 @@ define(["slycat-server-root", "slycat-web-client", "slycat-dialog", "knockout", 
                         value: image_columns,
                         input: true,
                         success: function() {
-                          client.post_model_finish({
+                          client.put_model_parameter({
                             mid: component.model._id(),
+                            aid: "cluster-columns",
+                            value: component.cluster_column(),
+                            input: true,
                             success: function() {
-                              component.tab(6);
+                              client.post_model_finish({
+                                mid: component.model._id(),
+                                success: function() {
+                                  component.tab(6);
+                                }
+                              });
                             }
                           });
                         }
