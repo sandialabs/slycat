9e2590e48e3b4fb9f96c1f6b6a712617f4fd9776
Timothy M. Shead <tshead@sandia.gov>
2015-08-06 20:37:38 +0000

    Allow custom pages to be displayed without models.

---
+++ web-server/js/slycat-page-main.js
@@ -0,0 +1,19 @@
+/*
+Copyright 2013, Sandia Corporation. Under the terms of Contract
+DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+rights in this software.
+*/
+
+define("slycat-page-main", ["slycat-server-root", "slycat-changes-feed", "knockout"], function(server_root, changes_feed, ko)
+{
+  var module = {};
+  module.start = function()
+  {
+    var page = {}
+    page.server_root = server_root;
+    page.projects = changes_feed.projects();
+    ko.applyBindings(page, document.querySelector("html"));
+  }
+
+  return module;
+});
