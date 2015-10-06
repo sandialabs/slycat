90706e229bb20f038653c8bc2e48df1975eab395
Matthew Letter <mletter1@unm.edu>
2015-10-06 09:27:45 -0600

    updated multipart file upload comments

--- web-server/plugins/slycat-multipart-upload-demo/slycat-multipart-upload-demo.py
+++ web-server/plugins/slycat-multipart-upload-demo/slycat-multipart-upload-demo.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 def register_slycat_plugin(context):
   import os
 
@@ -6,6 +7,19 @@ def register_slycat_plugin(context):
     context = dict()
     return pystache.render(open(os.path.join(os.path.dirname(__file__), "ui.html"), "r").read(), context)
 
-  # Register a custom page.
-  context.register_page("multipart-upload-demo", page_html)
+  '''
 
+  Register a custom page
+
+  Running this example code:
+
+    - Configure your web server to load the slycat-multipart-upload-demo plugin by adding it to the /etc/slycat/web-server-config.ini for the developer image
+    - Point a browser to https://your-slycat-server/pages/multipart-upload-demo
+    - Use the file chooser button to point your browser to a copy of slycat/data/cars.csv (demo is hard-coded to work with this file).
+    - Click “Upload”.
+    - The plugin code splits the file into two pieces, uploads them separately to a CCA model, and finishes.
+      - you can see this in the network analyzer from any browser
+    - Visit the new model to see that everything looks the way it should be.
+
+  '''
+  context.register_page("multipart-upload-demo", page_html)
