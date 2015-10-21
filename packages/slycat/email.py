97b87a4ce467d71a96f1bb0f554fd86d89690a40
srbdev <srberna@sandia.gov>
2015-10-21 14:22:57 -0600

    Fix #516

--- packages/slycat/email.py
+++ packages/slycat/email.py
@@ -1,8 +1,8 @@
 import subprocess
-import slycat.web.server
-
 
 def send_error(subject, body):
+  import slycat.web.server
+
   echo = subprocess.Popen(["echo", "\"%s\"" % body], stdout=subprocess.PIPE)
   mail = subprocess.Popen(["mail", "-s", "\"%s %s\"" % (slycat.web.server.config["slycat-web-server"]["error-email"]["subject"], subject), slycat.web.server.config["slycat-web-server"]["error-email"]["address"]], stdin=echo.stdout, stdout=subprocess.PIPE)
 
