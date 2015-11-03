78e047c8ea8e43c4d4f6d8f035d758c6129739d4
srbdev <srberna@sandia.gov>
2015-11-02 18:16:18 -0700

    Add additional check to maybe fix test errors

--- packages/slycat/email.py
+++ packages/slycat/email.py
@@ -3,7 +3,7 @@ import subprocess
 def send_error(subject, body):
   import slycat.web.server
 
-  if "error-email" in slycat.web.server.config["slycat-web-server"]:
+  if "slycat-web-server" in slycat.web.server.config and "error-email" in slycat.web.server.config["slycat-web-server"]:
     echo = subprocess.Popen(["echo", "\"%s\"" % body], stdout=subprocess.PIPE)
     mail = subprocess.Popen(["mail", "-s", "\"%s %s\"" % (slycat.web.server.config["slycat-web-server"]["error-email"]["subject"], subject), slycat.web.server.config["slycat-web-server"]["error-email"]["address"]], stdin=echo.stdout, stdout=subprocess.PIPE)
 
