40c5f878b2631ba25919f26d87b00446f561b288
Matthew Letter <mletter1@unm.edu>
2015-11-12 13:15:47 -0700

    adding new end to end docker agnostic test see #533

---
+++ end-to-end-tests/environment.py
@@ -0,0 +1,27 @@
+"""
+This file is used for the setup and breakdown of out testing environment
+"""
+
+__author__ = 'mletter'
+
+from selenium import webdriver
+import time
+
+def before_all(context):
+  """
+  befoer we run our tests we need to grab a browser
+  :param context: the object that will be passed to all our tests
+  :return: not used
+  """
+  context.browser = webdriver.Firefox()
+  context.browser.implicitly_wait(2)
+  context.server_url = 'https://slycat-dev.sandia.gov'
+
+
+def after_all(context):
+  """
+  we have a running browser so we need to kill it after we are done
+  :param context: the object that will be passed to all our tests
+  :return: not used
+  """
+  context.browser.quit()
