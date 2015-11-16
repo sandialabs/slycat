Fix backend for multiple image columns computation

web-server/plugins/slycat-parameter-image-plus-model/slycat-parameter-image-plus-model.py
@@ -79,117 +79,131 @@ def register_slycat_plugin(context):
   def compute(mid, image_columns_names):
     """Called in a thread to perform work on the model."""
     try:
-      for image_column_name in image_columns_names:
-        database = slycat.web.server.database.couchdb.connect()
+      database = slycat.web.server.database.couchdb.connect()
+      model = database.get("model", mid)
+
+      # Do useful work here
+      try:
+        clusters_model_file = slycat.web.server.get_model_file(database, model, "clusters")
+      except:
+        cluster_columns = slycat.web.server.get_model_parameter(database, model, "cluster-columns")
+        cluster_measure = slycat.web.server.get_model_parameter(database, model, "cluster-measure")
+        cluster_linkage = slycat.web.server.get_model_parameter(database, model, "cluster-linkage")
+        metadata = slycat.web.server.get_model_arrayset_metadata(database, model, "data-table")
+        column_infos = metadata[0]['attributes']
+        column_data = list(slycat.web.server.get_model_arrayset_data(database, model, "data-table", ".../.../..."))
+        columns = []
+
+        cherrypy.log.error("*** columns: ***")
+        for column_index, column_info in enumerate(column_infos):
+          cherrypy.log.error("*** %s ***" % column_info["name"])
+          cherrypy.log.error("*** column_index: %s ***" % column_index)
+          cherrypy.log.error("*** %s ***" % column_data[column_index])
+          columns.append((column_info['name'], column_data[column_index]))
+
+        csv_distance.matrix = list(slycat.web.server.get_model_arrayset_data(database, model, "distance-matrix-%s" % image_columns_names[0], ".../.../..."))
+
+        # Create a mapping from unique cluster names to column rows.
+        clusters = collections.defaultdict(list)
+        for column_index, (name, column) in enumerate(columns):
+          cherrypy.log.error("*** cluster name: %s ***" % name)
+          if name not in image_columns_names:
+            continue
+          for row_index, row in enumerate(column):
+            if row:
+              clusters[name].append((row_index, column_index))
+
+        # Compute a hierarchical clustering for each cluster column.
+        cluster_linkages = {}
+        cluster_exemplars = {}
+
+        for index, (name, storage) in enumerate(sorted(clusters.items())):
+
+          cherrypy.log.error("*** clusters items: %s ***" % name)
+
+          progress_begin = float(index) / float(len(clusters))
+          progress_end = float(index + 1) / float(len(clusters))
+
+          # Compute a distance matrix comparing every image to every other ...
+          observation_count = len(storage)
+          left, right = numpy.triu_indices(observation_count, k=1)
+          distance = compute_distance(left, right, storage, name, cluster_measure, measures[cluster_measure], columns)
+
+          # Use the distance matrix to cluster observations ...
+          print "Clustering %s" % name
+          #distance = scipy.spatial.distance.squareform(distance_matrix)
+          linkage = scipy.cluster.hierarchy.linkage(distance, method=str(cluster_linkage))
+          cluster_linkages[name] = linkage
+
+          # Identify exemplar waveforms for each cluster ...
+          distance_matrix = scipy.spatial.distance.squareform(distance)
+
+          summed_distances = numpy.zeros(shape=(observation_count))
+          exemplars = dict()
+          cluster_membership = []
+
+          for i in range(observation_count):
+            exemplars[i] = i
+            cluster_membership.append(set([i]))
+
+          print "Identifying examplars for %s" % (name)
+          for i in range(len(linkage)):
+            cluster_id = i + observation_count
+            (f_cluster1, f_cluster2, height, total_observations) = linkage[i]
+            cluster1 = int(f_cluster1)
+            cluster2 = int(f_cluster2)
+            # Housekeeping: assemble the membership of the new cluster
+            cluster_membership.append(cluster_membership[cluster1].union(cluster_membership[cluster2]))
+            # We need to update the distance from each member of the new
+            # cluster to all the other members of the cluster.  That means
+            # that for all the members of cluster1, we need to add in the
+            # distances to members of cluster2, and for all members of
+            # cluster2, we need to add in the distances to members of
+            # cluster1.
+            for cluster1_member in cluster_membership[cluster1]:
+              for cluster2_member in cluster_membership[cluster2]:
+                summed_distances[cluster1_member] += distance_matrix[cluster1_member][cluster2_member]
+
+            for cluster2_member in cluster_membership[int(cluster2)]:
+              for cluster1_member in cluster_membership[cluster1]:
+                summed_distances[cluster2_member] += distance_matrix[cluster2_member][cluster1_member]
+
+            min_summed_distance = None
+            max_summed_distance = None
+
+            exemplar_id = 0
+            for member in cluster_membership[cluster_id]:
+              if min_summed_distance is None or summed_distances[member] < min_summed_distance:
+                min_summed_distance = summed_distances[member]
+                exemplar_id = member
+
+              if max_summed_distance is None or summed_distances[member] > min_summed_distance:
+                max_summed_distance = summed_distances[member]
+
+            exemplars[cluster_id] = exemplar_id
+          cluster_exemplars[name] = exemplars
+
+        # Ingest the raw data into Slycat.
+        # Store an alphabetized collection of cluster names.
+        slycat.web.server.put_model_file(database, model, "clusters", value=json.dumps(sorted(clusters.keys())), content_type="application/json", input=True)
+
         model = database.get("model", mid)
 
-        # Do useful work here
-        try:
-          clusters_model_file = slycat.web.server.get_model_file(database, model, "clusters-%s" % image_column_name)
-        except:
-          cluster_columns = slycat.web.server.get_model_parameter(database, model, "cluster-columns")
-          cluster_measure = slycat.web.server.get_model_parameter(database, model, "cluster-measure")
-          cluster_linkage = slycat.web.server.get_model_parameter(database, model, "cluster-linkage")
-          metadata = slycat.web.server.get_model_arrayset_metadata(database, model, "data-table")
-          column_infos = metadata[0]['attributes']
-          column_data = list(slycat.web.server.get_model_arrayset_data(database, model, "data-table", ".../.../..."))
-          columns = []
-          for column_index, column_info in enumerate(column_infos):
-            columns.append((column_info['name'], column_data[column_index]))
+        # Store each cluster.
+        cherrypy.log.error("*** clusters keys: ***")
+        for key in clusters.keys():
+          cherrypy.log.error("*** key: %s ***" % key)
 
-          csv_distance.matrix = list(slycat.web.server.get_model_arrayset_data(database, model, "distance-matrix-%s" % image_column_name, ".../.../..."))
-
-          # Create a mapping from unique cluster names to column rows.
-          clusters = collections.defaultdict(list)
-          for column_index, (name, column) in enumerate(columns):
-            if name not in [cluster_columns]:
-              continue
-            for row_index, row in enumerate(column):
-              if row:
-                clusters[name].append((row_index, column_index))
-
-          # Compute a hierarchical clustering for each cluster column.
-          cluster_linkages = {}
-          cluster_exemplars = {}
-
-          for index, (name, storage) in enumerate(sorted(clusters.items())):
-            progress_begin = float(index) / float(len(clusters))
-            progress_end = float(index + 1) / float(len(clusters))
-
-            # Compute a distance matrix comparing every image to every other ...
-            observation_count = len(storage)
-            left, right = numpy.triu_indices(observation_count, k=1)
-            distance = compute_distance(left, right, storage, name, cluster_measure, measures[cluster_measure], columns)
-
-            # Use the distance matrix to cluster observations ...
-            print "Clustering %s" % name
-            #distance = scipy.spatial.distance.squareform(distance_matrix)
-            linkage = scipy.cluster.hierarchy.linkage(distance, method=str(cluster_linkage))
-            cluster_linkages[name] = linkage
-
-            # Identify exemplar waveforms for each cluster ...
-            distance_matrix = scipy.spatial.distance.squareform(distance)
-
-            summed_distances = numpy.zeros(shape=(observation_count))
-            exemplars = dict()
-            cluster_membership = []
-
-            for i in range(observation_count):
-              exemplars[i] = i
-              cluster_membership.append(set([i]))
-
-            print "Identifying examplars for %s" % (name)
-            for i in range(len(linkage)):
-              cluster_id = i + observation_count
-              (f_cluster1, f_cluster2, height, total_observations) = linkage[i]
-              cluster1 = int(f_cluster1)
-              cluster2 = int(f_cluster2)
-              # Housekeeping: assemble the membership of the new cluster
-              cluster_membership.append(cluster_membership[cluster1].union(cluster_membership[cluster2]))
-              # We need to update the distance from each member of the new
-              # cluster to all the other members of the cluster.  That means
-              # that for all the members of cluster1, we need to add in the
-              # distances to members of cluster2, and for all members of
-              # cluster2, we need to add in the distances to members of
-              # cluster1.
-              for cluster1_member in cluster_membership[cluster1]:
-                for cluster2_member in cluster_membership[cluster2]:
-                  summed_distances[cluster1_member] += distance_matrix[cluster1_member][cluster2_member]
-
-              for cluster2_member in cluster_membership[int(cluster2)]:
-                for cluster1_member in cluster_membership[cluster1]:
-                  summed_distances[cluster2_member] += distance_matrix[cluster2_member][cluster1_member]
-
-              min_summed_distance = None
-              max_summed_distance = None
-
-              exemplar_id = 0
-              for member in cluster_membership[cluster_id]:
-                if min_summed_distance is None or summed_distances[member] < min_summed_distance:
-                  min_summed_distance = summed_distances[member]
-                  exemplar_id = member
-
-                if max_summed_distance is None or summed_distances[member] > min_summed_distance:
-                  max_summed_distance = summed_distances[member]
-
-              exemplars[cluster_id] = exemplar_id
-            cluster_exemplars[name] = exemplars
-
-          # Ingest the raw data into Slycat.
-          # Store an alphabetized collection of cluster names.
-          slycat.web.server.put_model_file(database, model, "clusters-%s" % image_column_name, value=json.dumps(sorted(clusters.keys())), content_type="application/json", input=True)
-
+          database = slycat.web.server.database.couchdb.connect()
           model = database.get("model", mid)
 
-          # Store each cluster.
-          for key in clusters.keys():
-            slycat.web.server.put_model_file(
-              database,
-              model,
-              "cluster-%s" % key,
-              value=json.dumps({"linkage" : cluster_linkages[key].tolist(), "exemplars" : cluster_exemplars[key], "input-indices" : [row_index for row_index, column_index in clusters[key]],}),
-              content_type="application/json",
-              input=True)
+          slycat.web.server.put_model_file(
+            database,
+            model,
+            "cluster-%s" % key,
+            value=json.dumps({"linkage" : cluster_linkages[key].tolist(), "exemplars" : cluster_exemplars[key], "input-indices" : [row_index for row_index, column_index in clusters[key]],}),
+            content_type="application/json",
+            input=True)
 
       model = database.get("model", mid)
       slycat.web.server.update_model(database, model, state="finished", result="succeeded", finished=datetime.datetime.utcnow().isoformat(), progress=1.0, message="")