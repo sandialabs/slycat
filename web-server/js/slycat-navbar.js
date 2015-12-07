7a4c049775d98e1bd4434a526d5ba390fe1baf6d
Alex M. Sielicki <alex@techmilo.com>
2015-12-07 23:51:10 +0000

    Cleaning up sign out code.

--- web-server/js/slycat-navbar.js
+++ web-server/js/slycat-navbar.js
@@ -390,8 +390,14 @@ define("slycat-navbar", ["slycat-server-root", "slycat-web-client", "slycat-chan
       component.update_references();
       component.sign_out = function()
       {
-        window.alert("hello");
-        client.sign_out({success:function(){window.alert("you are logged out");},error: function(){window.alert("something went wrong");}})
+        client.sign_out({ 
+          success: function(){ 
+            window.location.href = "/";
+          }, 
+          error: function(){ 
+            window.alert("Sorry, something went wrong and you are not signed out."); 
+          } 
+        })
       }
 
     },
