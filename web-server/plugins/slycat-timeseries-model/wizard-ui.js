d12b7e8f404ca6eada3ac7ccc818968e4f649648
srbdev <srberna@sandia.gov>
2016-02-02 11:53:56 -0700

    Start on update to timeseries wizard to compute hdf5 files

--- web-server/plugins/slycat-timeseries-model/wizard-ui.js
+++ web-server/plugins/slycat-timeseries-model/wizard-ui.js
@@ -5,6 +5,11 @@ define(['slycat-server-root', 'slycat-web-client', 'slycat-dialog', 'knockout', 
     component.tab = ko.observable(0);
     component.project = params.projects()[0];
     component.model = mapping.fromJS({ _id: null, name: 'New Timeseries Model', description: '', marking: null });
+    component.is_compute_hdf5 = ko.observable('compute');
+    component.remote = mapping.fromJS({hostname: null, username: null, password: null, status: null, status_type: null, enable: ko.computed(function(){return component.is_compute_hdf5() == 'compute' ? true : false;}), focus: false, sid: null});
+    component.remote.focus.extend({notify: "always"});
+    component.browser = mapping.fromJS({path:null, selection: []});
+    component.csv_url = ko.observable('');
     component.directory = ko.observable('');
     component.cluster_sample_count = ko.observable(1000);
     component.cluster_sample_type = ko.observableArray(['uniform-paa', 'uniform-pla']);
@@ -31,15 +36,51 @@ define(['slycat-server-root', 'slycat-web-client', 'slycat-dialog', 'knockout', 
       });
     };
 
+    component.connect = function() {
+      component.remote.status_type("info");
+      component.remote.status("Connecting ...");
+      client.post_remotes({
+        hostname: component.remote.hostname(),
+        username: component.remote.username(),
+        password: component.remote.password(),
+        success: function(sid) {
+          component.remote.sid(sid);
+          component.tab(2);
+        },
+        error: function(request, status, reason_phrase) {
+          component.remote.status_type("danger");
+          component.remote.status(reason_phrase);
+          component.remote.focus("password");
+        }
+      });
+    };
+
+    component.select_compute = function() {
+      var c = component.is_compute_hdf5();
+
+      if (c === 'compute') {
+        $(".modal-dialog").addClass("modal-lg");
+        $(".ps-tab-remote-data").css("display", "block");
+        component.connect();
+      } else if (c === 'no-compute')
+        component.tab(3);
+    };
+
     component.process_parameters = function() {
       component.put_model_parameters();
-      component.tab(2);
+      component.tab(4);
     };
 
     component.go_to_model = function() {
       location = server_root + 'models/' + component.model._id();
     };
 
+    component.load_csv = function() {
+      $('.remote-browser-continue-data').toggleClass("disabled", true);
+      component.csv_url(component.browser.selection()[0]);
+      console.log(component.csv_url());
+      component.tab(3);
+    };
 
     component.put_model_parameters = function(callback) {
       client.put_model_parameter({
@@ -87,7 +128,7 @@ define(['slycat-server-root', 'slycat-web-client', 'slycat-dialog', 'knockout', 
     };
 
     component.finish = function() {
-      component.tab(3);
+      component.tab(5);
     };
 
     return component;
