ff17aef917a1ba4f497a400e28c151ce164ac863
Matthew Letter <mletter1@unm.edu>
2016-03-16 11:11:40 -0600

    adding sandia cr

--- web-server/slycat-login/js/app.js
+++ web-server/slycat-login/js/app.js
@@ -1,5 +1,7 @@
 /*
-  created by mletter
+  Copyright 2016, Sandia Corporation. Under the terms of Contract
+  DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
+  rights in this software.
  */
 require(["jquery", "URI"], function($, URI)
 {
