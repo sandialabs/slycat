ec4e63da47d74ec61827e366e66d9d713ea5609b
Matthew Letter <mletter1@unm.edu>
2016-05-17 09:55:14 -0600

    finished random csv sampler

--- web-client/slycat-random-csv-output.py
+++ web-client/slycat-random-csv-output.py
@@ -1,25 +1,30 @@
-import paramiko
-import re
+"""
+This script is designed to take a csv file and randomly selected x number of
+rows based on --random_sample_size with a default of 10
+"""
 import csv
 import slycat.web.client
 import random
-import getpass
-import cPickle as pickle
 
 parser = slycat.web.client.ArgumentParser()
 parser.add_argument("file", default="-", help="Input CSV file.  Default: %(default)s")
-parser.add_argument("--random_sample", default=22, help="port number.  Default: %(default)s")
-parser.add_argument("--output_file_name", default="_random_samples.csv", help="cpickle output csv file.  Default: %(default)s")
+parser.add_argument("--random_sample_size", default=10, help="number of randomly selected rows to output.  Default: %(default)s")
+parser.add_argument("--output_file_name", default="_random_samples.csv", help="output csv file name.  Default: %(default)s")
 arguments = parser.parse_args()
 
 file_list=[]
+print "opening input csv file"
 with open(arguments.file) as csvfile:
     reader = csv.DictReader(csvfile)
-    with open((str(arguments.random_sample)+arguments.output_file_name), 'w') as csv_output_file:
+    print "opening output csv file"
+    with open((str(arguments.random_sample_size)+arguments.output_file_name), 'w') as csv_output_file:
         writer = csv.DictWriter(csv_output_file, fieldnames=reader.fieldnames)
         writer.writeheader()
+        rows = []
         for row in reader:
-            writer.writerow(row)
-
-    # for row in reader:
-    #     print ', '.join(reader.fieldnames)
+            rows.append(row)
+        random.shuffle(rows)
+        print "writing out to output csv file"
+        for x in range(arguments.random_sample_size):
+            writer.writerow(rows[x])
+print "output file was named %s" % (str(arguments.random_sample_size)+arguments.output_file_name)
