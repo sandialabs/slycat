1dacf136b0f58517e46a46298af4b532e52e6d38
Matthew Letter <mletter1@unm.edu>
2016-05-17 09:28:40 -0600

    cleaning up new random csv selection script

--- web-client/slycat-random-csv-output.py
+++ web-client/slycat-random-csv-output.py
@@ -2,6 +2,7 @@ import paramiko
 import re
 import csv
 import slycat.web.client
+import random
 import getpass
 import cPickle as pickle
 
@@ -11,88 +12,10 @@ parser.add_argument("--random_sample", default=22, help="port number.  Default: 
 parser.add_argument("--output_file_name", default="_random_sample.csv", help="cpickle output csv file.  Default: %(default)s")
 arguments = parser.parse_args()
 
-# with open(filename, 'wb') as fp:
-# #   pickle.dump(self.fitResults, fp)
-# try:
-#     print "trying to find old file"
-#     with open(arguments.temp_file, 'rb') as fp:
-#       temp_dict = pickle.load(fp)
-# except IOError:
-#     print "failed to find "
-#     temp_dict = None
-#
-# if temp_dict:
-#     print "pre calculated dict found"
-#     # print temp_dict
-# else:
-#     temp_dict = {}
-#
-# print "host: ", arguments.host
-# print "port: ", arguments.port
-# print "file_path: ",arguments.file
-# print "user: ",arguments.user
-#
-# # print "password: ", arguments.password
-# expression = re.compile("file://")
-# if "file_list" not in temp_dict:
-#     print "file_list not in pickle file"
-#     file_list=[]
-#     with open(arguments.file) as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         for row in reader:
-#             for item in row:
-#                 if bool(expression.search(row[item])):
-#                     file_list.append(re.sub(r'file:\/\/[a-zA-Z]*\/', "/", row[item]))
-#
-#     with open(arguments.temp_file, 'wb') as fp:
-#         print "writing out file list"
-#         temp_dict["file_list"]=file_list
-#         pickle.dump(temp_dict, fp)
-#
-# print "number of files ", len(temp_dict["file_list"])
-# # see if we have a file size map
-# if "file_size_map" in temp_dict:
-#     print "file size map found in pickle"
-#     file_size_map = temp_dict["file_size_map"]
-# else:
-#     file_size_map = {}
-#
-#
-# size = 0
-# if arguments.password is None:
-#     arguments.password = getpass.getpass("%s password: " % arguments.user)
-# try:
-#     client = paramiko.SSHClient()
-#     client.load_system_host_keys()
-#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#     client.connect(arguments.host, username=arguments.user, password=arguments.password, port=int(arguments.port))
-#     not_in_count = 1
-#     in_count = 0
-#     for path in temp_dict["file_list"]:
-#         if path not in file_size_map:
-#             not_in_count += 1
-#             stdin, stdout, stderr = client.exec_command("ls -l %s | cut -d \" \" -f5" % path)
-#             # print path
-#             for line in stdout:
-#                 file_size_map[path] = int(line.strip('\n'))
-#                 size = size + int(line.strip('\n'))
-#                 # print int(line.strip('\n'))
-#         else:
-#             size = size + file_size_map[path]
-#             in_count += 1
-#         if ((in_count + not_in_count) % 100) == 0:
-#             print "in count:%s not in count: %s" %(in_count, not_in_count)
-#
-#         if ((not_in_count) % 100) == 0:
-#             print "writing out temp_dict"
-#             with open(arguments.temp_file, 'wb') as fp:
-#                 temp_dict["file_size_map"] = file_size_map
-#                 pickle.dump(temp_dict, fp)
-#
-# except KeyboardInterrupt as e:
-#     print "writing out temp_dict"
-#     with open(arguments.temp_file, 'wb') as fp:
-#         temp_dict["file_size_map"] = file_size_map
-#         pickle.dump(temp_dict, fp)
-#
-# print size, " bytes"
+file_list=[]
+with open(arguments.file) as csvfile:
+    reader = csv.DictReader(csvfile)
+    with open('names.csv', 'w') as csv_output_file:
+        pass
+    for row in reader:
+        print ', '.join(row)
