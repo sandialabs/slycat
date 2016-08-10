b54964c1acc75d9772272350a9815cd1cd72e197
Jonathan Compton <JECompton@gmail.com>
2016-08-10 16:55:21 -0600

    test: write tests to check for correct uri parsing

--- test/uri_test.py
+++ test/uri_test.py
@@ -1,11 +1,46 @@
-import unittest
+import pytest
 from uri import URI
 
-class TestSlycatUri(unittest.TestCase):
-  #def test_toString_same_as_valueOf(self):
-   def test_create_uri(self):
-     u = URI()
-     self.assertEqual(u.toString(), "http://example.com")
-#     u = URI("http://example.com")
-#     self.assertEqual(u.toString(), "http://example.com")
+auth_uri = URI('http://some_user:some_password@example.com:8080/foo?bar=baz')
+
+def test_create_uri():
+  simple_uri = URI('http://example.com/foo?bar=baz')
+  assert simple_uri.toString() == 'http://example.com/foo?bar=baz'
+
+def test_toString_same_as_valueOf():
+  assert auth_uri.toString() == 'http://some_user:some_password@example.com:8080/foo?bar=baz'
+
+def test_valueOf_returns_same_as_toString():
+  assert auth_uri.valueOf() == auth_uri.toString()
+
+def test_protocol_parsed():
+  assert auth_uri.protocol() == 'http'
+
+def test_scheme_is_same_as_protocol():
+  assert auth_uri.scheme() == auth_uri.protocol()
+
+def test_username_parsed():
+  assert auth_uri.username() == 'some_user'
+
+def test_password_parsed():
+  assert auth_uri.password() == 'some_password'
+
+def test_hostname_parsed():
+  assert auth_uri.hostname() == 'example.com'
+
+def test_port_parsed():
+  assert auth_uri.port() == '8080'
+
+def test_removing_search_value_from_uri():
+  uri = URI('http://example.com/foo?bar=baz')
+  assert uri.removeSearch(['bar']).toString() == 'http://example.com/foo'
+
+# Why does this sitll have the '?' and the previous test doesn't?
+def test_removing_search_value_with_key_from_uri():
+  uri = URI('http://example.com/foo?bar=baz')
+  assert uri.removeSearch(['bar'], 'baz').toString() == 'http://example.com/foo'
+
+def test_removeQuery_is_same_as_removeSearch():
+  uri = URI('http://example.com/foo?bar=baz')
+  assert uri.removeQuery(['bar']).toString() == uri.removeSearch(['bar']).toString()
 
