9af41b3414d2f91eae5942f347a8b48ad3fe397c
Jonathan Compton <JECompton@gmail.com>
2016-08-25 09:19:38 -0600

    pull in stashed tests

--- test/hdf5_test.py
+++ test/hdf5_test.py
@@ -2,3 +2,10 @@ import pytest
 import hdf5
 
 ## DArray tests
+#class TestDarrayProperties:
+#  def setup(self):
+#    # Need to learn about slycat.darray first to pass it to init...
+#    self.darray = DArray(slycat darray goes here)
+#
+#  def test_set_protocol(self):
+#    assert str(self.uri.protocol(value='https')) == 'https://some_user:some_password@example.com:8080/foo?bar=baz#anchor'
