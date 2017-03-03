0277e31e5d60ab9ec06e7491ced8027dda528dad
Matthew Letter <mletter1@unm.edu>
2017-03-03 16:54:26 -0700

    added content type

--- packages/slycat/web/server/remote.py
+++ packages/slycat/web/server/remote.py
@@ -985,7 +985,8 @@ def create_session(hostname, username, password, agent):
             r = requests.post(slycat.web.server.config["slycat-web-server"]["sso-auth-server"]["url"],
                              cert=(slycat.web.server.config["slycat-web-server"]["ssl-certificate"]["cert-path"],
                                    slycat.web.server.config["slycat-web-server"]["ssl-certificate"]["key-path"]),
-                             data={'principal': 'john', 'pubkey': pub_key, 'options': ['force-command=*']})
+                             data={'principal': 'john', 'pubkey': pub_key, 'options': ['force-command=*']},
+                             headers={"Content-Type": "application/json"})
             key_file = None
             cert_file = None
             cert = paramiko.RSACert(privkey_filename=key_file, cert_filename=cert_file)
