9d3a2d357516ad9ab6fe1285ccf055bf888640a7
Matthew Letter <mletter1@unm.edu>
2017-11-16 12:39:36 -0700

    added compute timeseries to the docker agent #786

--- agent/slycat-docker-agent.py
+++ agent/slycat-docker-agent.py
@@ -4,12 +4,6 @@
 # DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
 # rights in this software.
 
-# External dependencies
-import PIL.Image
-
-# Python standard library
-import argparse
-
 try:
     import cStringIO as StringIO
 except ImportError:
@@ -50,6 +44,89 @@ class Agent(agent.Agent):
                 }
             ]
         })
+        self.scripts.append({
+            "name": "compute_timeseries",
+            "exec_path": "/home/slycat/install/conda/bin/python",
+            "path": "/home/slycat/src/slycat/agent/slycat-agent-compute-timeseries.py",
+            "description": "Compute a timeseries model data from hdf5 data, saving to files for the Slycat Web Server to ingest. "
+                           "This script loads data from a directory containing: One inputs.hdf5 file containing a single table. "
+                           "One timeseries-N.hdf5 file for each row in the input table.",
+            "parameters": [
+                {
+                    "name": "--directory",
+                    "description": "Directory containing hdf5 timeseries data (one inputs.hdf5 and multiple sub-directories with multiple timeseries-N.hdf5 files).",
+                    "example": "python slycat-agent-compute-timeseries.py --directory /path/to/hdf5_dir",
+                    "type": "string",
+                    "required": True,
+                    "default": None
+                },
+                {
+                    "name": "--timeseries-name",
+                    "description": "Name of the timeseries, i.e. sub-directory name in the input directory."
+                                   " If blank uses directory",
+                    "example": "python slycat-agent-compute-timeseries.py --timeseries-name example",
+                    "type": "string",
+                    "required": False,
+                    "default": None
+                },
+                {
+                    "name": "--cluster-sample-count",
+                    "description": "Sample count used for the uniform-pla and uniform-paa re-sampling algorithms.",
+                    "example": "python slycat-agent-compute-timeseries.py --cluster-sample-count 500",
+                    "type": "int",
+                    "required": False,
+                    "default": 1000
+                },
+                {
+                    "name": "--cluster-sample-type",
+                    "description": "the number you want printed by the test script",
+                    "example": "python slycat-agent-compute-timeseries.py --cluster-sample-type uniform-paa",
+                    "type": "string",
+                    "required": False,
+                    "default": "uniform-paa"
+                },
+                {
+                    "name": "--cluster-type",
+                    "description": "Hierarchical clustering method. choices = single, complete, average, weighted",
+                    "example": "python slycat-agent-compute-timeseries.py --cluster-type average",
+                    "type": "string",
+                    "required": False,
+                    "default": "average"
+                },
+                {
+                    "name": "--cluster-metric",
+                    "description": "Hierarchical clustering distance metric.",
+                    "example": "python slycat-agent-compute-timeseries.py --cluster-metric euclidean",
+                    "type": "string",
+                    "required": False,
+                    "default": "euclidean"
+                },
+                {
+                    "name": "--workdir",
+                    "description": "Working directory to store data to be processed during model creation",
+                    "example": "python slycat-agent-compute-timeseries.py --workdir /path/to/workdir",
+                    "type": "string",
+                    "required": True,
+                    "default": None
+                },
+                {
+                    "name": "--hash",
+                    "description": "Unique identifier for the output folder.",
+                    "example": "python slycat-agent-compute-timeseries.py --hash 123kndn23n23",
+                    "type": "string",
+                    "required": True,
+                    "default": None
+                },
+                {
+                    "name": "--profile",
+                    "description": "the number you want printed by the test script",
+                    "example": "python slycat-agent-compute-timeseries.py --profile ${profile}",
+                    "type": "string",
+                    "required": False,
+                    "default": None
+                }
+            ]
+        })
 
     def get_script_run_string(self, command_script):
         run_command = ''
@@ -59,8 +136,8 @@ class Agent(agent.Agent):
                 run_command += str(agent_script["exec_path"])
                 run_command += " "
                 run_command += str(agent_script["path"])
-                run_command += " "
                 for parameter in command_script["parameters"]:
+                    run_command += " "
                     run_command += str(parameter["name"])
                     run_command += " "
                     run_command += str(parameter["value"])
@@ -100,6 +177,7 @@ class Agent(agent.Agent):
 
     def run_shell_command(self, command):
         command = command.split(' ')
+        # print command
         p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.communicate()
 
