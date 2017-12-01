31562898d5534761c34541a684584bc71b96c889
Matthew Letter <mletter1@unm.edu>
2017-12-01 16:24:20 -0700

    adding better up script for the web server

--- open-source-docker/docker/open-source-build/slycat-base/run-slycat.sh
+++ open-source-docker/docker/open-source-build/slycat-base/run-slycat.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
-
 #
 #
 
+service sshd start
+sleep 2
 service rsyslog start
 service couchdb start
-sleep 1
+sleep 2
 /home/slycat/install/conda/bin/python /home/slycat/src/slycat/web-server/slycat-couchdb-setup.py
-service slycat-web-server start
+sleep 2
 service haproxy start
+service slycat-web-server start
+
+
 
 while [ 0 ]
 do
   sleep 4
+
+  if ! pgrep -x "sshd" > /dev/null
+  then
+ 	service rsyslog start
+	service couchdb start
+	sleep 1
+	/home/slycat/install/conda/bin/python /home/slycat/src/slycat/web-server/slycat-couchdb-setup.py
+	service slycat-web-server start
+	service haproxy start
+	service sshd start
+  fi
 
   service slycat-web-server status
   retval=$?
