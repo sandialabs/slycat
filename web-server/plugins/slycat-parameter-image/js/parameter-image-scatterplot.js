8d4187b0768331266fb12e8641240fa75f08c1b9
Alex M. Sielicki <alex@techmilo.com>
2018-01-09 14:03:16 -0700

    Removing debugging code

--- web-server/plugins/slycat-parameter-image/js/parameter-image-scatterplot.js
+++ web-server/plugins/slycat-parameter-image/js/parameter-image-scatterplot.js
@@ -2116,7 +2116,6 @@ define("slycat-parameter-image-scatterplot", ["slycat-server-root", "d3", "URI",
       // Increment highest_z_index and assign it to the current frame
       self.options.highest_z_index++;
       frame.css("z-index", self.options.highest_z_index);
-      frame.addClass("z_index_" + self.options.highest_z_index);
 
       $(".open-image").removeClass("selected");
       frame.addClass("selected");
