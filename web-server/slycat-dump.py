97d64c3d861162213bcfc49960eea0015f1abcbf
Matthew Letter <mletter1@unm.edu>
2018-04-09 11:35:11 -0600

    updated the server code to properly copy references
    we needed to add bookmarks and references to the database copy function for the migration to our new couch 2.0 implementation

--- web-server/slycat-dump.py
+++ web-server/slycat-dump.py
@@ -18,9 +18,11 @@ parser.add_argument("--all", action="store_true", help="Dump all projects.")
 parser.add_argument("--couchdb-database", default="slycat", help="CouchDB database.  Default: %(default)s")
 parser.add_argument("--couchdb-host", default="localhost", help="CouchDB host.  Default: %(default)s")
 parser.add_argument("--couchdb-port", type=int, default=5984, help="CouchDB port.  Default: %(default)s")
-parser.add_argument("--data-store", default="data-store", help="Path to the hdf5 data storage directory.  Default: %(default)s")
+parser.add_argument("--data-store", default="data-store",
+                    help="Path to the hdf5 data storage directory.  Default: %(default)s")
 parser.add_argument("--force", action="store_true", help="Overwrite existing data.")
-parser.add_argument("--project-id", default=[], action="append", help="Project ID to dump.  You may specify --project-id multiple times.")
+parser.add_argument("--project-id", default=[], action="append",
+                    help="Project ID to dump.  You may specify --project-id multiple times.")
 arguments = parser.parse_args()
 
 logging.getLogger().setLevel(logging.INFO)
@@ -28,48 +30,65 @@ logging.getLogger().addHandler(logging.StreamHandler())
 logging.getLogger().handlers[0].setFormatter(logging.Formatter("{} - %(levelname)s - %(message)s".format(sys.argv[0])))
 
 if arguments.force and os.path.exists(arguments.output_dir):
-  shutil.rmtree(arguments.output_dir)
+    shutil.rmtree(arguments.output_dir)
 if os.path.exists(arguments.output_dir):
-  raise Exception("Output directory already exists.")
+    raise Exception("Output directory already exists.")
 
 couchdb = couchdb.Server()[arguments.couchdb_database]
 
 os.makedirs(arguments.output_dir)
 
 if arguments.all:
-  arguments.project_id = set(arguments.project_id + [row["id"] for row in couchdb.view("slycat/projects")])
+    arguments.project_id = set(arguments.project_id + [row["id"] for row in couchdb.view("slycat/projects")])
+
+bookmark_ids = list(row["id"] for row in couchdb.view("slycat/project-bookmarks"))
+reference_ids = list(row["id"] for row in couchdb.view("slycat/references"))
 
 logging.info("Dumping bookmarks")
-for bookmark in couchdb.view("slycat/project-bookmarks"):
-  json.dump(bookmark, open(os.path.join(arguments.output_dir, "bookmark-%s.json" % bookmark["id"]), "w"))
+for bookmark_id in bookmark_ids:
+    print bookmark_id
+    bookmark = couchdb.get(bookmark_id, attachments=True)
+    print bookmark
+    json.dump(bookmark, open(os.path.join(arguments.output_dir, "bookmark-%s.json" % bookmark["_id"]), "w"))
 logging.info("Done with bookmarks")
 
+logging.info("Dumping references")
+for reference_id in reference_ids:
+    print reference_id
+    reference = couchdb.get(reference_id, attachments=True)
+    print reference
+    json.dump(reference, open(os.path.join(arguments.output_dir, "reference-%s.json" % reference["_id"]), "w"))
+logging.info("Done with references")
 
 for project_id in arguments.project_id:
-  logging.info("Dumping project %s", project_id)
-  project = couchdb.get(project_id, attachments=True)
-  json.dump(project, open(os.path.join(arguments.output_dir, "project-%s.json" % project["_id"]), "w"))
+    logging.info("Dumping project %s", project_id)
+    project = couchdb.get(project_id, attachments=True)
+    json.dump(project, open(os.path.join(arguments.output_dir, "project-%s.json" % project["_id"]), "w"))
 
-  project_arrays = set()
+    project_arrays = set()
 
-  for row in couchdb.view("slycat/project-models", startkey=project_id, endkey=project_id):
-    logging.info("Dumping model %s", row["id"])
-    model = couchdb.get(row["id"], attachments=True)
-    json.dump(model, open(os.path.join(arguments.output_dir, "model-%s.json" % model["_id"]), "w"))
+    for row in couchdb.view("slycat/project-models", startkey=project_id, endkey=project_id):
+        logging.info("Dumping model %s", row["id"])
+        model = couchdb.get(row["id"], attachments=True)
+        json.dump(model, open(os.path.join(arguments.output_dir, "model-%s.json" % model["_id"]), "w"))
 
-    artifact_types = model["artifact-types"]
-    for key, value in model.items():
-      if not key.startswith("artifact:"):
-        continue
-      artifact_name = key[9:]
-      artifact_type = artifact_types[artifact_name]
-      if artifact_type == "hdf5":
-        if value not in project_arrays:
-          logging.info("Dumping array set %s", value)
-          project_arrays.add(value)
-          shutil.copy(slycat.hdf5.path(value, arguments.data_store), os.path.join(arguments.output_dir, "array-set-%s.hdf5" % value))
-      elif artifact_type in ["file", "json"]:
-        continue # file artifacts are stored as document attachments, and json artifacts are stored document directly, so we don't have to do anything for them.
-      else:
-        logging.warning("Skipping unsupported artifact type: %s %s", artifact_name, artifact_type)
-
+        artifact_types = model["artifact-types"]
+        for key, value in model.items():
+            if not key.startswith("artifact:"):
+                continue
+            artifact_name = key[9:]
+            artifact_type = artifact_types[artifact_name]
+            if artifact_type == "hdf5":
+                if value not in project_arrays:
+                    logging.info("Dumping array set %s", value)
+                    project_arrays.add(value)
+                    try:
+                        shutil.copy(slycat.hdf5.path(value, arguments.data_store),
+                                    os.path.join(arguments.output_dir, "array-set-%s.hdf5" % value))
+                    except IOError:
+                        logging.error("is the data store directory correct for hdf5 files? we couldn't find the file")
+                        raise Exception("hdf5 file not found")
+            elif artifact_type in ["file", "json"]:
+                continue  # file artifacts are stored as document attachments, and json artifacts are stored document directly, so we don't have to do anything for them.
+            else:
+                logging.warning("Skipping unsupported artifact type: %s %s", artifact_name, artifact_type)
--- web-server/slycat-load.py
+++ web-server/slycat-load.py
@@ -18,9 +18,11 @@ parser.add_argument("input_dir", help="Directory containing data dumped with sly
 parser.add_argument("--couchdb-database", default="slycat", help="CouchDB database.  Default: %(default)s")
 parser.add_argument("--couchdb-host", default="localhost", help="CouchDB host.  Default: %(default)s")
 parser.add_argument("--couchdb-port", type=int, default=5984, help="CouchDB port.  Default: %(default)s")
-parser.add_argument("--data-store", default="data-store", help="Path to the hdf5 data storage directory.  Default: %(default)s")
+parser.add_argument("--data-store", default="data-store",
+                    help="Path to the hdf5 data storage directory.  Default: %(default)s")
 parser.add_argument("--force", action="store_true", help="Overwrite existing data.")
-parser.add_argument("--marking", default=[], nargs="+", help="Use --marking='<source>:<target>' to map <source> markings to <target> markings.  You may specify multiple maps, separated by whitespace.")
+parser.add_argument("--marking", default=[], nargs="+",
+                    help="Use --marking='<source>:<target>' to map <source> markings to <target> markings.  You may specify multiple maps, separated by whitespace.")
 arguments = parser.parse_args()
 
 logging.getLogger().setLevel(logging.INFO)
@@ -35,41 +37,50 @@ couchdb = couchdb.Server()[arguments.couchdb_database]
 
 # Load projects ...
 for source in glob.glob(os.path.join(arguments.input_dir, "project-*.json")):
-  logging.info("Loading project %s", source)
-  project = json.load(open(source))
-  del project["_rev"]
-  if arguments.force and project["_id"] in couchdb:
-    del couchdb[project["_id"]]
-  couchdb.save(project)
+    logging.info("Loading project %s", source)
+    project = json.load(open(source))
+    del project["_rev"]
+    if arguments.force and project["_id"] in couchdb:
+        del couchdb[project["_id"]]
+    couchdb.save(project)
 
 # Load models ...
 for source in glob.glob(os.path.join(arguments.input_dir, "model-*.json")):
-  logging.info("Loading model %s", source)
-  model = json.load(open(source))
-  del model["_rev"]
-  if model["marking"] in markings:
-    model["marking"] = markings[model["marking"]]
-  if arguments.force and model["_id"] in couchdb:
-    del couchdb[model["_id"]]
-  couchdb.save(model)
+    logging.info("Loading model %s", source)
+    model = json.load(open(source))
+    del model["_rev"]
+    if model["marking"] in markings:
+        model["marking"] = markings[model["marking"]]
+    if arguments.force and model["_id"] in couchdb:
+        del couchdb[model["_id"]]
+    couchdb.save(model)
 
 # Load array sets ...
 for source in glob.glob(os.path.join(arguments.input_dir, "array-set-*.hdf5")):
-  logging.info("Loading array set %s", source)
-  array = re.match(r".*array-set-(.*)\.hdf5", source).group(1)
-  if arguments.force and array in couchdb:
-    del couchdb[array]
-  couchdb.save({"_id":array, "type":"hdf5"})
-  destination = slycat.hdf5.path(array, arguments.data_store)
-  if not os.path.exists(os.path.dirname(destination)):
-    os.makedirs(os.path.dirname(destination))
-  shutil.copy(source, destination)
+    logging.info("Loading array set %s", source)
+    array = re.match(r".*array-set-(.*)\.hdf5", source).group(1)
+    if arguments.force and array in couchdb:
+        del couchdb[array]
+    couchdb.save({"_id": array, "type": "hdf5"})
+    destination = slycat.hdf5.path(array, arguments.data_store)
+    if not os.path.exists(os.path.dirname(destination)):
+        os.makedirs(os.path.dirname(destination))
+    shutil.copy(source, destination)
 
 # Load bookmarks ...
 for source in glob.glob(os.path.join(arguments.input_dir, "boomark-*.json")):
-  logging.info("Loading boomark %s", source)
-  boomark = json.load(open(source))
-  del boomark["_rev"]
-  if arguments.force and boomark["_id"] in couchdb:
-    del couchdb[boomark["_id"]]
-  couchdb.save(boomark)
+    logging.info("Loading boomark %s", source)
+    boomark = json.load(open(source))
+    del boomark["_rev"]
+    if arguments.force and boomark["_id"] in couchdb:
+        del couchdb[boomark["_id"]]
+    couchdb.save(boomark)
+
+# Load bookmarks ...
+for source in glob.glob(os.path.join(arguments.input_dir, "reference-*.json")):
+    logging.info("Loading references %s", source)
+    reference = json.load(open(source))
+    del reference["_rev"]
+    if arguments.force and reference["_id"] in couchdb:
+        del couchdb[reference["_id"]]
+    couchdb.save(reference)
