5b6d972a24ebda2b13eb53fa8b3488eccd2731da
Matthew Letter <mletter1@unm.edu>
2018-04-19 14:45:53 -0600

    removing comments from json files
    json files can't have comments since they are data files

--- agent/json/compute_timeseries.js
+++ agent/json/compute_timeseries.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract
-// DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
-// retains certain rights in this software.
-
 {
 	"name": "compute_timeseries",
 	"exec_path": "/home/slycat/install/conda/bin/python",
