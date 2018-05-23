65d563ccf0f05de014499fe39226ffc2901f63c7
Gittinger <jgittin@s1025794.srn.sandia.gov>
2018-05-23 17:18:01 -0600

    Fixing dropdown component...something weird happened with previous commit

--- web-server/plugins/slycat-parameter-image/js/Components/controls-dropdown.js
+++ web-server/plugins/slycat-parameter-image/js/Components/controls-dropdown.js
@@ -1,13 +1,31 @@
-Unstaged changes after reset:
-M	web-server/dist/ui_parameter_image.js
-M	web-server/dist/webpack-bundle-analyzer-report.html
-M	web-server/dist/webpack-bundle-analyzer-stats.json
-M	web-server/dist/webpack-visualizer-stats.html
-M	web-server/plugins/slycat-parameter-image/js/Components/controls-dropdown.js
-M	web-server/plugins/slycat-parameter-image/js/Components/controls-group.js
-M	web-server/plugins/slycat-parameter-image/js/Components/controls-playback.js
-M	web-server/plugins/slycat-parameter-image/js/Components/controls-selection.js
-M	web-server/plugins/slycat-parameter-image/js/Components/controls-video.js
-M	web-server/plugins/slycat-parameter-image/js/color-switcher.js
-D	web-server/plugins/slycat-parameter-image/js/controls-dropdown.js
-M	web-server/plugins/slycat-parameter-image/js/parameter-controls.js
+import React from "react";
+
+class ControlsDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let optionItems = this.props.items.map((item) =>
+      <li role='presentation' key={item.key} className={item.key == this.props.selected ? 'active' : ''}>
+        <a role="menuitem" tabIndex="-1" onClick={(e) => this.props.set_selected(this.props.state_label, item.key, this.props.trigger, e)}>
+          {item.name}
+        </a>
+      </li>);
+    return (
+      <React.Fragment>
+      <div className="btn-group btn-group-xs">
+        <button className="btn btn-default dropdown-toggle" type="button" id={this.props.id} data-toggle="dropdown" aria-expanded="true" title={this.props.title}>
+          {this.props.label}&nbsp;
+          <span className="caret"></span>
+        </button>
+        <ul className="dropdown-menu" role="menu" aria-labelledby={this.props.id}>
+          {optionItems}
+        </ul>
+      </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ControlsDropdown
