f903c1bb50a1a86bb31e269b305de95c8334762e
Gittinger <jgittin@s1025794.srn.sandia.gov>
2018-05-23 17:12:39 -0600

    Moving dropdown component to its own file

---
+++ web-server/plugins/slycat-parameter-image/js/Components/controls-dropdown.js
@@ -0,0 +1,13 @@
+Unstaged changes after reset:
+M	web-server/dist/ui_parameter_image.js
+M	web-server/dist/webpack-bundle-analyzer-report.html
+M	web-server/dist/webpack-bundle-analyzer-stats.json
+M	web-server/dist/webpack-visualizer-stats.html
+M	web-server/plugins/slycat-parameter-image/js/Components/controls-dropdown.js
+M	web-server/plugins/slycat-parameter-image/js/Components/controls-group.js
+M	web-server/plugins/slycat-parameter-image/js/Components/controls-playback.js
+M	web-server/plugins/slycat-parameter-image/js/Components/controls-selection.js
+M	web-server/plugins/slycat-parameter-image/js/Components/controls-video.js
+M	web-server/plugins/slycat-parameter-image/js/color-switcher.js
+D	web-server/plugins/slycat-parameter-image/js/controls-dropdown.js
+M	web-server/plugins/slycat-parameter-image/js/parameter-controls.js
