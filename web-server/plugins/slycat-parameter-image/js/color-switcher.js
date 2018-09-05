b4d30cb03b91bd87a5078240e98838bdc5362cb7
Matthew Letter <mletter@s1025079.srn.sandia.gov>
2018-09-05 09:50:12 -0600

    cleaned up color switcher
    removed all the var variable instanciations
    
    cleaned up the length of all the lines
    
    removed unused imports and comments

--- web-server/plugins/slycat-parameter-image/js/color-switcher.js
+++ web-server/plugins/slycat-parameter-image/js/color-switcher.js
@@ -1,17 +1,22 @@
-/* Copyright (c) 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract  DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government  retains certain rights in this software. */
+/*
+Copyright (c) 2013, 2018 National Technology
+and Engineering Solutions of Sandia, LLC . Under
+the terms of Contract  DE-NA0003525 with
+National Technology and Engineering Solutions
+of Sandia, LLC, the U.S. Government  retains
+certain rights in this software.
+*/
 
 import d3 from "js/d3.min";
 import "jquery-ui";
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import ColorBar from './Components/color-bar';
 import ColorButton from './Components/color-bar-redux.js';
 import { createStore } from 'redux';
-import helloWorldReducer from './Reducers/hello-world-reducer';
 import colorSwitcherReducer from './Reducers/color-switcher-reducer';
 import COLOR_MAP from './Components/color-map.js';
 import COLOR_LABELS from './Components/color-labels.js';
-
 
 $.widget("slycat.colorswitcher",
 {
@@ -22,11 +27,8 @@ $.widget("slycat.colorswitcher",
   },
   _create: function()
   {
-    var self = this;
-
+    let self = this;
     this.color_maps = COLOR_MAP;
-
-
     const dropdown = [
                 {
                     id: 'color-switcher',
@@ -39,44 +41,40 @@ $.widget("slycat.colorswitcher",
                     single: true,
                 }];
 
-    const helloWorldStore = createStore(helloWorldReducer);
     const colorSwitcherStore = createStore(colorSwitcherReducer);
-    
-    const color_bar = <ColorBar element={self.element}
+    const color_bar = <ColorBar
+        element={self.element}
         dropdown={dropdown}
         selection={self.options.selection}
         />;
-
-
-    //const redux_test = <Button store={helloWorldStore}/>
-    const color_switcher_redux = <ColorButton store={colorSwitcherStore} element={self.element} dropdown={dropdown} selection={self.options.selection}
-    />;
-
-    //self.redux_test = ReactDOM.render(redux_test, document.getElementById('redux-test'));
+    const color_switcher_redux = <ColorButton
+        store={colorSwitcherStore}
+        element={self.element} dropdown={dropdown}
+        selection={self.options.selection}
+        />;
 
     self.color_bar = ReactDOM.render(color_bar, document.getElementById('color-switcher'));
     self.color_switcher_redux = ReactDOM.render(color_switcher_redux, document.getElementById('redux-test'));
-
-    var all_color_stops = [];
-    var all_background_color = [];
-
+    let all_color_stops = [];
+    let all_background_color = [];
     $.each(this.color_maps, function(key, value) {
-
-        var gradient_data = self.get_gradient_data(key);
-        var color_stops = [];
+        let gradient_data = self.get_gradient_data(key);
+        let color_stops = [];
         for (var i = 0; i < gradient_data.length; i++) {
-            color_stops.push(gradient_data[i].color + " " + gradient_data[i].offset + "%");
+            color_stops.push(gradient_data[i].color
+                + " "
+                + gradient_data[i].offset + "%");
         }
-        var background_color = self.get_background(key);
-
+        let background_color = self.get_background(key);
         all_color_stops.push(color_stops);
         all_background_color.push(background_color);
     });
-
-    for (var i = 0; i < all_color_stops.length; i++) {
+    for (let i = 0; i < all_color_stops.length; i++) {
       $("#color-switcher a").eq(i)
             .css({
-                "background-image": "linear-gradient(to bottom, " + all_color_stops[i].join(", ") + "), linear-gradient(to bottom, " + all_background_color[i] + ", " + all_background_color[i] + ")",
+                "background-image": "linear-gradient(to bottom, "
+                    + all_color_stops[i].join(", ") + "), linear-gradient(to bottom, "
+                    + all_background_color[i] + ", " + all_background_color[i] + ")",
                 "background-size": "5px 75%, 50px 100%",
                 "background-position": "right 10px center, right 5px center",
                 "background-repeat": "no-repeat, no-repeat",
@@ -88,13 +86,16 @@ $.widget("slycat.colorswitcher",
   _setOption: function(key, value)
   {
 
-    if(key == "color")
+    if(key === "color")
     {
-      self.color_bar.setState({colormap: Number(self.options.colormap)});
+      self.color_bar.setState({
+          colormap: Number(self.options.colormap)
+      });
     }
   },
 
-  // Return a d3 rgb object with the suggested background color for the given color map.
+  // Return a d3 rgb object with the suggested
+  // background color for the given color map.
   get_background: function(name)
   {
     if(name === undefined)
@@ -129,11 +130,11 @@ $.widget("slycat.colorswitcher",
       min = 0.0;
     if(max === undefined)
       max = 1.0;
-    var domain = []
-    var domain_scale = d3.scale.linear()
-      .domain([0, this.color_maps[name].colors.length-1])
-      .range([min, max]);
-    for(var i in this.color_maps[name].colors)
+    let domain = [];
+    let domain_scale = d3.scale.linear()
+          .domain([0, this.color_maps[name].colors.length - 1])
+          .range([min, max]);
+    for(let i in this.color_maps[name].colors)
       domain.push(domain_scale(i));
     return d3.scale.linear().domain(domain).range(this.color_maps[name].colors);
   },
@@ -153,10 +154,10 @@ $.widget("slycat.colorswitcher",
     if(values === undefined)
       values = [0, 1];
 
-    var tempOrdinal = d3.scale.ordinal().domain(values).rangePoints([0, 100], 0);
-    var tempColorscale = this.get_color_scale(name, 0, 100);
-    var rgbRange = [];
-    for(var i=0; i<values.length; i++)
+    let tempOrdinal = d3.scale.ordinal().domain(values).rangePoints([0, 100], 0);
+    let tempColorscale = this.get_color_scale(name, 0, 100);
+    let rgbRange = [];
+    for(let i=0; i<values.length; i++)
     {
       rgbRange.push( tempColorscale( tempOrdinal(values[i]) ) );
     }
@@ -171,15 +172,15 @@ $.widget("slycat.colorswitcher",
 
   get_gradient_data: function(name)
   {
-    var self = this;
+    let self = this;
 
     if(name === undefined)
       name = this.options.colormap;
 
-    var colors = self.color_maps[name]["colors"];
-    var length = colors.length;
-    var data = [];
-    for(var i=0; i < length; i++){
+    let colors = self.color_maps[name]["colors"];
+    let length = colors.length;
+    let data = [];
+    for(let i=0; i < length; i++){
       data.push({offset: i*(100/(length-1)), color: colors[length-1-i],});
     }
     return data;
@@ -187,7 +188,7 @@ $.widget("slycat.colorswitcher",
 
   setUpColorMapsForAllColumns: function(name, columns)
   {
-    for(var j = 0; j != columns.length; ++j)
+    for(let j = 0; j !== columns.length; ++j)
     {
       columns[j].colorMap = this.get_color_scale(name, columns[j].columnMin, columns[j].columnMax);
     }
