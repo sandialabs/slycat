bfc1cd3c135391f59684a2ee5bb3bc511120dd2a
Alex M. Sielicki <alex@techmilo.com>
2018-11-02 15:32:15 -0700

    Adding support of Sass SCSS in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -145,6 +145,26 @@ module.exports = {
           'less-loader' // compiles Less to CSS
         ]
       },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
     ],
   },
   optimization: {
