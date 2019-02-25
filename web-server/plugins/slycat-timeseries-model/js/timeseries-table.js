694a42aab6dca3b7cc5b83a24a674acde49dceb2
Alex M. Sielicki <alex@techmilo.com>
2019-02-25 15:34:32 -0800

    Upgrading Timeseries to use new SlickGrid. Addresses #821

--- web-server/plugins/slycat-timeseries-model/js/timeseries-table.js
+++ web-server/plugins/slycat-timeseries-model/js/timeseries-table.js
@@ -5,13 +5,13 @@
 
 import d3 from "d3";
 import "jquery-migrate/dist/jquery-migrate.min";
-import "js/jquery.event.drag-2.3.0";
-import "js/jquery.event.drop-2.3.0";
-import "js/slick.core";
-import "js/slick.grid";
-import "js/slick.rowselectionmodel";
-import "js/slick.headerbuttons";
-import "js/slick.autotooltips";
+import "slickgrid/lib/jquery.event.drag-2.3.0";
+import "slickgrid/lib/jquery.event.drop-2.3.0";
+import "slickgrid/slick.core";
+import "slickgrid/slick.grid";
+import "slickgrid/plugins/slick.rowselectionmodel";
+import "slickgrid/plugins/slick.headerbuttons";
+import "slickgrid/plugins/slick.autotooltips";
 import * as chunker from "./chunker";
 
 $.widget("timeseries.table",
--- web-server/plugins/slycat-timeseries-model/js/ui.js
+++ web-server/plugins/slycat-timeseries-model/js/ui.js
@@ -5,9 +5,9 @@
 
 import jquery_ui_css from "jquery-ui/themes/base/all.css";
 
-import slick_grid_css from "css/slickgrid/slick.grid.css";
-import slick_default_theme_css from "css/slickgrid/slick-default-theme.css";
-import slick_headerbuttons_css from "css/slickgrid/slick.headerbuttons.css";
+import slick_grid_css from "slickgrid/slick.grid.css";
+import slick_default_theme_css from "slickgrid/slick-default-theme.css";
+import slick_headerbuttons_css from "slickgrid/plugins/slick.headerbuttons.css";
 import slick_slycat_theme_css from "css/slick-slycat-theme.css";
 import ui_css from "../css/ui.css";
 
