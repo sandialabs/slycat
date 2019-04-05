85386e03a6ba0b4c9bf300e5e872d15cb4212ef5
Matthew Letter <mletter@s1025079.srn.sandia.gov>
2019-04-05 10:25:06 -0600

    Update slycat-img-to-video.py

--- agent/slycat-img-to-video.py
+++ agent/slycat-img-to-video.py
@@ -8,19 +8,34 @@ import cv2
 import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', default='~/Desktop/movie_images/', help='a parameter.')
-arguments = parser.parse_args()
+parser.add_argument('--input', default='/Desktop/movie_images/', help='a parameter.')
+parser.add_argument('--width', default=500, help='a parameter.')
+parser.add_argument('--height', default=500, help='a parameter.')
+parser.add_argument('--verbose', default=False, help='a parameter.')
 
-img_array = []
-for filename in sorted(glob.glob(arguments.input + '*.jpg')):
-    print(filename)
-    img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width,height)
-    img_array.append(img)
- 
-out = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 1, size)
- 
+arguments = parser.parse_args()
+size = None
+def print_verbose(*args):
+    if arguments.verbose:
+        print(*args)
+
+if os.path.exists(arguments.input):
+  img_array = []
+  for filename in sorted(glob.glob(arguments.input + '*.jpg')):
+      print_verbose(filename)
+      img = cv2.imread(filename)
+      height, width, layers = img.shape
+      print_verbose("Images dimentions {} {}".format(height, width))
+      temp_size = (width,height)
+      if size is None:
+        size = temp_size
+      elif size != temp_size:
+        raise Exception("you have two format sizes for your images, images sizes must be the same")
+      img_array.append(img.astype('uint8'))
+else:
+  raise FileNotFoundError("bad path")
+out = cv2.VideoWriter('out.mp4', cv2.VideoWriter_fourcc(*'H264'), 1, size, True)
+
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
