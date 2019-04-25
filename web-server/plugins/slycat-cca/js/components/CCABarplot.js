d76766c670192533352e8e9dd09bb51b881b337f
Alex M. Sielicki <alex@techmilo.com>
2019-04-25 15:10:48 -0700

    Adding outputs to CCA barplot in React

--- web-server/plugins/slycat-cca/js/components/CCABarplot.js
+++ web-server/plugins/slycat-cca/js/components/CCABarplot.js
@@ -14,7 +14,13 @@ class CCABarplot extends React.Component {
     const button_style = 'btn-outline-dark';
 
     const barplotHeaderColumns = this.props.x_loadings.map((item, index) => (
-      <div className={`barplotHeaderColumn col${index+1} cca${index + 1}`} key={index}>
+      <div 
+        className={`
+          barplotHeaderColumn 
+          col${index+1} 
+          cca${index + 1}`} 
+        key={index}
+      >
         <div className="wrapper">
           <div className="negativeSpacer spacer" />
           <div className="barplotHeaderColumnLabelWrapper">
@@ -67,7 +73,9 @@ class CCABarplot extends React.Component {
       <React.Fragment key={inputs_index}>
         <div className="barplotColumn input">
           <div 
-            className={`barplotCell col0 rowInput inputLabel row${inputs_index} variable${this.props.inputs[inputs_index]}`}
+            className={`barplotCell col0 rowInput inputLabel \
+              row${inputs_index} \
+              variable${this.props.inputs[inputs_index]}`}
             data-loadings_index={inputs_index}
             data-variable={this.props.inputs[inputs_index]}
           >
@@ -78,13 +86,18 @@ class CCABarplot extends React.Component {
         </div>
         <div className="barplotCanvas input">
           <div 
-            className={`barplotRow rowInput row${inputs_index} variable${this.props.inputs[inputs_index]}`}
+            className={`barplotRow rowInput \
+              row${inputs_index} \
+              variable${this.props.inputs[inputs_index]}`}
             data-loadings_index={inputs_index}
             data-variable={this.props.inputs[inputs_index]}
           >
             {this.props.x_loadings.map((item, x_loadings_index) =>
               <div 
-                className={`barplotCell rowInput row${x_loadings_index} col${x_loadings_index+1} variable${this.props.inputs[inputs_index]}`}
+                className={`barplotCell rowInput \
+                  row${inputs_index} \
+                  col${x_loadings_index+1} \
+                  variable${this.props.inputs[inputs_index]}`}
                 key={x_loadings_index}
               >
                 <div className="wrapper">
@@ -99,6 +112,59 @@ class CCABarplot extends React.Component {
                   <div className="positiveSpacer spacer">
                     <div className={`positive cca${x_loadings_index + 1}`} 
                       style={{width: positive_bar_width(this.props.x_loadings[x_loadings_index][inputs_index])}}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    ));
+
+    const outputs = this.props.outputs.map((item, outputs_index) => (
+      <React.Fragment key={outputs_index}>
+        <div className="barplotColumn output">
+          <div 
+            className={`barplotCell col0 rowOutput outputLabel \
+              row${outputs_index + this.props.inputs.length} \
+              variable${this.props.outputs[outputs_index]}`}
+            data-loadings_index={outputs_index}
+            data-variable={this.props.outputs[outputs_index]}
+          >
+            <div className="wrapper">
+              {this.props.metadata["column-names"][this.props.outputs[outputs_index]]}
+            </div>
+          </div>
+        </div>
+        <div className="barplotCanvas output">
+          <div 
+            className={`barplotRow rowOutput \
+              row${outputs_index + this.props.inputs.length} \
+              variable${this.props.outputs[outputs_index]}`}
+            data-loadings_index={outputs_index}
+            data-variable={this.props.outputs[outputs_index]}
+          >
+            {this.props.x_loadings.map((item, x_loadings_index) =>
+              <div 
+                className={`barplotCell rowOutput \
+                  row${outputs_index + this.props.inputs.length} \
+                  col${x_loadings_index+1} \
+                  variable${this.props.outputs[outputs_index]}`}
+                key={x_loadings_index}
+              >
+                <div className="wrapper">
+                  <div className="negativeSpacer spacer">
+                    <div className={`negative cca${x_loadings_index + 1}`} 
+                      style={{width: negative_bar_width(this.props.y_loadings[x_loadings_index][outputs_index])}}
+                    />
+                  </div>
+                  <div className="barplotCellValue">
+                    {Number(this.props.y_loadings[x_loadings_index][outputs_index]).toFixed(3)}
+                  </div>
+                  <div className="positiveSpacer spacer">
+                    <div className={`positive cca${x_loadings_index + 1}`} 
+                      style={{width: positive_bar_width(this.props.y_loadings[x_loadings_index][outputs_index])}}
                     />
                   </div>
                 </div>
@@ -142,6 +208,9 @@ class CCABarplot extends React.Component {
             <div className="barplotViewport">
               <div className="barplotGroup inputs">
                 {inputs}
+              </div>
+              <div className="barplotGroup outputs">
+                {outputs}
               </div>
             </div>
           </div>
