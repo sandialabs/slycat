e51f3bf7893e5879d1345107be9aa9a96d43bbf5
Gittinger <jgittin@s1025794.srn.sandia.gov>
2019-05-24 13:56:35 -0600

    Adding another test for warning

--- web-server/__tests__/warning-test.test.tsx
+++ web-server/__tests__/warning-test.test.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Warning, {WarningProps} from "components/Warning";
 import { mount } from "enzyme";
 
-
 describe('when loading a warning message',() =>{
   test('we have expected props on initial load', async () => {
     const message = ['test1','test2','test3'];
@@ -17,6 +16,19 @@ describe('when loading a warning message',() =>{
         );
     const props = render.props() as WarningProps;
     expect(props).toMatchSnapshot();
+  });
+
+  test('we should have a full initialization', async () => {
+    const message = ['test1','test2','test3'];
+    const properties: WarningProps = {
+      warningMessage:message,
+    };
+    const render = await mount(
+      <Warning
+        {...properties}
+      />
+        );
+     expect(render.instance()).toMatchSnapshot();
   });
 });
 
