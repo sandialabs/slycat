a8e6b3bc314b264964b7c93aad494f379183c046
Alex M. Sielicki <alex@techmilo.com>
2019-05-27 10:50:05 +0100

    Formatting

--- web-server/components/ControlsDropdownColor.js
+++ web-server/components/ControlsDropdownColor.js
@@ -2,66 +2,65 @@ import React from "react";
 import ControlsDropdown from 'components/ControlsDropdown';
 
 class ControlsDropdownColor extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+      super(props);
 
-        this.state = {
-        selection: this.props.dropdown[0].selected,
-      };
+      this.state = {
+      selection: this.props.dropdown[0].selected,
+    };
 
-        this.state[this.props.dropdown.state_label] = this.props.dropdown.selected;
+    this.state[this.props.dropdown.state_label] = this.props.dropdown.selected;
 
-        this.set_selected = this.set_selected.bind(this);
+    this.set_selected = this.set_selected.bind(this);
+  }
+
+  set_selected(state_label, key, trigger, e) {
+    // Do nothing if the state hasn't changed (e.g., user clicked on currently selected variable)
+    if(key == this.state[state_label])
+    {
+      return;
     }
+    // That function will receive the previous state as the first argument, and the props at the time the update is applied as the
+    // second argument. This format is favored because this.props and this.state may be updated asynchronously,
+    // you should not rely on their values for calculating the next state.
+    const obj = {};
+    obj[state_label] = key;
+    this.setState((prevState, props) => (obj));
+    this.state.selection = key;
 
-    set_selected(state_label, key, trigger, e) {
-      // Do nothing if the state hasn't changed (e.g., user clicked on currently selected variable)
-      if(key == this.state[state_label])
-      {
-        return;
-      }
-      // That function will receive the previous state as the first argument, and the props at the time the update is applied as the
-      // second argument. This format is favored because this.props and this.state may be updated asynchronously,
-      // you should not rely on their values for calculating the next state.
-      const obj = {};
-      obj[state_label] = key;
-      this.setState((prevState, props) => (obj));
-      this.state.selection = key;
-
-      // This is the legacy way of letting the rest of non-React components that the state changed. Remove once we are converted to React.
-      this.props.element.trigger(trigger, key);
+    // This is the legacy way of letting the rest of non-React components that the state changed. Remove once we are converted to React.
+    this.props.element.trigger(trigger, key);
   }
 
   get_selected_colormap() {
-      if(this.state.selection == null) {
-          return "night";
-      }
-      else {
-        return this.state.selection;
-      }
+    if(this.state.selection == null) {
+      return "night";
+    }
+    else {
+      return this.state.selection;
+    }
   }
 
-    render() {
+  render() {
+    // Define default button style
+    const button_style = 'btn-outline-dark';
 
-      // Define default button style
-      const button_style = 'btn-outline-dark';
-
-      return (
-        <ControlsDropdown 
-          key={this.props.dropdown[0].id} 
-          id={this.props.dropdown[0].id} 
-          label={this.props.dropdown[0].label} 
-          title={this.props.dropdown[0].title}
-          state_label={this.props.dropdown[0].state_label} 
-          trigger={this.props.dropdown[0].trigger}
-          items={this.props.dropdown[0].items}
-          selected={this.state.selection} 
-          single={this.props.dropdown[0].single} 
-          set_selected={this.set_selected}
-          button_style={button_style}
-        />
-      );
-    }
+    return (
+      <ControlsDropdown 
+        key={this.props.dropdown[0].id} 
+        id={this.props.dropdown[0].id} 
+        label={this.props.dropdown[0].label} 
+        title={this.props.dropdown[0].title}
+        state_label={this.props.dropdown[0].state_label} 
+        trigger={this.props.dropdown[0].trigger}
+        items={this.props.dropdown[0].items}
+        selected={this.state.selection} 
+        single={this.props.dropdown[0].single} 
+        set_selected={this.set_selected}
+        button_style={button_style}
+      />
+    );
+  }
 }
 
 export default ControlsDropdownColor
