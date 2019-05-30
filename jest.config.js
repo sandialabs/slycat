18a9992d6adb479acab71e49dbcaa1ee881092b8
Matthew Letter <mletter@sandia.gov>
2019-05-30 12:18:49 -0600

    added serializer to jest config

--- jest.config.js
+++ jest.config.js
@@ -135,7 +135,7 @@ module.exports = {
   // setupFilesAfterEnv: [],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  // snapshotSerializers: [],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
 
   // The test environment that will be used for testing
   testEnvironment: "jsdom",
