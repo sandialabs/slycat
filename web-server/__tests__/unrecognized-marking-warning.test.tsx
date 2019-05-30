121f65af9c0b9edde59ce8c0b212f4f69147156a
Matthew Letter <mletter@sandia.gov>
2019-05-30 14:34:24 -0600

    cleanup

--- web-server/__tests__/unrecognized-marking-warning.test.tsx
+++ web-server/__tests__/unrecognized-marking-warning.test.tsx
@@ -7,11 +7,6 @@ import { mount } from "enzyme";
 
 
 describe('when loading a bad marking the UnrecognizedMarkingWarning',() =>{ 
-  // const properties: UnrecognizedMarkingWarningProps = {
-  //   marking: {not:"empty"},
-  //   warning_element: {setAttribute:jest.fn()},
-  //   project_id:"1234"
-  // };
   const marking="empty";
   const warning_element= {setAttribute:jest.fn()};
   const render = mount(
