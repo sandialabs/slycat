3eaea9e42f2fc7c6bb46c3106ae84fee799eec24
Gittinger <jgittin@s1025794.srn.sandia.gov>
2019-06-05 13:30:53 -0600

    Creating Tabs component

---
+++ web-server/components/Tabs.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+/**
+ * not used
+ */
+export interface TabsProps {
+}
+
+/**
+ * not used
+ */
+export interface TabsState {
+}
+
+/**
+ * takes a list of messages to be displayed as a warning
+ */
+export default class Tabs  extends React.Component<TabsProps,TabsState> {
+  public constructor(props:TabsProps) {
+    super(props);
+  }
+  render () {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
