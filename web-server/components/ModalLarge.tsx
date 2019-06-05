376a56fe5ac21c74b541f254964d4e711a59f4fa
Matthew Letter <mletter@sandia.gov>
2019-06-05 12:54:26 -0600

    Create ModalLarge.tsx

---
+++ web-server/components/ModalLarge.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+/**
+ * not used
+ */
+export interface ModalLargeProps {
+}
+
+/**
+ * not used
+ */
+export interface ModalLargeState {
+}
+
+/**
+ * takes a list of messages to be displayed as a warning
+ */
+export default class ModalLarge  extends React.Component<ModalLargeProps,ModalLargeState> {
+  public constructor(props:ModalLargeProps) {
+    super(props);
+  }
+  render () {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
