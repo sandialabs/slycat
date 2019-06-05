5b4800fdbdea1610eece996d65e041446bb2db54
Matthew Letter <mletter@sandia.gov>
2019-06-05 13:19:57 -0600

    Update ModalLarge.tsx

--- web-server/components/ModalLarge.tsx
+++ web-server/components/ModalLarge.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 
 /**
- * not used
+ * @member modalId string dom id for the modal
+ * @member closingCallBack callback function for cleanup when closing the modal
+ * @member title test for the top of the modal
  */
 export interface ModalLargeProps {
+  modalId: string
+  closingCallBack:Function
+  title: string
 }
 
 /**
@@ -18,10 +23,40 @@ export interface ModalLargeState {
 export default class ModalLarge  extends React.Component<ModalLargeProps,ModalLargeState> {
   public constructor(props:ModalLargeProps) {
     super(props);
+    this.state = {
+    }
   }
+  
+  /**
+   *close the modal and call the cleanup function
+   *
+   * @memberof ModalLarge
+   */
+  closeModal = (e:React.MouseEvent) =>
+  {
+    this.props.closingCallBack();
+    ($('#' + this.props.modalId) as any).modal('hide');
+  };
+
   render () {
     return(
-      <div>
+      <div className='modal fade' data-backdrop='false' id={this.props.modalId}>
+        <div className='modal-dialog modal-lg'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h3 className='modal-title'>{this.props.title}</h3>
+              <button type='button' className='close' onClick={this.closeModal} aria-label='Close'>
+                X
+              </button>
+            </div>
+            <div className='modal-body' id="slycat-wizard">
+
+              <div className='modal-footer'>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
