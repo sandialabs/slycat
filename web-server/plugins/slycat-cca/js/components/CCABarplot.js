a08c6ec566cd4bd1782c09df2b4e689bc742ef8a
Alex M. Sielicki <alex@techmilo.com>
2019-06-27 16:41:08 -0700

    Adding auto scroll functionality to CCA barplot to make selected variable and CCA component visible. Making CCA barplot sorting work through Redux state.

--- web-server/plugins/slycat-cca/js/components/CCABarplot.js
+++ web-server/plugins/slycat-cca/js/components/CCABarplot.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { connect } from 'react-redux';
-import { setVariableSelected, setCCAComponentSelected } from '../actions';
+import { setVariableSelected, setCCAComponentSelected, setCCAComponentSorted } from '../actions';
 
 import _ from "lodash";
+// Using scrollintoview jQuery plugin instead of browser built-in functionality (DOM's scrollIntoView() function).
+// DOM's scrollIntoView() works but has problems: 
+//   1. scrolls each time it's called, even when element is already visible, to bring it to top or bottom of scroll area.
+//   2. always scrolls in both directions, displayin left edge of target element even if user 
+//      has already scrolled horizontally to display other CCA components.
+//   3. lacks animation when scrolling
+//   4. always scrolls to top or bottom of scroll area instead of the least amount needed to make it visible
+// The jQuery plugin solves all these issues.
+import "js/jquery.scrollintoview.min";
 
 class CCABarplot extends React.Component {
   constructor(props) {
@@ -36,17 +45,6 @@ class CCABarplot extends React.Component {
       });
       this.outputs_rows.push(row);
     });
-
-    // Sort rows
-    if(this.props.sort.component != undefined)
-    {
-      // Default sort direction to descending
-      this.sort_direction = this.props.sort.direction != undefined ? this.props.sort.direction : 'descending';
-      // Translate sort direction to lodash terms
-      let lodashDirection = this.sort_direction == 'ascending' ? 'asc' : 'desc';
-      this.inputs_rows = _.orderBy(this.inputs_rows, ['x_loading_' + this.props.sort.component + '_abs'], [lodashDirection]);
-      this.outputs_rows = _.orderBy(this.outputs_rows, ['y_loading_' + this.props.sort.component + '_abs'], [lodashDirection]);
-    }
   }
 
   /* Sizing table */
@@ -64,7 +62,7 @@ class CCABarplot extends React.Component {
     this.inputsHeight = $('.barplotGroup.inputs').height();
     this.outputsHeight = $('.barplotGroup.outputs').height();
 
-    this.handle_component_change_transition();
+    this.scroll_selected_component_into_view();
     this.resize_canvas();
 
     $(".barplotCanvas.input").bind("scroll", function(){
@@ -139,13 +137,42 @@ class CCABarplot extends React.Component {
 
     // Can't figure out how to do this in CSS so setting explicit height here :(
     $(".barplotHeaderColumn.mask.col0").height( $(".barplotHeader .barplotRow:first-child").height() );
+
+    // When the component mounts, we scroll the selected variable and selected CCA component into view
+    this.scroll_selected_variable_into_view();
+    this.scroll_selected_component_into_view();
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
+    // When the selected component changes, we need to scroll the first output cell of the selected component into view
+    // and resize the table
     if (this.props.cca_component_selected !== prevProps.cca_component_selected)
     {
-      this.handle_component_change_transition();
+      this.scroll_selected_component_into_view();
       this.resize_canvas();
+    }
+    // When the sorted CCA component or its direction changes
+    // or when the selected variable changes,
+    // scroll the selected variable into view.
+    if ( this.props.cca_component_sort_direction !== prevProps.cca_component_sort_direction ||
+         this.props.cca_component_sorted !== prevProps.cca_component_sorted ||
+         this.props.variable_selected !== prevProps.variable_selected
+       )
+    {
+      this.scroll_selected_variable_into_view();
+    }
+  }
+
+  sort_rows = () =>
+  {
+    if(this.props.cca_component_sorted != undefined)
+    {
+      // Default sort direction to descending
+      this.sort_direction = this.props.cca_component_sort_direction != undefined ? this.props.cca_component_sort_direction : 'descending';
+      // Translate sort direction to lodash terms
+      let lodashDirection = this.sort_direction == 'ascending' ? 'asc' : 'desc';
+      this.inputs_rows = _.orderBy(this.inputs_rows, ['x_loading_' + this.props.cca_component_sorted + '_abs'], [lodashDirection]);
+      this.outputs_rows = _.orderBy(this.outputs_rows, ['y_loading_' + this.props.cca_component_sorted + '_abs'], [lodashDirection]);
     }
   }
 
@@ -242,21 +269,29 @@ class CCABarplot extends React.Component {
     $(".barplotGroup.inputs .ui-resizable-s").css("left", "-" + verticalScrollbarWidth + "px");
   }
 
-  handle_component_change_transition = () =>
+  scroll_selected_component_into_view = () =>
   {
     // Scroll the first output cell of the selected component into view
     $(".rowOutput.selected-component", this.cca_barplot_table.current).first().scrollintoview({direction: "horizontal",});
   }
 
+  scroll_selected_variable_into_view = () =>
+  {
+    // Scroll the selected variable row into view
+    $(".barplotRow.selected-variable", this.cca_barplot_table.current).scrollintoview({direction: "vertical",});
+  }
+
   render() {
+    this.sort_rows();
+
     const barplotHeaderColumns = this.props.x_loadings.map((item, index) => (
       <div 
         className={`
-          barplotHeaderColumn 
-          col${index+1} 
-          cca${index + 1}
+          barplotHeaderColumn \
+          col${index+1} \
+          cca${index + 1} \
           ${this.props.cca_component_selected == index ? 'selected-component' : ''}
-          `}
+        `}
         key={index}
       >
         <div className='wrapper'>
@@ -266,10 +301,15 @@ class CCABarplot extends React.Component {
               onClick={() => this.props.setCCAComponentSelected(index)}>
               CCA{index + 1}
             </span>
-            <span className={`sortCCAComponent \
-              ${this.props.sort.component == index ? 
-                (this.props.sort.direction == 'ascending' ? 'icon-sort-ascending' : 'icon-sort-descending') : 
-                'icon-sort-off'}`} />
+            <span 
+              className={`
+                sortCCAComponent \
+                ${this.props.cca_component_sorted == index ? 
+                  (this.props.cca_component_sort_direction == 'ascending' ? 'icon-sort-ascending' : 'icon-sort-descending') : 
+                  'icon-sort-off'}
+              `}
+              onClick={() => this.props.setCCAComponentSorted(index)}
+            />
           </div>
           <div className='positiveSpacer spacer' />
         </div>
@@ -508,10 +548,15 @@ const mapStateToProps = state => {
   return {
     variable_selected: state.variable_selected,
     cca_component_selected: state.cca_component_selected,
+    cca_component_sorted: state.cca_component_sorted,
+    cca_component_sort_direction: state.cca_component_sort_direction,
   }
 };
 
 export default connect(
   mapStateToProps,
-  { setVariableSelected, setCCAComponentSelected, }
+  { setVariableSelected, 
+    setCCAComponentSelected,
+    setCCAComponentSorted,
+  }
 )(CCABarplot)
