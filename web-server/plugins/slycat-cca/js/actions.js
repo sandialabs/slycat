03ecef35a68ffa748d8018115fbd76a346825389
Alex M. Sielicki <alex@techmilo.com>
2019-07-10 11:59:15 -0700

    Adding thunk action creators to request, fetch, receive selected variable values.

--- web-server/plugins/slycat-cca/js/actions.js
+++ web-server/plugins/slycat-cca/js/actions.js
@@ -1,3 +1,6 @@
+import * as chunker from "js/chunker";
+import api_root from "js/slycat-api-root";
+
 /*
  * action types
  */
@@ -11,13 +14,123 @@ export const SET_SIMULATIONS_SELECTED = 'SET_SIMULATIONS_SELECTED';
 export const ADD_SIMULATIONS_SELECTED = 'ADD_SIMULATIONS_SELECTED';
 export const REMOVE_SIMULATIONS_SELECTED = 'REMOVE_SIMULATIONS_SELECTED';
 export const TOGGLE_SIMULATIONS_SELECTED = 'TOGGLE_SIMULATIONS_SELECTED';
+export const REQUEST_VARIABLE = 'REQUEST_VARIABLE';
+export const RECEIVE_VARIABLE = 'RECEIVE_VARIABLE';
 
 export function setVariableSelected(variable, state_label, trigger, e) {
-  return { 
-  	type: SET_VARIABLE_SELECTED, 
-  	id: variable,
+  return function(dispatch, getState) {
+    dispatch(selectVariable(variable));
+    dispatch(fetchVariableValuesIfNeeded(variable));
   }
 }
+
+function selectVariable(variable) {
+  return { 
+    type: SET_VARIABLE_SELECTED, 
+    id: variable,
+  }
+}
+
+function requestVariable(variable) {
+  return {
+    type: REQUEST_VARIABLE,
+    variable
+  }
+}
+
+function receiveVariable(variable, result) {
+  return {
+    type: RECEIVE_VARIABLE,
+    variable,
+    values: result,
+  }
+}
+
+// Thunk action creator. Use like so:
+// store.dispatch(fetchVariableValues(0))
+export function fetchVariableValues(variable) {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return function(dispatch, getState) {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(requestVariable(variable))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    // return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+    //   .then(
+    //     response => response.json(),
+    //     // Do not use catch, because that will also catch
+    //     // any errors in the dispatch and resulting render,
+    //     // causing a loop of 'Unexpected batch number' errors.
+    //     // https://github.com/facebook/react/issues/6895
+    //     error => console.log('An error occurred.', error)
+    //   )
+    //   .then(json =>
+    //     // We can dispatch many times!
+    //     // Here, we update the app state with the results of the API call.
+
+    //     dispatch(receivePosts(subreddit, json))
+    //   )
+    
+    chunker.get_model_array_attribute({
+      api_root : api_root,
+      mid : getState().derived.model_id,
+      aid : "data-table",
+      array : 0,
+      attribute : variable,
+      success : function(result)
+      {
+        dispatch(receiveVariable(variable, result))
+      },
+      // error : artifact_missing
+    });
+      
+  }
+}
+
+function shouldFetchVariableValues(state, variable) {
+  const column_data = state.derived.column_data[variable];
+  // No entry for that column means go
+  if (!column_data) {
+    return true
+  } 
+  // If we are fetching that column, no go
+  else if (column_data.isFetching) {
+    return false
+  }
+  // Have an entry for column but not fetching it, so go
+  else {
+    return true;
+  }
+}
+
+export function fetchVariableValuesIfNeeded(variable) {
+  // Note that the function also receives getState()
+  // which lets you choose what to dispatch next.
+
+  // This is useful for avoiding a network request if
+  // a cached value is already available.
+
+  return (dispatch, getState) => {
+    if (shouldFetchVariableValues(getState(), variable)) {
+      // Dispatch a thunk from thunk!
+      return dispatch(fetchVariableValues(variable))
+    } else {
+      // Let the calling code know there's nothing to wait for.
+      return Promise.resolve()
+    }
+  }
+}
+
 export function setVariableSorted(variable) {
   return { 
   	type: SET_VARIABLE_SORTED, 
