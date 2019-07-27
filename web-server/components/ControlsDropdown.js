769b2c20b185dd5f5948c6c37c86d598ef5f555e
Alex M. Sielicki <alex@techmilo.com>
2019-07-27 09:35:41 -0700

    Setting max height on dropdowns so they don't go beyond page height when there are many items. Closes #905

--- web-server/components/ControlsDropdown.js
+++ web-server/components/ControlsDropdown.js
@@ -3,6 +3,31 @@ import React from "react";
 class ControlsDropdown extends React.Component {
   constructor(props) {
     super(props);
+
+    // Create a ref to the button
+    this.dropdown_toggle = React.createRef();
+    this.dropdown_menu = React.createRef();
+  }
+
+  componentDidMount() {
+    // Need to add the event listener to the dropdown menu's parent element because:
+    // "All dropdown events are fired at the .dropdown-menu’s parent element"
+    // https://getbootstrap.com/docs/4.3/components/dropdowns/#events
+    $(this.dropdown_menu.current.parentNode).on('shown.bs.dropdown', this._handleShowDropdown);
+    // addEventListener does not work because Boostrap uses jQuery events, 
+    // which are "namespaced" in a way that doesn't work with addEventListener.
+    // this.dropdown_menu.current.parentNode.addEventListener('show.bs.dropdown', this._handleShowDropdown);
+  }
+
+  _handleShowDropdown = (e) => {
+    // Get the dropdown menu inside the dropdown element container
+    let menus = $('.dropdown-menu', e.currentTarget);
+    // Get the container that holds the model's panes
+    let container = $('.ui-layout-container').first();
+    // Set the max height of each menu to 70px less than the container.
+    // This prevents the menus from sticking out beyond the page and allows
+    // them to be scrollable when they are too long.
+    menus.css('max-height', (container.height() - 70) + 'px');
   }
 
   render() {
@@ -25,10 +50,14 @@ class ControlsDropdown extends React.Component {
         <button type="button" id={this.props.id} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" 
           className={`btn dropdown-toggle btn-sm ${this.props.button_style}`}
           title={this.props.title}
+          ref={this.dropdown_toggle}
         >
           {this.props.label}&nbsp;
         </button>
-        <div className="dropdown-menu" aria-labelledby={this.props.id}>
+        <div className="dropdown-menu" 
+          aria-labelledby={this.props.id}
+          ref={this.dropdown_menu}
+        >
           {optionItems}
         </div>
       </React.Fragment>);
