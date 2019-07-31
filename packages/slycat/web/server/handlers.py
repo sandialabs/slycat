169ea184662062112a0450d6b679fabc6449cf37
Gittinger <jgittin@s1025794.srn.sandia.gov>
2019-07-31 13:39:29 -0600

    Fixing new function

--- packages/slycat/web/server/handlers.py
+++ packages/slycat/web/server/handlers.py
@@ -433,33 +433,30 @@ def post_project_models(pid):
     cherrypy.response.status = "201 Model created."
     return {"id": mid}
 
-def create_project_data_from_pid(pid, aid, file):
+@cherrypy.tools.json_in(on=True)
+# @cherrypy.tools.json_out(on=True)
+def create_project_data_from_pid(pid, file=None, file_name=None):
     """
     creates a project level data object from a project id 
     that can be used to create new
     models in the current project
-    :param mid: model ID
-    :param aid: artifact ID
+    :param file_name: artifact ID
     :param file: file attachment
     :return: not used
     """
     content_type = "text/csv"
+    if file is not None and file_name is None:
+        data = file.file.read()
     database = slycat.web.server.database.couchdb.connect()
     timestamp = time.time()
     formatted_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
     did = uuid.uuid4().hex
-    if isinstance(file, list):
-        file = file[0]
-    if len(aid) > 1:
-        if isinstance(aid[1], list):
-            aid[1] = aid[1][0]
-    else:
-        aid.append("unnamed_file")
+
     data = {
         "_id": did,
         "type": "project_data",
-        "file_name": formatted_timestamp + "_" + aid[1],
-        "data_table": aid[0],
+        "file_name": formatted_timestamp + "_" + file_name,
+        "data_table": "data-table",
         "project": pid,
         "mid": [""],
         "created": datetime.datetime.utcnow().isoformat(),
