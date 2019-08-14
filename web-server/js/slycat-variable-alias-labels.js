753e0ea1249c0ddd8368c18ad91a992419b918e1
Alex M. Sielicki <alex@techmilo.com>
2019-08-13 17:01:34 -0700

    Creating scafolding for variable alias labels component. Addresses #907

---
+++ web-server/js/slycat-variable-alias-labels.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function SlycatVariableAliasLabels(props) {
+
+  return (
+    <div className={`slycat-variable-alias-labels ${props.uniqueID}`}>
+      Placeholder for variable alias ui
+    </div>
+  );
+}
