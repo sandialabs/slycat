6a0d0b3b8a464ab55411344f84e12de5d18a5ff0
Alex M. Sielicki <alex@techmilo.com>
2019-08-28 15:12:22 -0700

    Adding label length validation.

--- web-server/js/slycat-variable-alias-labels.js
+++ web-server/js/slycat-variable-alias-labels.js
@@ -4,6 +4,9 @@ import css from "css/slycat-variable-alias-labels.scss";
 export default function SlycatVariableAliasLabels(props) {
 
   let variables = props.metadata['column-names'];
+  let minLength = 1;
+  let maxLength = 256;
+  let alias, aliasLength;
 
   function getVariableAlias(index) {
     let alias = variables[index];
@@ -26,14 +29,22 @@ export default function SlycatVariableAliasLabels(props) {
         <tbody>
         {
           variables.map((variable, index) => {
+            alias = getVariableAlias(index);
+            aliasLength = alias.length;
             return (
               <tr key={index}>
                 <th scope='row' className='col-form-label-sm align-middle variable-name'>{variable}</th>
-                <td>
+                <td className={`${minLength <= aliasLength && aliasLength <= maxLength  ? '' : 'was-validated'}`}>
                     <input type='text' className='form-control form-control-sm variable-alias'
+                      minLength={minLength} 
+                      maxLength={maxLength}
+                      required
                       name={index}
-                      defaultValue={getVariableAlias(index)} 
+                      defaultValue={alias} 
                       onChange={props.onChange} />
+                    <div className='invalid-feedback'>
+                      Please enter a label betweeen {minLength} and {maxLength} characters long.
+                    </div>
                 </td>
               </tr>
             )
