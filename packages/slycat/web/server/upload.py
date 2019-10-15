df80fa6936bd4172ebe9dba937cb35f5c5101375
Martin <smartin@s1001888.srn.sandia.gov>
2019-10-15 15:38:34 -0600

    Cleaned up bug fix.

--- packages/slycat/web/server/upload.py
+++ packages/slycat/web/server/upload.py
@@ -181,15 +181,11 @@ class Session(object):
       # TODO: need to decide if we should remove this or refactor for multi files again
       files = []
       storage = path(self._uid)
-      import zipfile
       for file_dir in sorted(glob.glob(os.path.join(storage, "file-*")), key=numeric_order):
         # cherrypy.log.error("Assembling %s" % file_dir)
 
         is_bin_file = False
         file_parts = []
-
-        #is_zip_file = False
-        #file = None
 
         for file_part in sorted(glob.glob(os.path.join(file_dir, "part-*")), key=numeric_order):
           # cherrypy.log.error(" Loading %s" % file_part)
@@ -210,17 +206,6 @@ class Session(object):
           if is_bin_file == True:
             with open(file_part, "rb") as f:
               file_parts.append(f.read())
-
-          #if not is_zip_file:
-          #  is_zip_file = zipfile.is_zipfile(file_part)
-          #if is_zip_file:
-          #  with open(file_part, "rb") as f:
-          #    file = f.read()
-          #else:
-          #  if file is None:
-          #    file=""
-          #  with open(file_part, "r") as f:
-          #    file += f.read()
 
         # join file parts to make full file
         if is_bin_file == False:
