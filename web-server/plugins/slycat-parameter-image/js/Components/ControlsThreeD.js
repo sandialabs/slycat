2e3dc47d27200c9ab232425f327e8b8ef73e6cda
Alex M. Sielicki <alex@techmilo.com>
2019-11-11 13:38:06 -0800

    Adding 3D Color control and populating it with VTK color maps

--- web-server/plugins/slycat-parameter-image/js/Components/ControlsThreeD.js
+++ web-server/plugins/slycat-parameter-image/js/Components/ControlsThreeD.js
@@ -1,5 +1,8 @@
 import React from "react";
+import ControlsGroup from 'components/ControlsGroup';
 import ControlsButtonToggle from "./controls-button-toggle";
+import ControlsDropdown from './ControlsDropdown';
+import vtkColorMaps from 'vtk.js/Sources/Rendering/Core/ColorTransferFunction/ColorMaps';
 
 class ControlsThreeD extends React.Component {
   constructor(props) {
@@ -7,14 +10,28 @@ class ControlsThreeD extends React.Component {
   }
 
   render() {
+    let color_map_items = vtkColorMaps.rgbPresetNames.map((name) => { 
+      return {key: name, name: name,} 
+    });
+
     return !this.props.any_threeD_open ? null : (
       <React.Fragment>
-        {/* div.input-group-prepend should be enabled only when there is more than a single control present here */}
-        {/* <div className='input-group-prepend'> */}
-          <ControlsButtonToggle title={this.props.threeD_sync ? 'Unsync 3D Viewers' : 'Sync 3D Viewers'} icon='fa-rotate-right' 
-            active={this.props.threeD_sync} set_active_state={this.props.set_threeD_sync} button_style={this.props.button_style}
-          />
-        {/* </div> */}
+        <ControlsButtonToggle title={this.props.threeD_sync ? 'Unsync 3D Viewers' : 'Sync 3D Viewers'} icon='fa-rotate-right' 
+          active={this.props.threeD_sync} set_active_state={this.props.set_threeD_sync} button_style={this.props.button_style}
+        />
+        <ControlsDropdown 
+          key="threeD-color-dropdown"
+          id="threeD-color-dropdown" 
+          label="3D Color"
+          title="Change 3D color"
+          state_label="threeD_color"
+          trigger="threeD-color-selection-changed"
+          items={color_map_items}
+          // selected={this.state.selection} 
+          // single={this.props.dropdown[0].single} 
+          // set_selected={this.set_selected}
+          button_style={this.props.button_style}
+        />
       </React.Fragment>
     );
   }
