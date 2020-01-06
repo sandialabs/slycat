18fa6db21afbe8eea4361eeb64fd63c682d6c562
Alex Milosz Sielicki <alex@techmilo.com>
2020-01-06 15:30:09 -0700

    Moving session check to code that applies to selecting files in addition to navigating directories.

--- web-server/js/slycat-remote-browser.js
+++ web-server/js/slycat-remote-browser.js
@@ -114,38 +114,31 @@ ko.components.register("slycat-remote-browser",
 
     component.open = function(file)
     {
-      // If the file is our parent directory, move up the hierarchy.
-      if(file.name() == "..")
-      {
-        component.browse(path_dirname(component.path()));
-      }
-      // If the file is a directory, move down the hierarchy.
-      else if(file.type() == "d")
-      {
-        component.browse(path_join(component.path(), file.name()));
-      }
-      // If it's a file, signal observers.
-      else if(file.type() == "f")
-      {
-        if(component.open_file_callback)
-          component.open_file_callback();
-      }
-    }
-
-    component.up = function() {
-      component.open({name: function(){return '..';}});
-    }
-
-    component.browse = function(path)
-    {
+      // First check if we have a remote connection...
       client.get_remotes_fetch(component.hostname())
         .then((json) => {
+          // If we have a session, go on.
           if(json.status) {
-            component.browse_to(path);
+            // If the file is our parent directory, move up the hierarchy.
+            if(file.name() == "..")
+            {
+              component.browse(path_dirname(component.path()));
+            }
+            // If the file is a directory, move down the hierarchy.
+            else if(file.type() == "d")
+            {
+              component.browse(path_join(component.path(), file.name()));
+            }
+            // If it's a file, signal observers.
+            else if(file.type() == "f")
+            {
+              if(component.open_file_callback)
+                component.open_file_callback();
+            }
           }
+          // Otherwise...we don't have a session anymore, so set the session_exists
+          // var accordingly and run the reauth method if one was passed.
           else {
-            // We don't have a session anymore, so set the session_exists
-            // var accordingly and run the reauth method if one was passed.
             component.session_exists(false);
             if(params.reauth) {
               params.reauth();
@@ -154,7 +147,11 @@ ko.components.register("slycat-remote-browser",
       });
     }
 
-    component.browse_to = function(path)
+    component.up = function() {
+      component.open({name: function(){return '..';}});
+    }
+
+    component.browse = function(path)
     {
       client.post_remote_browse(
       {
