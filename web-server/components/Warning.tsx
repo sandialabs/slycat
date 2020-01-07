0cad1782edd6443ce6d50851912ae3ef0d0c3427
Alex Milosz Sielicki <alex@techmilo.com>
2020-01-07 14:14:20 -0700

    Changing Warning component to use Bootstrap warning alert instead of home grown one.

--- web-server/components/Warning.tsx
+++ web-server/components/Warning.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 
 /**
  * @member warningMessage array of message to be displayed
- * @member backgroundColor css value of background of the message
  */
 export interface WarningProps {
   warningMessage:Array<string>,
-  backgroundColor?:string
 }
 
 /**
@@ -22,33 +20,11 @@ export default class Warning  extends React.Component<WarningProps,WarningState>
   public constructor(props:WarningProps) {
     super(props);
   }
-  
-  /**
-   * build a list of label for display based of the props warning messages
-   *
-   * @memberof Warning
-   */
-  getLabels = () => {
-    const labels = this.props.warningMessage.map((message, i) => {
-      return (
-          <label key={i} style={{
-            color: 'red', 
-            fontSize:'15.5px', 
-            textAlign:'center', 
-            backgroundColor:this.props.backgroundColor?this.props.backgroundColor:'', 
-            fontWeight:'bold'
-            }}>
-            {message}
-          </label>
-      );
-    });
-    return labels
-  }
 
   render () {
     return(
-      <div>
-        {this.getLabels()}
+      <div className="alert alert-warning" role="alert">
+        {this.props.warningMessage}
       </div>
     );
   }
