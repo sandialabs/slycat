4154bf29e4d0c467f8a2bdd26b78d1e506321730
Alex Milosz Sielicki <alex@techmilo.com>
2020-02-07 17:39:23 -0600

    Making sure we have a successful call to the api before doing anything else. Addresses #936

--- web-server/js/slycat-projects-main.js
+++ web-server/js/slycat-projects-main.js
@@ -14,26 +14,46 @@ import client from 'js/slycat-web-client';
 
 // These next 2 lines are required render the navbar using knockout. Remove them once we convert it to react.
 import ko from 'knockout';
-import { renderNavBar } from "js/slycat-navbar";
 
 // Wait for document ready
 $(document).ready(function() {
-  renderNavBar();
-  client.get_projects({
-    success: function(result) {
-      const projects_list = <SearchWrapper items={result.projects} type="projects" />
-      // console.log('got projects');
-      ReactDOM.render(
-        projects_list,
-        document.getElementById('slycat-projects')
-      );
+
+  function loadNavBar() {
+    return import(/* webpackChunkName: "slycat-navbar" */ 'js/slycat-navbar').then(navbar => {
+      navbar.renderNavBar();
+      client.get_projects({
+        success: function(result) {
+          const projects_list = <SearchWrapper items={result.projects} type="projects" />
+          // console.log('got projects');
+          ReactDOM.render(
+            projects_list,
+            document.getElementById('slycat-projects')
+          );
+        },
+        error: function(request, status, reason_phrase) {
+          console.log("Unable to retrieve project.");
+        }
+      });
+    
+      // These next 2 lines render the navbar using knockout. Remove them once we convert it to react.
+      var page = {}
+      ko.applyBindings(page, document.querySelector("html"));
+    });
+  }
+
+  // First we do a simple api call for the user to make sure we are authenticated
+  // before going ahead with loading the navbar, which makes a bunch more
+  // api calls.
+  client.get_user(
+  {
+    success: function(user)
+    {
+      loadNavBar();  
     },
-    error: function(request, status, reason_phrase) {
-      console.log("Unable to retrieve project.");
+    error: function()
+    {
+      console.log("Can't retrieve current user before loading the navbar.");
     }
   });
-
-  // These next 2 lines render the navbar using knockout. Remove them once we convert it to react.
-  var page = {}
-  ko.applyBindings(page, document.querySelector("html"));
+  
 });
