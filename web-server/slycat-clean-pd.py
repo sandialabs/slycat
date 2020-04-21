86051a479e247881386531b69a52fbcb6388b673
Warren Hunt <wlhunt@sandia.gov>
2020-04-21 09:34:55 -0600

    mod logging to run under linux

--- web-server/slycat-clean-pd.py
+++ web-server/slycat-clean-pd.py
@@ -17,8 +17,11 @@ parser.add_argument("--password", default="", help="CouchDB admin password.  Def
 arguments = parser.parse_args()
 
 
-class log(object):
-  error = logging.getLogger("error").info
+logFile = 'project-data_CleanupLog.txt'
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(logging.FileHandler(logFile))
+logging.getLogger().handlers[0].setFormatter(logging.Formatter("{} - %(levelname)s - %(message)s".format(sys.argv[0])))
+
 # assuming CouchDB initialization from local process to local server
 creds = ""
 if arguments.admin != "":
@@ -32,22 +35,23 @@ while True:
         version = server.version()
         break
     except:
-        log.error("Waiting for couchdb.")
+        logging.warning("Waiting for couchdb.")
         time.sleep(2)
 
 if arguments.database not in server:
     server.create(arguments.database)
 couchdb = server[arguments.database]
-log.error("Connected to couchdb")
+logging.info('Connected to couchdb')
+
 """
 cleans out project data that is not being pointed at
 by a parameter space model, and cleans up models that 
 are not parameter space but point to project data
 """
-log.error("going through project data list")
+
 # get a view list of all pd ids
 for row in couchdb.view("slycat/project_datas"):
-    log.error("testing PD:%s" % str(row.id))
+    logging.info('Testing PD: {0}'.format(str(row.id)))
     pd_doc = couchdb.get(type="project_data",id=row.id)
     delete_pd = True
     # go through model list in pd and start cleaning
@@ -55,23 +59,23 @@ for row in couchdb.view("slycat/project_datas"):
         model_doc = couchdb.get(type="model",id=model_id)
         if model_doc is not None:
             if "model-type" in model_doc:
-                log.error("testing model type:[%s]" % str(model_doc["model-type"]))
+                #log.error("testing model type:[%s]" % str(model_doc["model-type"]))
                 if model_doc["model-type"] == "parameter-image":
-                    log.error("found parameter-image not deleting PD: %s" % str(row.id))
+                    logging.info('Skipping deletion of parameter-image PD: {0}'.format(str(row.id)))
                     delete_pd = False
                 # clean up models that don't need pd
                 elif "project_data" in model_doc:
-                    log.error("Removing PD from none parameter-space model")
+                    logging.info('Deleting non-parameter-image PD: {0}'.format(str(row.id)))
                     del model_doc["project_data"]
                     couchdb.save(model_doc)
             # clean up models that don't have a type
             elif "project_data" in model_doc:
-                log.error("Removing PD from none type model")
+                logging.info('Deleting PD: {0} from model without a model-type'.format(str(row.id)))
                 del model_doc["project_data"]
                 couchdb.save(model_doc)
     if delete_pd:
-        # delete the bad project data
-        log.error("starting Deletion of PD::: %s" % str(row.id))
+        # delete project data that doesn't have an associated model 
+        logging.info('Deleting PD with NO mid: {0}'.format(str(row.id)))
         couchdb.delete(pd_doc)
-        log.error("finished Deletion of PD::: %s" % str(row.id))
-log.error("**Finished running slycat-clean-pd script**")
+        
+logging.info('Done deleting project datas')
