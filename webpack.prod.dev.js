d77b2ea424e986dc7d5ddc5d2401457c187df989
Alex M. Sielicki <alex@techmilo.com>
2020-05-08 10:20:39 -0700

    Cleaning up new prod.dev config

--- webpack.prod.dev.js
+++ webpack.prod.dev.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // Commenting out ExportNodeModules plugin because it crashes with Babel7
 // const ExportNodeModules = require('webpack-node-modules-list');
@@ -19,19 +18,6 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    // Commenting out Visualizer and Analyzer plugins because they're not needed in production
-    // new Visualizer({
-    //   filename: 'webpack-visualizer-stats.html'
-    // }),
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static',
-    //   reportFilename: 'webpack-bundle-analyzer-report.html',
-    //   openAnalyzer: false,
-    //   generateStatsFile: true,
-    //   statsFilename: 'webpack-bundle-analyzer-stats.json',
-    // }),
-    // Commenting out ExportNodeModules plugin because it crashes with Babel7
-    // new ExportNodeModules(),
   ],
   devServer: {
     // Only compiles on refresh, not on file change. But does not work, complains of running webpack twice.
