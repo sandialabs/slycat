7aa7a0a5f5fc8b4743b89283ddbc33b69b440b88
Matthew Letter <mletter@sandia.gov>
2020-05-15 13:54:10 -0600

    cleanup

--- web-server/plugins/slycat-model-wizards/slycat-model-wizards.py
+++ web-server/plugins/slycat-model-wizards/slycat-model-wizards.py
@@ -6,9 +6,6 @@ def register_slycat_plugin(context):
   import os
   context.register_wizard("slycat-delete-model", "Delete Model", require={"action":"delete", "context":"model"})
   context.register_wizard("slycat-edit-model", "Model Name & Info", require={"action":"edit", "context":"model"})
-
-  context.register_wizard("slycat-edit-project", "Edit Project", require={"action":"edit", "context":"model"})
-  
   context.register_wizard("slycat-create-template", "Template", require={"action":"create", "context":"model"})
   # Alex commenting this out and instead using the line below because the Save As functionality is not ready yet.
   # context.register_wizard("slycat-create-saved-bookmark", "Save As", require={"action":"create", "context":"model"})
