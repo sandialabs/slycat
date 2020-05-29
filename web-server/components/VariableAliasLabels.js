d3c7491f36a20c55062b498531440db85c5e0b07
Alex M. Sielicki <alex@techmilo.com>
2020-05-29 14:01:42 -0700

    Converting Variable Alias Labels to a pure component for performance reasons.

--- web-server/components/VariableAliasLabels.js
+++ web-server/components/VariableAliasLabels.js
@@ -2,66 +2,70 @@ import React from "react";
 import css from "css/slycat-variable-alias-labels.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { render } from "enzyme";
 
-export default function VariableAliasLabels(props) {
+export default class VariableAliasLabels extends React.PureComponent {
 
-  let variables = props.metadata['column-names'];
+  variables = this.props.metadata['column-names'];
 
-  let clearLabel = (event) => {
+  clearLabel = (event) => {
     let index = event.currentTarget.name;
     let input = document.querySelector(`.slycat-variable-alias-labels input[name='${index}'`);
     input.value = '';
-    props.onChange(event);
+    this.props.onChange(event);
   }
 
-  return (
-    <div className={`slycat-variable-alias-labels ${props.uniqueID}`}>
-      <table className='table table-striped table-hover table-sm table-borderless'>
-        <thead>
-          <tr>
-            <th scope='col' className='align-top'>Name</th>
-            <th scope='col' className='align-top'>Label</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-          variables.map((variable, index) => {
-            let userInput = props.variableAliases[index] != undefined;
-            return (
-              <tr key={index}>
-                <th scope='row' className='align-middle variable-name'>{variable}</th>
-                <td>
-                <div className='input-group input-group-sm'>
-                    <input type='text' 
-                      className={`form-control form-control-sm variable-alias 
-                        ${userInput ? 'contains-user-input' : ''}`}
-                      minLength='0' 
-                      maxLength='256'
-                      name={index}
-                      value={props.variableAliases[index] ? props.variableAliases[index] : ''} 
-                      placeholder={variables[index]}
-                      onChange={props.onChange} />
-                    <div className='input-group-append'>
-                      <button 
-                        className='btn btn-outline-secondary' 
-                        type='button'
-                        title='Clear alias label.'
+  render() {
+    console.log('VariableAliasLabels render');
+    return (
+      <div className={`slycat-variable-alias-labels ${this.props.uniqueID}`}>
+        <table className='table table-striped table-hover table-sm table-borderless'>
+          <thead>
+            <tr>
+              <th scope='col' className='align-top'>Name</th>
+              <th scope='col' className='align-top'>Label</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            this.variables.map((variable, index) => {
+              let userInput = this.props.variableAliases[index] != undefined;
+              return (
+                <tr key={index}>
+                  <th scope='row' className='align-middle variable-name'>{variable}</th>
+                  <td>
+                  <div className='input-group input-group-sm'>
+                      <input type='text' 
+                        className={`form-control form-control-sm variable-alias 
+                          ${userInput ? 'contains-user-input' : ''}`}
+                        minLength='0' 
+                        maxLength='256'
                         name={index}
-                        value=''
-                        disabled={!userInput}
-                        onClick={clearLabel}
-                      >
-                        <FontAwesomeIcon icon={faTimes} />
-                      </button>
+                        value={this.props.variableAliases[index] ? this.props.variableAliases[index] : ''} 
+                        placeholder={this.variables[index]}
+                        onChange={this.props.onChange} />
+                      <div className='input-group-append'>
+                        <button 
+                          className='btn btn-outline-secondary' 
+                          type='button'
+                          title='Clear alias label.'
+                          name={index}
+                          value=''
+                          disabled={!userInput}
+                          onClick={this.clearLabel}
+                        >
+                          <FontAwesomeIcon icon={faTimes} />
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                </td>
-              </tr>
-            )
-          })
-        }
-        </tbody>
-      </table>
-    </div>
-  );
+                  </td>
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
