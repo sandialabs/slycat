6363f2b93f8cb278a9763d4eefd0d5fed43f82a0
Matthew Letter <mletter@sandia.gov>
2020-07-10 13:31:09 -0600

    improved the loading ui

---
+++ web-server/plugins/slycat-timeseries-model/plugin-components/LogList.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { LogListProps } from "./types";
+import Spinner from "components/Spinner.tsx";
+
+  /**
+   * Take items from the log and builds out dd elements
+   *
+   * @private
+   * @memberof LoadingPage
+   */
+  const logBuilder = (itemList: [string]): JSX.Element[] => {
+    return itemList.map((item, index) => <dd key={index.toString()}>{JSON.stringify(item)}</dd>);
+  };
+
+  /**
+   * Creates the JSX for the log list
+   *
+   * @private
+   * @memberof LoadingPage
+   */
+  const LogList: React.FC<LogListProps> = ({sessionExists, jobStatus, log}) => {
+    let items: JSX.Element[] = logBuilder(log.logLineArray);
+    return sessionExists ? (
+      <dl>
+        <dt>`&gt;` {jobStatus}</dt>
+        <dt>`&gt;` Slurm run Log:</dt>
+        {items.length >= 1 ? items : <Spinner />}
+      </dl>
+    ) : (
+      <Spinner />
+    );
+  };
+  export default LogList;
