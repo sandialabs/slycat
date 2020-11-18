fe9ab6befb81a25137049f7e6552bdf0a54220f9
Shawn Martin <smartin@sandia.gov>
2020-11-18 09:23:54 -0700

    Fixed docstrings in slycat web client.

--- packages/slycat/web/client/__init__.py
+++ packages/slycat/web/client/__init__.py
@@ -65,18 +65,18 @@ def _require_array_ranges(ranges):
 # https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 def print_progress_bar (iteration, total, prefix = '', suffix = '', 
   decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
+    
+    # Call in a loop to create terminal progress bar
+    # @params:
+    #     iteration   - Required  : current iteration (Int)
+    #     total       - Required  : total iterations (Int)
+    #     prefix      - Optional  : prefix string (Str)
+    #     suffix      - Optional  : suffix string (Str)
+    #     decimals    - Optional  : positive number of decimals in percent complete (Int)
+    #     length      - Optional  : character length of bar (Int)
+    #     fill        - Optional  : bar fill character (Str)
+    #     printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
