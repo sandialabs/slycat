f8559d1b6fb100130a4adc9df130dd0943bd9614
Alex Milosz Sielicki <alex@techmilo.com>
2021-01-28 13:09:45 -0800

    Adding enzyme adapter for react 17.

--- web-server/__tests__/setupTests.js
+++ web-server/__tests__/setupTests.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
 const localStorageMock = {
