99ab9570cec307933c17b22ee39d36bc493a8737
Gittinger <jgittin@s968301.srn.sandia.gov>
2021-05-13 16:00:36 -0600

    Initial commit, adding in react page

---
+++ web-server/plugins/slycat-smb/ui.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const SMBWizard= () => {
+    return (
+        <div>
+            SMB Wizard
+        </div>
+    )
+}
+ReactDOM.render(
+    <SMBWizard />,
+    document.querySelector(".smb-wizard")
+  );
