bbbdb343b9078af806d2dd1f48a0ee4be6ca7328
Gittinger <jgittin@s968301.srn.sandia.gov>
2021-05-13 16:18:01 -0600

    Adding remote file browser to smb page

--- web-server/plugins/slycat-smb/ui.js
+++ web-server/plugins/slycat-smb/ui.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import RemoteFileBrowser from 'components/RemoteFileBrowser.tsx'
+
+const onSelectTableFile = () => {
+    console.log("onSelecTableFile");
+}
+
+const onReauth = () => {
+    console.log("onReauth");
+}
+
 const SMBWizard= () => {
     return (
         <div>
             SMB Wizard
+            <RemoteFileBrowser
+              onSelectFileCallBack={onSelectTableFile}
+              onReauthCallBack={onReauth}
+              hostname={'localhost'}
+            />
         </div>
     )
 }
