253659246897b03435621d6e40a28f24be122b0e
Alex M. Sielicki <alex@techmilo.com>
2021-07-09 10:40:41 -0700

    Adding react selector component

---
+++ web-server/plugins/slycat-dac/js/components/Selector.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as d3 from "d3v5";
+// import * as d3 from "d3v6";
+import * as fc from "d3fcv14";
+import * as fcWebgl from "d3fc-webgl";
+
+class Selector extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    // Don't render anything if we have no mds_coords
+    if (!this.props.mds_coords || this.props.mds_coords.length == 0) {
+      return null;
+    }
+
+    const data = this.props.mds_coords;
+    
+    const border = this.props.SCATTER_BORDER;
+    const xScale = d3.scaleLinear().domain([0 - border, 1 + border]);
+    const yScale = d3.scaleLinear().domain([0 - border, 1 + border]);
+
+    return (
+      <div>
+        {/* <div>This is the React scatter plot.</div>
+        <br />
+        <div>These are from redux state.</div>
+        <div>mds_coords is: {this.props.mds_coords}</div>
+        <br />
+        <div>These are from local state.</div>
+        <div>MAX_POINTS_ANIMATE is: {this.props.MAX_POINTS_ANIMATE}</div>
+        <div>SCATTER_PLOT_TYPE is: {this.props.SCATTER_PLOT_TYPE}</div>
+        <div>cont_colormap is: {this.props.cont_colormap}</div>
+        <div>SCATTER_BORDER is: {this.props.SCATTER_BORDER}</div>
+        <div>POINT_COLOR is: {this.props.POINT_COLOR}</div>
+        <div>POINT_SIZE is: {this.props.POINT_SIZE}</div>
+        <div>NO_SEL_COLOR is: {this.props.NO_SEL_COLOR}</div>
+        <div>SELECTION_COLOR is: {this.props.SELECTION_COLOR}</div>
+        <div>FOCUS_COLOR is: {this.props.FOCUS_COLOR}</div>
+        <div>COLOR_BY_LOW is: {this.props.COLOR_BY_LOW}</div>
+        <div>COLOR_BY_HIGH is: {this.props.COLOR_BY_HIGH}</div>
+        <div>OUTLINE_NO_SEL is: {this.props.OUTLINE_NO_SEL}</div>
+        <div>OUTLINE_SEL is: {this.props.OUTLINE_SEL}</div>
+        <div>var_include_columns is: {this.props.var_include_columns}</div>
+        <div>init_alpha_values is: {this.props.init_alpha_values}</div>
+        <div>init_color_by_col is: {this.props.init_color_by_col}</div>
+        <div>init_zoom_extent is: {this.props.init_zoom_extent}</div>
+        <div>init_subset_center is: {this.props.init_subset_center}</div> */}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    mds_coords: state.derived.mds_coords,
+  };
+};
+
+export default connect(mapStateToProps, {
+  // changeThreeDColormap,
+  // updateThreeDColorBy,
+})(Selector);
--- web-server/plugins/slycat-dac/js/dac-ui.js
+++ web-server/plugins/slycat-dac/js/dac-ui.js
@@ -33,8 +33,7 @@ import "layout";
 
 import React from "react";
 import ReactDOM from "react-dom";
-// import ScatterPlot from "./components/ScatterPlot";
-import ScatterPlot from "./components/ScatterPlot14";
+import Selector from "./components/Selector";
 
 import { createStore, applyMiddleware, } from 'redux';
 import { Provider, } from 'react-redux';
@@ -950,41 +949,41 @@ $(document).ready(function() {
                       init_subset_center
                     );
                         
-                    // // Create React scatterplot
-                    // const scatter_plot_react = 
-                    //   (
-                    //   <Provider store={store}>
-                    //     <ScatterPlot 
-                    //       MAX_POINTS_ANIMATE={MAX_POINTS_ANIMATE}
-                    //       // Padding around the scatterplot
-                    //       SCATTER_BORDER={SCATTER_BORDER}
-                    //       POINT_COLOR={POINT_COLOR}
-                    //       POINT_SIZE={POINT_SIZE}
-                    //       SCATTER_PLOT_TYPE={SCATTER_PLOT_TYPE}
-                    //       NO_SEL_COLOR={NO_SEL_COLOR}
-                    //       SELECTION_COLOR={SELECTION_COLOR}
-                    //       FOCUS_COLOR={FOCUS_COLOR}
-                    //       COLOR_BY_LOW={COLOR_BY_LOW}
-                    //       COLOR_BY_HIGH={COLOR_BY_HIGH}
-                    //       cont_colormap={cont_colormap}
-                    //       OUTLINE_NO_SEL={OUTLINE_NO_SEL}
-                    //       OUTLINE_SEL={OUTLINE_SEL}
-                    //       var_include_columns={var_include_columns}
-                    //       init_alpha_values={init_alpha_values}
-                    //       init_color_by_col={init_color_by_col}
-                    //       // This one seems like it should just be zoom_extent and tracked
-                    //       // entirely in the bookmark
-                    //       init_zoom_extent={init_zoom_extent}
-                    //       init_subset_center={init_subset_center}
-                    //     />
-                    //   </Provider>
-                    //   )
-                    // ;
+                    // Create React selector
+                    const selector = 
+                      (
+                      <Provider store={store}>
+                        <Selector 
+                          MAX_POINTS_ANIMATE={MAX_POINTS_ANIMATE}
+                          // Padding around the scatterplot
+                          SCATTER_BORDER={SCATTER_BORDER}
+                          POINT_COLOR={POINT_COLOR}
+                          POINT_SIZE={POINT_SIZE}
+                          SCATTER_PLOT_TYPE={SCATTER_PLOT_TYPE}
+                          NO_SEL_COLOR={NO_SEL_COLOR}
+                          SELECTION_COLOR={SELECTION_COLOR}
+                          FOCUS_COLOR={FOCUS_COLOR}
+                          COLOR_BY_LOW={COLOR_BY_LOW}
+                          COLOR_BY_HIGH={COLOR_BY_HIGH}
+                          cont_colormap={cont_colormap}
+                          OUTLINE_NO_SEL={OUTLINE_NO_SEL}
+                          OUTLINE_SEL={OUTLINE_SEL}
+                          var_include_columns={var_include_columns}
+                          init_alpha_values={init_alpha_values}
+                          init_color_by_col={init_color_by_col}
+                          // This one seems like it should just be zoom_extent and tracked
+                          // entirely in the bookmark
+                          init_zoom_extent={init_zoom_extent}
+                          init_subset_center={init_subset_center}
+                        />
+                      </Provider>
+                      )
+                    ;
 
-                    // self.scatter_plot_react_render = ReactDOM.render(
-                    //   scatter_plot_react,
-                    //   document.getElementById('scatter-plot-react-debug')
-                    // );
+                    self.scatter_plot_react_render = ReactDOM.render(
+                        selector,
+                      document.getElementById('dac-selector')
+                    );
 
                   },
                   function () {
