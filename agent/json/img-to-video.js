4b4f642331721412994bc471923d4ca09671bac8
Matthew Letter <mletter@sandia.gov>
2022-03-25 14:09:17 -0600

    removing copyright on json files as this conflicts with json loads

--- agent/json/img-to-video.js
+++ agent/json/img-to-video.js
@@ -1,7 +1,3 @@
-// Copyright © 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract
-// DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
-// retains certain rights in this software.
-
 {
   "name": "img-to-video",
   "exec_path": "/home/slycat/install/conda/bin/python",
