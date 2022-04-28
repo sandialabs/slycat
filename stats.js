6e65905739584200bc02adcfd41d3dbb8e1dcbff
Alex M. Sielicki <alex@techmilo.com>
2022-04-28 11:13:07 -0700

    Cleaning up code. Doing another interation that runs webpack from inside stats script and tries to get at stats that way.

--- stats.js
+++ stats.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
+const webpack = require('webpack');
+const webpack_prod_config = require('./webpack.prod.js');
+
+console.debug(webpack_prod_config);
+
+webpack(webpack_prod_config, (err, stats) => { // [Stats Object](#stats-object)
+  if (err || stats.hasErrors()) {
+    // [Handle errors here](#error-handling)
+  }
+  // Done processing
+  console.debug(stats);
+});
 
 // Read in compilation-stats
 const data = require('./compilation-stats-prod.json');
@@ -8,8 +20,9 @@ const modules = data.modules;
 const packageLock = require('./package-lock.json');
 
 let js_modules_from_stats = {};
+let slycat_node_modules = {};
 
-console.log(`First the modules whose moduleType starts with javascript.`);
+// console.log(`First the modules whose moduleType starts with javascript.`);
 for(const module of modules)
 {
   const path_array = module.name.split('??')[0].split(`/`);
@@ -18,13 +31,14 @@ for(const module of modules)
 
   if(module.moduleType.startsWith('javascript') && last_index > -1)
   {
-    console.log(`moduleType: %o, name %o`, module.moduleType, module.name);
+    // console.log(`moduleType: %o, name %o`, module.moduleType, module.name);
 
     // For modules that being with @, need to look one level deeper
     const starts_with_at_symbol = path_array[last_index+1].startsWith('@');
     const path_array_sliced = path_array.slice(0, starts_with_at_symbol? last_index + 3 : last_index + 2);
 
     const node_modules_path = path_array_sliced.join('/');
+    const module_name = path_array_sliced.slice(1).join('/');
     const version_from_package_lock = packageLock.packages[node_modules_path] ? packageLock.packages[node_modules_path].version : 'NOTFOUND';
     
     js_modules_from_stats[module.name] = {
@@ -33,11 +47,16 @@ for(const module of modules)
       'starts_with_at_symbol' : starts_with_at_symbol,
       'path_array_sliced' : path_array_sliced,
       'node_modules_path' : node_modules_path,
+      'module_name' : module_name,
       'version_from_package_lock' : version_from_package_lock,
     };
+
+    slycat_node_modules[module_name] = version_from_package_lock;
   }
 }
-fs.writeFileSync('stats-js_modules_from_stats.json', JSON.stringify(js_modules_from_stats));
+
+fs.writeFileSync('stats-slycat_production_node_modules_debug.json', JSON.stringify(js_modules_from_stats, null, 2));
+fs.writeFileSync('slycat_production_node_modules.json', JSON.stringify(slycat_node_modules, null, 2));
 
 // Modules that are non javascript
 // console.log(`Then the modules whose moduleType does not start with javascript.`);
