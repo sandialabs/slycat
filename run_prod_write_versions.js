fc6ac18c612d250f2686b1f3ab59e86fe2a56592
Alex M. Sielicki <alex@techmilo.com>
2022-05-03 17:35:28 -0700

    Adding JS files that are included in slycat but not from node_modules

--- run_prod_write_versions.js
+++ run_prod_write_versions.js
@@ -46,8 +46,11 @@ webpack(webpack_prod_config, (err, stats) => { // [Stats Object](#stats-object)
     reasons: false,
   }).modules;
 
-  let js_modules_from_stats = {};
+  let node_modules_from_stats = {};
   let slycat_node_modules = [];
+
+  let slycat_modules_from_stats = {};
+  let slycat_modules = [];
 
   // console.log(`First the modules whose moduleType starts with javascript.`);
   for(const module of modules)
@@ -66,13 +69,13 @@ webpack(webpack_prod_config, (err, stats) => { // [Stats Object](#stats-object)
 
       // For modules that being with @, need to look one level deeper
       const starts_with_at_symbol = path_array[last_index+1].startsWith('@');
-      const path_array_sliced = path_array.slice(0, starts_with_at_symbol? last_index + 3 : last_index + 2);
+      const path_array_sliced = path_array.slice(0, starts_with_at_symbol ? last_index + 3 : last_index + 2);
 
       const node_modules_path = path_array_sliced.join('/');
       const module_name = path_array_sliced.slice(1).join('/');
       const version_from_package_lock = packageLock.packages[node_modules_path] ? packageLock.packages[node_modules_path].version : 'ERROR: MODULE VERSION NOT FOUND IN package-lock.json';
       
-      js_modules_from_stats[module.name] = {
+      node_modules_from_stats[module.name] = {
         'path_array' : path_array,
         'last_index' : last_index,
         'starts_with_at_symbol' : starts_with_at_symbol,
@@ -87,6 +90,35 @@ webpack(webpack_prod_config, (err, stats) => { // [Stats Object](#stats-object)
         version: version_from_package_lock,
       });
     }
+
+    // Deal with javascript modules not in node_modules
+    if(module.moduleType.startsWith('javascript') && last_index == -1)
+    {
+      // console.log(`moduleType: %o, name %o`, module.moduleType, module.name);
+
+      // For modules that being with @, need to look one level deeper
+      // const starts_with_at_symbol = path_array[last_index+1].startsWith('@');
+      const path_array_sliced = path_array.slice(0, last_index + 2);
+
+      const node_modules_path = path_array_sliced.join('/');
+      const module_name = path_array_sliced.slice(1).join('/');
+      // const version_from_package_lock = packageLock.packages[node_modules_path] ? packageLock.packages[node_modules_path].version : 'ERROR: MODULE VERSION NOT FOUND IN package-lock.json';
+      
+      slycat_modules_from_stats[module.name] = {
+        'path_array' : path_array,
+        // 'last_index' : last_index,
+        // 'starts_with_at_symbol' : starts_with_at_symbol,
+        'path_array_sliced' : path_array_sliced,
+        'node_modules_path' : node_modules_path,
+        'module_name' : module_name,
+        // 'version_from_package_lock' : version_from_package_lock,
+      };
+
+      slycat_modules.push({
+        name: module.name, 
+        // version: version_from_package_lock,
+      });
+    }
   }
 
   // Remove duplicate modules, but only if their versions are different. _.isEqual compares entire objects.
@@ -94,9 +126,18 @@ webpack(webpack_prod_config, (err, stats) => { // [Stats Object](#stats-object)
   // Sort my name then version
   slycat_node_modules = _.sortBy(slycat_node_modules, ['name', 'version'])
 
+
+  // Remove duplicate modules, but only if their versions are different. _.isEqual compares entire objects.
+  slycat_modules = _.uniqWith(slycat_modules, _.isEqual);
+  // Sort my name then version
+  slycat_modules = _.sortBy(slycat_modules, ['name'])
+
+
   // Write out a debug file
-  // fs.writeFileSync('stats_no_file-slycat_production_node_modules_debug.json', JSON.stringify(js_modules_from_stats, null, 2));
+  // fs.writeFileSync('stats_no_file-slycat_production_node_modules_debug.json', JSON.stringify(node_modules_from_stats, null, 2));
 
   // Write out file listing all node_module modules used in production Slycat build and their versions
   fs.writeFileSync('docs/javascript_dependencies_in_node_modules.json', JSON.stringify(slycat_node_modules, null, 2));
+
+  fs.writeFileSync('docs/javascript_dependencies_in_web_server.json', JSON.stringify(slycat_modules, null, 2));
 });
