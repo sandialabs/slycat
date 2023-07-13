3f20238145300f91141174adf522d1d52b6b1266
Alex M. Sielicki <alex@techmilo.com>
2023-07-12 19:02:10 -0700

    Making histogram work with auto_scale and hidden points.

--- web-server/plugins/slycat-parameter-image/js/Components/PSHistogram.tsx
+++ web-server/plugins/slycat-parameter-image/js/Components/PSHistogram.tsx
@@ -17,6 +17,7 @@ import {
   Y_LABEL_HORIZONTAL_OFFSET,
   selectYLabelY,
   selectXHasCustomRange,
+  selectXValuesWithoutHidden,
 } from "../selectors";
 import {
   selectShowHistogram,
@@ -24,6 +25,7 @@ import {
   selectFontSize,
   selectFontFamily,
   selectShowGrid,
+  selectAutoScale,
 } from "../scatterplotSlice";
 import * as d3 from "d3v7";
 
@@ -38,6 +40,7 @@ const PSHistogram: React.FC<PSHistogramProps> = (props) => {
   const x_ticks = useSelector(selectXTicks);
   const y_ticks = useSelector(selectYTicks);
   const x_values = useSelector(selectXValues);
+  const x_values_without_hidden = useSelector(selectXValuesWithoutHidden);
   const histogram_bar_stroke_width = useSelector(selectUnselectedBorderSize);
   const font_size = useSelector(selectFontSize);
   const font_family = useSelector(selectFontFamily);
@@ -50,6 +53,7 @@ const PSHistogram: React.FC<PSHistogramProps> = (props) => {
   const histogram_bar_color = slycat_color_maps.get_histogram_bar_color(colormap);
   const background = slycat_color_maps.get_background(colormap).toString();
   const x_has_custom_range = useSelector(selectXHasCustomRange);
+  const auto_scale = useSelector(selectAutoScale);
 
   // Only render the component if show_histogram is true
   if (!show_histogram) {
@@ -76,16 +80,23 @@ const PSHistogram: React.FC<PSHistogramProps> = (props) => {
     bin.domain(x_scale.domain()).thresholds(x_scale.ticks(x_ticks));
   }
 
-  const bins = bin(x_values);
+  const bins_without_hidden = bin(x_values_without_hidden);
+  const bins_with_hidden = bin(x_values);
 
   // Declare the y (vertical position) scale.
   const y_scale = d3
     .scaleLinear()
-    .domain([0, d3.max(bins, (d) => d.length)])
+    .domain([0, d3.max(bins_without_hidden, (d) => d.length)])
     .range(y_scale_range);
 
-  // Adjusting x_scale domain to match the bins
-  x_scale.domain([bins[0].x0, bins[bins.length - 1].x1]);
+  // Adjusting x_scale domain to match the bins.
+  // With auto_scale true, we use bins without hidden values to set the domain.
+  // But if it's false, we set it to match bins of x_values, not x_values_without_hidden.
+  const bins_for_x_scale_domain = auto_scale ? bins_without_hidden : bins_with_hidden;
+  x_scale.domain([
+    bins_for_x_scale_domain[0].x0,
+    bins_for_x_scale_domain[bins_for_x_scale_domain.length - 1].x1,
+  ]);
 
   return (
     <>
@@ -105,7 +116,7 @@ const PSHistogram: React.FC<PSHistogramProps> = (props) => {
         x_scale={x_scale}
         y_scale={y_scale}
         y_scale_range={y_scale_range}
-        bins={bins}
+        bins={bins_without_hidden}
         x_ticks={x_ticks}
         y_ticks={y_ticks}
         histogram_bar_color={histogram_bar_color}
