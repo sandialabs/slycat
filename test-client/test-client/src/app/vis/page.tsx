7f743a13394431918f4ee88f04ef6ccf4b9009b6
Alex Milosz Sielicki <alex@techmilo.com>
2024-04-17 13:20:15 -0600

    update iframe embed src

--- test-client/test-client/src/app/vis/page.tsx
+++ test-client/test-client/src/app/vis/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Slycat Embed Example</h1>
         <iframe
-          src="https://localhost:9000/models/d6b9d0c45b194a6e9a1b2f56835f2f3e?bid=1bf973f86734f0af085fb986ac42cf18"
+          src="https://localhost:9000/models/737adc5b3949418886a1e8bb30419222?embed&hideTable&hideFilters&hideControls&bid=33438b7c9af95a61412cde73962159c1"
           className="w-full grow"
         />
     </main>
