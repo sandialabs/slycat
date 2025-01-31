bea730df78ad70dd21a67724264f10df621d46ab
Matt <mletter@sandia.gov>
2025-01-31 13:54:29 -0700

    adding child content modal body

--- web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
+++ web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { CCAModalContent}  from "./CCAWizardContent";
+import { CCAWizardSteps } from "./CCAWizardSteps"
 
 export const CCAWizard = () =>{
     return (
           <CCAModalContent
-            modalId={'this.state.modalId'}
-            closingCallBack={()=>console.log('clean')}
-            title={"CCA Wizard"}
-            body={<div>Body</div>}
+            key={'slycat-wizard'}
+            // slycat-wizard is the standard wizard id from knockout
+            modalId={'slycat-wizard'}
+            closingCallBack={()=>console.log('clean and delete model')}
+            title={"New CCA Model"}
             footer={[<div>Footer</div>]}
-          />
+          >
+            <CCAWizardSteps key={'CCA Steps'}></CCAWizardSteps>
+            </CCAModalContent>
         );
 }
