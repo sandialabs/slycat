b164275b5f49920f007f7b447e797d0ffe8938ff
Matt <mletter@sandia.gov>
2025-01-31 13:34:41 -0700

    start porting cca to reactjs

---
+++ web-server/plugins/slycat-cca/js/components/CCAWizardContent.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+/**
+ * @member closingCallBack callback function for cleanup when closing the modal
+ * @member title test for the top of the modal
+ */
+export interface ModalContentProps {
+  modalId: string,
+  closingCallBack: Function;
+  title: string;
+  body: JSX.Element;
+  footer: JSX.Element[];
+}
+
+
+export const CCAModalContent = (props:ModalContentProps) => {
+
+  const {modalId, closingCallBack, title, body, footer} = props;
+  /**
+   *close the modal and call the cleanup function
+   * @memberof ModalContent
+   */
+  const handleCloseModal = React.useCallback((e: React.MouseEvent): void => {
+    closingCallBack();
+    ($("#" + modalId) as any).modal("hide");
+  },[modalId, closingCallBack])
+
+    return (
+      <div>
+        <div className="modal-header">
+          <h3 className="modal-title">{title}</h3>
+          <button
+            type="button"
+            className="close"
+            aria-label="Close"
+            onClick={handleCloseModal}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body" id="slycat-wizard">
+          {body}
+        </div>
+        <div className="modal-footer">{footer}</div>
+      </div>
+    );
+}
