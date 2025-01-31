5a3e5ddcc144a7983c4e1db9b2c1cb5ba494f792
Matt <mletter@sandia.gov>
2025-01-31 16:59:26 -0700

    start building the content

--- web-server/plugins/slycat-cca/js/components/CCAWizardContent.tsx
+++ web-server/plugins/slycat-cca/js/components/CCAWizardContent.tsx
@@ -1,3 +1,6 @@
+/* Copyright (c) 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract
+ DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
+ retains certain rights in this software. */
 import * as React from "react";
 
 /**
@@ -5,42 +8,38 @@ import * as React from "react";
  * @member title test for the top of the modal
  */
 export interface ModalContentProps {
-  modalId: string,
+  modalId: string;
   closingCallBack: Function;
   title: string;
   footer?: React.ReactNode[] | undefined;
 }
 
-
-export const CCAModalContent = (props:React.PropsWithChildren<ModalContentProps>) => {
-
-  const {modalId, closingCallBack, title, children, footer} = props;
+export const CCAModalContent = (props: React.PropsWithChildren<ModalContentProps>) => {
+  const { modalId, closingCallBack, title, children, footer } = props;
   /**
    *close the modal and call the cleanup function
    * @memberof ModalContent
    */
-  const handleCloseModal = React.useCallback((e: React.MouseEvent): void => {
-    closingCallBack();
-    ($("#" + modalId) as any).modal("hide");
-  },[modalId, closingCallBack])
+  const handleCloseModal = React.useCallback(
+    (e: React.MouseEvent): void => {
+      closingCallBack();
+      ($("#" + modalId) as any).modal("hide");
+    },
+    [modalId, closingCallBack],
+  );
 
-    return (
-      <div>
-        <div className="modal-header">
-          <h3 className="modal-title">{title}</h3>
-          <button
-            type="button"
-            className="close"
-            aria-label="Close"
-            onClick={handleCloseModal}
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div className="modal-body" id="slycat-wizard">
-          {children}
-        </div>
-        <div className="modal-footer">{footer}</div>
+  return (
+    <div>
+      <div className="modal-header">
+        <h3 className="modal-title">{title}</h3>
+        <button type="button" className="close" aria-label="Close" onClick={handleCloseModal}>
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-    );
-}
+      <div className="modal-body" id="slycat-wizard">
+        {children}
+      </div>
+      <div className="modal-footer">{footer}</div>
+    </div>
+  );
+};
