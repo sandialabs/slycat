211da7579723d2cdd105ad58b511d6bcdb5bc89b
Matt <mletter@sandia.gov>
2025-03-27 17:01:33 -0600

    building redux store for the wizard

--- web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
+++ web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
@@ -5,19 +5,25 @@ import * as React from "react";
 import { CCAModalContent } from "./CCAWizardContent";
 import { CCAWizardSteps } from "./CCAWizardSteps";
 import { useCCAWizardFooter } from "./CCAWizardUtils";
+import { useAppDispatch } from "./wizard-store/hooks";
+import { resetTabTracking } from "./wizard-store/reducers/tabTrackingSlice";
 
 export const CCAWizard = () => {
   const cCAWizardFooter = useCCAWizardFooter();
+  const dispatch = useAppDispatch();
   return (
-    <CCAModalContent
-      key={"slycat-wizard"}
-      // slycat-wizard is the standard wizard id from knockout
-      modalId={"slycat-wizard"}
-      closingCallBack={() => console.log("clean and delete model")}
-      title={"New CCA Model"}
-      footer={cCAWizardFooter}
-    >
-      <CCAWizardSteps key={"CCA Steps"} />
-    </CCAModalContent>
+      <CCAModalContent
+        key={"slycat-wizard"}
+        // slycat-wizard is the standard wizard id from knockout
+        modalId={"slycat-wizard"}
+        closingCallBack={() => {
+          console.log("clean and delete model")
+          dispatch(resetTabTracking())
+        }}
+        title={"New CCA Model"}
+        footer={cCAWizardFooter}
+      >
+        <CCAWizardSteps key={"CCA Steps"} />
+      </CCAModalContent>
   );
 };
--- web-server/plugins/slycat-cca/js/components/CCAWizardUtils.tsx
+++ web-server/plugins/slycat-cca/js/components/CCAWizardUtils.tsx
@@ -2,15 +2,21 @@
  DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
  retains certain rights in this software. */
 import * as React from "react";
+import { useAppDispatch } from "./wizard-store/hooks";
+import { setTabName, TabNames } from "./wizard-store/reducers/tabTrackingSlice";
 
 export const useCCAWizardFooter = () => {
+  const dispatch = useAppDispatch();
   const backButton = (
     <button key="back button" className="btn btn-light mr-auto" onClick={() => console.log("back")}>
       Back
     </button>
   );
   const nextButton = (
-    <button key="continue" className="btn btn-primary" onClick={() => console.log("click")}>
+    <button key="continue" className="btn btn-primary" onClick={() => {
+      console.log("click")
+      dispatch(setTabName(TabNames.CCSLocalBrowserTab))
+    }}>
       Continue
     </button>
   );
