d4488f12dfffb7201b23ea380acd5a0275b9bd5a
Matt <mletter@sandia.gov>
2025-03-27 17:01:33 -0600

    building redux store for the wizard

---
+++ web-server/plugins/slycat-cca/js/components/wizard-tabs/CCSLocalBrowserTab.tsx
@@ -0,0 +1,14 @@
+/* Copyright (c) 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract
+ DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
+ retains certain rights in this software. */
+import * as React from "react";
+import { SlycatLocalBrowser } from "../slycat-local-browser/SlycatLocalBrowser";
+
+export const CCALocalBrowserTab = (props: {hidden?: boolean}) => {
+  const { hidden = false } = props;
+  return (
+    <div hidden={hidden}>
+        <SlycatLocalBrowser/>
+    </div>
+  );
+};
