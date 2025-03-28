dbcead6f1ed09f7045bd792ed38deb40dc1fa322
Matt <mletter@sandia.gov>
2025-03-28 16:26:17 -0600

    adding redux step state for cca wizard model

--- web-server/plugins/slycat-cca/js/components/slycat-local-browser/SlycatLocalBrowser.tsx
+++ web-server/plugins/slycat-cca/js/components/slycat-local-browser/SlycatLocalBrowser.tsx
@@ -4,28 +4,46 @@
 import * as React from "react";
 
 export const SlycatLocalBrowser = () => {
+
+      // var file = component.browser.selection()[0];
+      // let file_name = file.name;
+      // var fileObject = {
+      //   pid: component.project._id(),
+      //   mid: component.model._id(),
+      //   file: file,
+      //   aids: [["data-table"], file_name],
+      //   parser: component.parser(),
+      //   progress: component.browser.progress,
+      //   progress_status: component.browser.progress_status,
+      //   progress_final: 90,
+      //   success: function () {
+      //     upload_success(component.browser);
+      //   },
+      //   error: function () {
+      //     dialog.ajax_error(
+      //       "Did you choose the correct file and filetype?  There was a problem parsing the file: ",
+      //     )();
+      //     $(".local-browser-continue").toggleClass("disabled", false);
+      //     component.browser.progress(null);
+      //     component.browser.progress_status("");
+      //   },
+      // };
+      // fileUploader.uploadFile(fileObject);
+
   return (
     <div className="form-group row">
-      <label className="col-sm-2 col-form-label" data-bind="css: {'disabled' : disabled}">
-        File
-      </label>
+      <label className="col-sm-2 col-form-label">File</label>
       <div className="col-sm-10">
-        <input
-          type="file"
-          className=""
-          id="slycat-local-browser-file"
-          placeholder="file"
-          data-bind="event:{change: selection_changed}, attr: {'disabled' : disabled, 'multiple' : multiple}"
-        ></input>
-        <div className="progress" data-bind="visible: progress() != undefined && progress() > 0">
+        <input type="file" className="" id="slycat-local-browser-file" placeholder="file"></input>
+        <div className="progress" style={{ visibility: "hidden" }}>
           <div
             className="progress-bar progress-bar-striped progress-bar-animated"
             role="progressbar"
             aria-valuemin={0}
             aria-valuemax={100}
             // data-bind="
-            //   attr: {'aria-valuenow' : progress},
-            style={{'width' : '0' + '%'}}
+            aria-valuenow={100}
+            style={{ width: "0" + "%" }}
             //   text: progress_status,"
           >
             Uploading
