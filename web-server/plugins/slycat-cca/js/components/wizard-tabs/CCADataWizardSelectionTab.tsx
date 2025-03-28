a093bc3149fd1b3c7e8219c80496b27ac79434fe
Matt <mletter@sandia.gov>
2025-03-28 16:26:17 -0600

    adding redux step state for cca wizard model

--- web-server/plugins/slycat-cca/js/components/wizard-tabs/CCADataWizardSelectionTab.tsx
+++ web-server/plugins/slycat-cca/js/components/wizard-tabs/CCADataWizardSelectionTab.tsx
@@ -2,9 +2,13 @@
  DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
  retains certain rights in this software. */
 import * as React from "react";
+import { useAppDispatch, useAppSelector } from "../wizard-store/hooks";
+import { dataLocationType, selectDataLocation, setDataLocation } from "../wizard-store/reducers/cCAWizardSlice";
 
 export const CCAWizardDataSelectionTab = (props: {hidden?: boolean}) => {
   const { hidden = false } = props;
+  const dataLocation = useAppSelector(selectDataLocation)
+  const dispatch = useAppDispatch();
   return (
     <div hidden={hidden}>
       <div className="form-check" style={{ marginLeft: "15px" }}>
@@ -14,7 +18,8 @@ export const CCAWizardDataSelectionTab = (props: {hidden?: boolean}) => {
             name="local-or-remote-radios"
             id="local-radio"
             value="local"
-            data-bind="checked: cca_type"
+            onChange={React.useCallback(()=>{dispatch(setDataLocation(dataLocationType.LOCAL))},[dispatch])}
+            checked={dataLocation && dataLocation === dataLocationType.LOCAL}
           />
           Local
         </label>
@@ -26,7 +31,8 @@ export const CCAWizardDataSelectionTab = (props: {hidden?: boolean}) => {
             name="local-or-remote-radios"
             id="remote-radio"
             value="remote"
-            data-bind="checked: cca_type"
+            onChange={React.useCallback(()=>{dispatch(setDataLocation(dataLocationType.REMOTE))},[dispatch])}
+            checked={dataLocation && dataLocation === dataLocationType.REMOTE}
           />
           Remote
         </label>
