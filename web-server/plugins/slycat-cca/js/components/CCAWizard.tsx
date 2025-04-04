a39e28f0d7ab9ff167c8188e2df042c231cdb29e
Matt <mletter@sandia.gov>
2025-04-04 13:09:52 -0600

    adding model creation and cleanup

--- web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
+++ web-server/plugins/slycat-cca/js/components/CCAWizard.tsx
@@ -5,31 +5,65 @@ import * as React from "react";
 import { CCAModalContent } from "./CCAWizardContent";
 import { CCAWizardSteps } from "./CCAWizardSteps";
 import { useCCAWizardFooter } from "./CCAWizardUtils";
-import { useAppDispatch } from "./wizard-store/hooks";
-import { resetCCAWizard, setPid } from "./wizard-store/reducers/cCAWizardSlice";
+import { useAppDispatch, useAppSelector } from "./wizard-store/hooks";
+import {
+  resetCCAWizard,
+  selectMid,
+  selectPid,
+  setMid,
+  setPid,
+} from "./wizard-store/reducers/cCAWizardSlice";
+import client from "js/slycat-web-client";
 
 interface CCAWizardParams {
   pid: string;
+  marking?: string;
 }
 
 export const CCAWizard = (params: CCAWizardParams) => {
-  const { pid } = params;
+  const { pid, marking } = params;
+  const [modalOpen, setModalOpen] = React.useState(true);
   const cCAWizardFooter = useCCAWizardFooter();
   const dispatch = useAppDispatch();
+  const statePid = useAppSelector(selectPid);
+  const stateMid = useAppSelector(selectMid);
 
   React.useEffect(() => {
-    dispatch(setPid(pid));
-   }, [dispatch, pid])
-  
+    console.log("statePid, pid, selectMid", statePid, pid, stateMid);
+    if (modalOpen) {
+      if (!statePid) {
+        dispatch(setPid(pid));
+      }
+      if (!stateMid && statePid) {
+        // create the model on open so we have something to reference later
+        client
+          .post_project_models_fetch({
+            pid: statePid,
+            type: "cca",
+            name: "",
+            description: "",
+            marking: marking ?? "",
+          })
+          .then((result) => {
+            dispatch(setMid(result.id));
+          });
+      }
+    }
+  }, [dispatch, pid, statePid, stateMid]);
+
   return (
     <CCAModalContent
       key={"slycat-wizard"}
       // slycat-wizard is the standard wizard id from knockout
       modalId={"slycat-wizard"}
-      closingCallBack={() => {
-        console.log("clean and delete model");
+      closingCallBack={React.useCallback(() => {
+        setModalOpen(false);
+        if (stateMid) {
+          console.log("delete");
+          client.delete_model_fetch({ mid: stateMid });
+        }
         dispatch(resetCCAWizard());
-      }}
+      }, [stateMid])}
       title={"New CCA Model"}
       footer={cCAWizardFooter}
     >
