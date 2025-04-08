b1e5f6c65f592954fad590747dc29202bdb45d68
Alex M. Sielicki <alex@techmilo.com>
2025-04-08 15:06:17 -0700

    feat: integrate model fetching and dynamic UI features in ShareModel component

--- web-server/plugins/slycat-model-wizards/share-ui.ts
+++ web-server/plugins/slycat-model-wizards/share-ui.ts
@@ -5,12 +5,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ShareModel from "./ShareModel";
+import slycatClient from "js/slycat-web-client";
 
 export default {
   viewModel: {
     createViewModel: function (params: any, componentInfo: { element: HTMLElement }) {
       const root = createRoot(componentInfo.element);
-      root.render(React.createElement(ShareModel));
+      
+      // Get the model ID from the URL path (last segment)
+      const pathname = window.location.pathname;
+      const pathParts = pathname.split('/');
+      const mid = pathParts[pathParts.length - 1];
+      
+      if (mid) {
+        // Fetch the model data
+        slycatClient.get_model_fetch(mid)
+          .then((model) => {
+            // Pass the model-type to the ShareModel component
+            root.render(React.createElement(ShareModel, { modelType: model["model-type"] }));
+          })
+          .catch((error) => {
+            console.error("Error fetching model:", error);
+            // Render component without model type if there's an error
+            root.render(React.createElement(ShareModel, {}));
+          });
+      } else {
+        // Render component without model type if no mid is found
+        root.render(React.createElement(ShareModel, {}));
+      }
 
       // Return the viewModel
       return {};
