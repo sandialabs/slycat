59efa6dfb62177e50b4ad2d410c35085e05de6db
Matt <mletter@sandia.gov>
2025-05-15 17:38:40 -0600

    adding table ingestion

---
+++ web-server/plugins/slycat-cca/js/components/wizard-tabs/CCATableIngestion.tsx
@@ -0,0 +1,45 @@
+/* Copyright (c) 2013, 2018 National Technology and Engineering Solutions of Sandia, LLC . Under the terms of Contract
+ DE-NA0003525 with National Technology and Engineering Solutions of Sandia, LLC, the U.S. Government
+ retains certain rights in this software. */
+import * as React from "react";
+import SlycatTableIngestion from "js/slycat-table-ingestion-react";
+import { useAppDispatch, useAppSelector } from "../wizard-store/hooks";
+import { selectAttributes } from "../wizard-store/reducers/cCAWizardSlice";
+import { useHandleTableIngestionOnChange } from "../CCAWizardUtils";
+
+export const CCATableIngestion = (props: { hidden?: boolean }) => {
+  const { hidden = false } = props;
+  const attributes = useAppSelector(selectAttributes);
+  const handleTableIngestionOnChange = useHandleTableIngestionOnChange(attributes);
+  let axes_properties = [
+    {
+      name: "Axis Type",
+      type: "select",
+      values: ["Input", "Output", "Neither"],
+      disabledValues: {
+        Log: [],
+      },
+    },
+  ];
+  return (
+    <div hidden={hidden}>
+      test
+      <SlycatTableIngestion
+        uniqueID="varOptions"
+        variables={attributes}
+        properties={axes_properties}
+        onChange={handleTableIngestionOnChange}
+        onBatchChange={handleTableIngestionOnChange}
+      />
+      <form role="form">
+        <div className="form-group mt-3">
+          <div className="form-check pl-1">
+            <label>
+              <input type="checkbox" data-bind="checked: scale_inputs" /> Scale to unit variance
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
