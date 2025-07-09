ec60878a01aa15864bbb6e6485e333b543643d8e
Alex Milosz Sielicki <alex@techmilo.com>
2025-07-09 11:49:43 -0700

    refactor: make ModalLarge close button compatible with bootstrap 5

--- web-server/components/ModalLarge.tsx
+++ web-server/components/ModalLarge.tsx
@@ -6,27 +6,25 @@ import * as React from "react";
  * @member title test for the top of the modal
  */
 export interface ModalLargeProps {
-  modalId: string
-  closingCallBack:Function
-  title: string
-  body: JSX.Element
-  footer: JSX.Element
+  modalId: string;
+  closingCallBack: Function;
+  title: string;
+  body: JSX.Element;
+  footer: JSX.Element;
 }
 
 /**
  * not used
  */
-export interface ModalLargeState {
-}
+export interface ModalLargeState {}
 
 /**
  * takes a list of messages to be displayed as a warning
  */
-export default class ModalLarge  extends React.Component<ModalLargeProps,ModalLargeState> {
-  public constructor(props:ModalLargeProps) {
+export default class ModalLarge extends React.Component<ModalLargeProps, ModalLargeState> {
+  public constructor(props: ModalLargeProps) {
     super(props);
-    this.state = {
-    }
+    this.state = {};
   }
 
   /**
@@ -34,29 +32,29 @@ export default class ModalLarge  extends React.Component<ModalLargeProps,ModalLa
    *
    * @memberof ModalLarge
    */
-  closeModal = (e: React.MouseEvent): void =>
-  {
+  closeModal = (e: React.MouseEvent): void => {
     this.props.closingCallBack();
-    ($('#' + this.props.modalId) as any).modal('hide');
   };
 
-  render () {
-    return(
-      <div className='modal fade' data-bs-backdrop='false' id={this.props.modalId}>
-        <div className='modal-dialog modal-lg'>
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h3 className='modal-title'>{this.props.title}</h3>
-              <button type='button' className='close' aria-label='Close' onClick={this.closeModal}>
-                <span aria-hidden='true'>&times;</span>
-              </button>
+  render() {
+    return (
+      <div className="modal fade" data-bs-backdrop="false" id={this.props.modalId}>
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3 className="modal-title">{this.props.title}</h3>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                onClick={this.closeModal}
+              />
             </div>
-            <div className='modal-body' id="slycat-wizard">
+            <div className="modal-body" id="slycat-wizard">
               {this.props.body}
             </div>
-            <div className='modal-footer'>
-              {this.props.footer}
-            </div>
+            <div className="modal-footer">{this.props.footer}</div>
           </div>
         </div>
       </div>
