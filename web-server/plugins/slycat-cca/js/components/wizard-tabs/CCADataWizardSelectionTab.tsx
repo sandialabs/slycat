4227b4cd4cd950ef67d4d4fc8f13092648f74abb
Matt <mletter@sandia.gov>
2025-07-31 16:48:38 -0600

    cleanup and styling

--- web-server/plugins/slycat-cca/js/components/wizard-tabs/CCADataWizardSelectionTab.tsx
+++ web-server/plugins/slycat-cca/js/components/wizard-tabs/CCADataWizardSelectionTab.tsx
@@ -3,22 +3,29 @@
  retains certain rights in this software. */
 import * as React from "react";
 import { useAppDispatch, useAppSelector } from "../wizard-store/hooks";
-import { dataLocationType, selectDataLocation, setDataLocation } from "../wizard-store/reducers/cCAWizardSlice";
+import {
+  dataLocationType,
+  selectDataLocation,
+  setDataLocation,
+} from "../wizard-store/reducers/cCAWizardSlice";
 
-export const CCAWizardDataSelectionTab = (props: {hidden?: boolean}) => {
+export const CCAWizardDataSelectionTab = (props: { hidden?: boolean }) => {
   const { hidden = false } = props;
-  const dataLocation = useAppSelector(selectDataLocation)
+  const dataLocation = useAppSelector(selectDataLocation);
   const dispatch = useAppDispatch();
   return (
     <div hidden={hidden}>
       <div className="form-check" style={{ marginLeft: "15px" }}>
         <label>
           <input
+            style={{ marginRight: ".5rem" }}
             type="radio"
             name="local-or-remote-radios"
             id="local-radio"
             value="local"
-            onChange={React.useCallback(()=>{dispatch(setDataLocation(dataLocationType.LOCAL))},[dispatch])}
+            onChange={React.useCallback(() => {
+              dispatch(setDataLocation(dataLocationType.LOCAL));
+            }, [dispatch])}
             checked={dataLocation && dataLocation === dataLocationType.LOCAL}
           />
           Local
@@ -27,11 +34,14 @@ export const CCAWizardDataSelectionTab = (props: {hidden?: boolean}) => {
       <div className="form-check" style={{ marginLeft: "15px" }}>
         <label>
           <input
+            style={{ marginRight: ".5rem" }}
             type="radio"
             name="local-or-remote-radios"
             id="remote-radio"
             value="remote"
-            onChange={React.useCallback(()=>{dispatch(setDataLocation(dataLocationType.REMOTE))},[dispatch])}
+            onChange={React.useCallback(() => {
+              dispatch(setDataLocation(dataLocationType.REMOTE));
+            }, [dispatch])}
             checked={dataLocation && dataLocation === dataLocationType.REMOTE}
           />
           Remote
