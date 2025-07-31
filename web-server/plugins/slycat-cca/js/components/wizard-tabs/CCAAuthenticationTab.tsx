e3dbf1c8327fd425b3d074ea75efc07ceb2aa47e
Matt <mletter@sandia.gov>
2025-07-31 16:37:50 -0600

    handle enter keydown on form

--- web-server/plugins/slycat-cca/js/components/wizard-tabs/CCAAuthenticationTab.tsx
+++ web-server/plugins/slycat-cca/js/components/wizard-tabs/CCAAuthenticationTab.tsx
@@ -8,30 +8,48 @@ import {
   AuthenticationInformation,
   selectAuthInfo,
   selectLoading,
+  selectTab,
   setAuthInfo,
+  setTabName,
+  TabNames,
 } from "../wizard-store/reducers/cCAWizardSlice";
+import { useHandleAuthentication } from "../CCAWizardUtils";
 export const CCAAuthenticationTab = (props: { hidden?: boolean }) => {
   const { hidden = false } = props;
   const dispatch = useAppDispatch();
+  const handleAuthentication = useHandleAuthentication();
   const authInfo = useAppSelector(selectAuthInfo);
   const loading = useAppSelector(selectLoading);
-  const callBack = (
-    hostname: string,
-    username: string,
-    password: string,
-    session_exists: boolean,
-  ) => {
-    const authInfo: AuthenticationInformation = {
-      username,
-      password: btoa(password),
-      // decode with atob
-      hostname,
-      sessionExists: session_exists,
-    };
-    dispatch(setAuthInfo(authInfo));
-  };
+  const callBack = React.useCallback(
+    (hostname: string, username: string, password: string, session_exists: boolean) => {
+      const authInfo: AuthenticationInformation = {
+        username,
+        password: btoa(password),
+        // decode with atob
+        hostname,
+        sessionExists: session_exists,
+      };
+      dispatch(setAuthInfo(authInfo));
+    },
+    [dispatch],
+  );
   return (
-    <div hidden={hidden}>
+    <div
+      hidden={hidden}
+      onKeyUp={React.useCallback(
+        (e: React.KeyboardEvent) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (authInfo?.sessionExists) {
+              dispatch(setTabName(TabNames.CCA_REMOTE_BROWSER_TAB));
+            } else {
+              handleAuthentication();
+            }
+          }
+        },
+        [authInfo, dispatch, handleAuthentication],
+      )}
+    >
       <SlycatRemoteControls
         sessionExists={authInfo.sessionExists}
         loadingData={loading}
