4374fb34a61de12de6c3522b996a48f8fda84250
Alex Milosz Sielicki <alex@techmilo.com>
2025-08-05 15:14:25 +0200

    style: prettier

--- web-server/components/SlycatTimeInput.tsx
+++ web-server/components/SlycatTimeInput.tsx
@@ -1,77 +1,84 @@
-'use strict';
-import * as React from 'react';
+import * as React from "react";
 
 /**
  */
 export interface SlycatTimeInputProps {
-  hours: number
-  minutes: number
-  minCallBack: Function
-  hourCallBack: Function
-  label: string
+  hours: number;
+  minutes: number;
+  minCallBack: Function;
+  hourCallBack: Function;
+  label: string;
 }
 
 /**
  * not used
  */
 export interface SlycatTimeInputState {
-  hours: number
-  minutes: number
+  hours: number;
+  minutes: number;
 }
 /**
  * class that creates a a form with checkboxes
  * some other process
  */
-export default class SlycatTimeInput extends React.Component<SlycatTimeInputProps, SlycatTimeInputState> {
+export default class SlycatTimeInput extends React.Component<
+  SlycatTimeInputProps,
+  SlycatTimeInputState
+> {
   /**
    * not used
    */
-  public constructor(props:SlycatTimeInputProps) {
-    super(props)
+  public constructor(props: SlycatTimeInputProps) {
+    super(props);
     this.state = {
       hours: props.hours,
-      minutes: props.minutes
-    }
+      minutes: props.minutes,
+    };
   }
 
   onHourChange = (value) => {
     // localStorage.setItem("slycat-remote-controls-username", value);
     // this.setState({value: value});
-    if (value === '') {
+    if (value === "") {
       value = 0;
     }
-    this.setState({hours:value});
+    this.setState({ hours: value });
     this.props.hourCallBack(value);
   };
 
   onMinuteChange = (value) => {
-    if (value === '') {
+    if (value === "") {
       value = 0;
     }
-    this.setState({minutes:value});
+    this.setState({ minutes: value });
     this.props.minCallBack(value);
-  }
+  };
 
-  public render () {
+  public render() {
     return (
-    <div className='form-group row mb-3'>
-        <label className='col-sm-2 col-form-label'>{this.props.label}</label>
-        <div className='col-sm-2'>
+      <div className="form-group row mb-3">
+        <label className="col-sm-2 col-form-label">{this.props.label}</label>
+        <div className="col-sm-2">
           <input
-            className='form-control' type='number' min={0}
+            className="form-control"
+            type="number"
+            min={0}
             value={this.state.hours}
-            onChange={(e)=>this.onHourChange(e.target.value)}
-            />
-            Hours
+            onChange={(e) => this.onHourChange(e.target.value)}
+          />
+          Hours
         </div>
-        <div className='col-sm-2'>
-          <input className='form-control' type='number' min={0}
+        <div className="col-sm-2">
+          <input
+            className="form-control"
+            type="number"
+            min={0}
             value={this.state.minutes}
-            onChange={(e)=>this.onMinuteChange(e.target.value)}
-            />
-            Minutes
+            onChange={(e) => this.onMinuteChange(e.target.value)}
+          />
+          Minutes
         </div>
-    </div>
+      </div>
     );
   }
 }
