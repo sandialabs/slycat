56dd8d5aa8dcd02be1e6a819335425627c2a1502
Warren Hunt <wlhunt@sandia.gov>
2026-02-10 16:07:18 -0700

    copyright update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,9 +55,9 @@ for module_name in ["couchdb", "couchdb.client", "h5py", "numpy",
 # -- Project information -----------------------------------------------------
 
 project = 'Slycat'
-copyright = """2013, Sandia Corporation. Under the terms of Contract
-DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains certain
-rights in this software"""
+copyright = """Copyright 2025 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
+certain rights in this software."""
 author = 'Matthew Letter'
 master_doc = 'index'
 
